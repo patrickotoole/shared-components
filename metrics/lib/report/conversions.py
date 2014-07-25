b32890153caf1b64ffab683e81af91229bb53131
thisiswei <99plus2@gmail.com>
2014-07-25 13:56:52 -0400

    add logging, default reportconversions group to conversion, not getting advertisers with no active pixel ids

--- metrics/lib/report/conversions.py
+++ metrics/lib/report/conversions.py
@@ -24,18 +24,21 @@ Hourly cron script that report/pull conversion data:
 """
 from datetime import timedelta
 from collections import defaultdict
+import logging
 
 from lib.report.base import ReportBase
 from lib.report.base import _get_or_create_console
 
 from lib.report.utils.constants import GOOGLE_ADX
 from lib.report.utils.constants import ADVERTISER_IDS
+from lib.report.utils.constants import CONVERSIONS
 
 LIMIT = 1
 
 
 def _advertiser_to_pixels_mapping():
     console = _get_or_create_console()
+    logging.info("getting pixel ids")
     res = console.get('/pixel')
     pixels = res.json.get("response").get("pixels")
     d = defaultdict(list)
@@ -45,17 +48,27 @@ def _advertiser_to_pixels_mapping():
     return dict(d)
 
 PIXELS_D = None
+def _get_pixel_d():
+    global PIXELS_D
+    pixels_d = PIXELS_D or _advertiser_to_pixels_mapping()
+    if not PIXELS_D:
+        PIXELS_D = pixels_d
+    return pixels_d
 
 class ReportConversions(ReportBase):
+    def get_report(self, *args, **kwargs):
+        kwargs['group'] = CONVERSIONS
+        return super(ReportConversions, self).get_report(*args, **kwargs)
 
     def _filter(self, df, *args, **kwargs):
-        return _analyze(df)
+        return df
 
     def _get_timedelta(self, lookback):
         return timedelta(hours=lookback)
 
     def _get_advertiser_ids(self):
-        return ADVERTISER_IDS
+        pixel_d = _get_pixel_d()
+        return pixel_d.keys()
 
     def _get_pixel_ids(self, advertiser_id):
         global PIXELS_D
@@ -65,13 +78,4 @@ class ReportConversions(ReportBase):
         return pixels_d.get(advertiser_id)
 
 def _analyze(df):
-    grouped = df.groupby(['hour',
-                          'advertiser_id',
-                          'line_item_id',
-                          'campaign_id',
-                          'creative_id',
-                          'seller_member',
-                          ])
-    res = grouped[['imps', 'clicks', 'media_cost']].sum()
-    res = res.xs(GOOGLE_ADX, level="seller_member").reset_index()
-    return res
+    pass
