3d18956f48adabb8d4e410ae1ecbb86b56f09dc2
thisiswei <99plus2@gmail.com>
2014-07-25 17:52:51 -0400

    cache window hours, add pc, is_valid columns

--- metrics/lib/report/conversions.py
+++ metrics/lib/report/conversions.py
@@ -15,26 +15,27 @@ Hourly cron script that report/pull conversion data:
     auction_id
     imp_time
     conversion_time
-     - last_activity
-     - deleted
-     - active
-     - new_user
-     - is_valid
-     - notes
+    - last_activity
+    - deleted
+    - active
+    - new_user
+    - is_valid
+    - notes
 """
 from datetime import timedelta
 from collections import defaultdict
 import logging
 
+from link import lnk
+
 from lib.report.base import ReportBase
 from lib.report.base import _get_or_create_console
 
-from lib.report.utils.constants import GOOGLE_ADX
-from lib.report.utils.constants import ADVERTISER_IDS
 from lib.report.utils.constants import CONVERSIONS
+from lib.report.utils.utils import convert_datetime
+from lib.report.utils.utils import memo
 
 LIMIT = 1
-
 
 def _advertiser_to_pixels_mapping():
     console = _get_or_create_console()
@@ -55,12 +56,44 @@ def _get_pixel_d():
         PIXELS_D = pixels_d
     return pixels_d
 
+def _is_valid(row):
+    pid = row['pixel_id']
+    window_hours = _get_pc_or_pv_hour(pid)
+    window_hours = timedelta(window_hours.get('pc') if row['pc'] else
+                             window_hours.get('pv'))
+    conversion_time = convert_datetime(row['datetime'])
+    imp_time = convert_datetime(row['imp_time'])
+    row['is_valid'] = imp_time + window_hours <= conversion_time
+    return row
+
+@memo
+def _get_pc_or_pv_hours():
+    _db = _get_db()
+    query = "select * from advertiser_pixel where deleted=0"
+    res = _db.select(query)
+    dict_ = res.as_dict()
+    return dict((int(d.get('pixel_id')), dict(pc=int(d.get('pc_window_hours')),
+                                              pv=int(d.get('pv_window_hours')))
+                ) for d in dict_)
+
+def _get_pc_or_pv_hour(pid):
+    dict_ = _get_pc_or_pv_hours()
+    return dict_.get(pid)
+
+@memo
+def _get_db():
+    _db = lnk.dbs.mysql
+    return _db
+
 class ReportConversions(ReportBase):
     def get_report(self, *args, **kwargs):
         kwargs['group'] = CONVERSIONS
         return super(ReportConversions, self).get_report(*args, **kwargs)
 
     def _filter(self, df, *args, **kwargs):
+        df['pc'] = df['post_click_or_post_view_conv'] == 'Post Click'
+        df['is_valid'] = 0
+        df.apply(_is_valid, axis=1)
         return df
 
     def _get_timedelta(self, lookback):
@@ -77,5 +110,19 @@ class ReportConversions(ReportBase):
             PIXELS_D = pixels_d
         return pixels_d.get(advertiser_id)
 
-def _analyze(df):
-    pass
+def _groupby(df):
+    grouped = df.groupby(["pixel_id",
+                          "pixel_name",
+                          "line_item_id",
+                          "line_item_name",
+                          "campaign_id",
+                          "campaign_name",
+                          "creative_id",
+                          "creative_name",
+                          "order_id",
+                          "user_id",
+                          "auction_id",
+                          "imp_time",
+                          "datetime",
+                          ])
+    return grouped
