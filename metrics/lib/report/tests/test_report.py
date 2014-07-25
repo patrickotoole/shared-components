4391f111e523a784636727fcf864945bac9381b8
thisiswei <99plus2@gmail.com>
2014-07-25 18:16:57 -0400

    limit to 10

--- metrics/lib/report/tests/test_report.py
+++ metrics/lib/report/tests/test_report.py
@@ -20,7 +20,7 @@ class ReportTestCase(unittest.TestCase):
         obj = get_report_obj('domain')
         metrics = 'best'
         csv_path = os.path.realpath(__file__ + '../../test_csv_files/advertiser,site_domain.csv')
-        result = obj.get_report(path=csv_path, metrics=metrics)
+        result = obj.get_report(limit=10, path=csv_path, metrics=metrics)
         expected = {19235: 'Bigstock (225133)',
                     23825: 'Journelle (250058)',
                     23826: 'Journelle (250058)',
@@ -39,6 +39,7 @@ class ReportTestCase(unittest.TestCase):
         obj = get_report_obj('datapulling')
         resp = obj.get_report(
                 path=csv_path,
+                limit=10,
                 )
         expected = {0: 799,1: 2346, 2: 2635, 3: 10115, 4: 4811, 5: 714, 6: 1700, 7: 1122, 8: 17, 9: 204}
         response = resp.to_dict().get('imps')
