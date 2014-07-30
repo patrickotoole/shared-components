bca017a994ab8819be7e0f315d7224b46a63dedd
thisiswei <99plus2@gmail.com>
2014-07-30 13:04:29 -0400

    only raise when getting report fails

--- metrics/lib/report/work/report.py
+++ metrics/lib/report/work/report.py
@@ -7,6 +7,9 @@ from lib.report.utils.utils import local_now
 from lib.report.reportutils import get_default_db
 from lib.report.reportutils import get_report_obj
 
+class UnableGetReporError(ValueError):
+    pass
+
 
 class ReportWorker(BaseWorker):
     def __init__(self, name, _db=None):
@@ -23,35 +26,41 @@ class ReportWorker(BaseWorker):
         try:
             self._work(**kwargs)
             status = True
-        except Exception:
+        except UnableGetReporError:
             logging.info("report job : %s, failed" % self._name)
             status = False
 
         job_ended_at = local_now()
-        cur = self._db.cursor()
-        _create_report_events(cur,
+        con = self._db
+        _create_report_events(con,
                 name=self._name,
                 start=job_created_at,
                 end=job_ended_at,
-                status=status,
+                status=int(status),
                 )
         return status
 
     def _work(self, **kwargs):
         _obj = get_report_obj(self._name)
-        df = _obj.get_report(**kwargs)
-        table_name = _get_table_name(self._name)
-        cur = self._db.cursor()
+        table_name = _obj._table_name
+        key = _obj._get_unique_table_key()
+        try:
+            df = _obj.get_report(**kwargs)
+        except Exception as e:
+            logging.warn(e)
+            raise UnableGetReporError(e)
+        con = self._db
         col_names = df.columns.tolist()
         logging.info("inserting into table: %s, cols: %s" % (table_name, col_names))
-        _sql._write_mysql(df, table_name, col_names, cur)
+        _sql._write_mysql(df, table_name, col_names, con.cursor(), key=key)
+        con.commit()
 
 def _get_table_name(name):
     return ('v4_reporting' if name == 'datapulling' else
             'domain_reporting' if name == 'domain' else
             'conversion_reporting')
 
-def _create_report_events(cur, **kwargs):
+def _create_report_events(con, **kwargs):
     kwargs['failure'] = kwargs['success'] = None
     if kwargs['status']:
         kwargs['success'] = kwargs['end']
@@ -59,4 +68,6 @@ def _create_report_events(cur, **kwargs):
         kwargs['failure'] = kwargs['end']
     df = pd.DataFrame([kwargs])
     logging.info("creating report event")
-    _sql._write_mysql(df, "reportevent", df.columns.tolist(), cur)
+    cur = con.cursor()
+    _sql._write_mysql(df, "reportevent", df.columns.tolist(), cur, key=None)
+    con.commit()
