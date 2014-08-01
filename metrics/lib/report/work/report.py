8cf806c78edc5f464d39de2f39f2f8ebf886efe7
thisiswei <99plus2@gmail.com>
2014-08-01 01:38:59 -0400

    rearrange work report

--- metrics/lib/report/work/report.py
+++ metrics/lib/report/work/report.py
@@ -2,60 +2,64 @@ import logging
 
 from lib.report.work.base import BaseWorker
 from lib.pandas_sql import s as _sql
-from lib.report.utils.utils import local_now
-from lib.report.utils.utils import get_dates
+from lib.report.utils.sqlutils import get_report_names
+from lib.report.utils.sqlutils import get_unique_keys
 from lib.report.reportutils import get_db
 from lib.report.reportutils import get_report_obj
-from lib.report.event.report import EventReport
+from lib.report.event.report import accounting
 
-class UnableGetReporError(ValueError):
+
+class ReportError(ValueError):
     pass
 
-
 class ReportWorker(BaseWorker):
-    def __init__(self, name, _db=None):
-        self._db = _db or get_db()
+    def __init__(self, name=None, con=None):
+        self.con = con or get_db()
         self._name = name
 
     def do_work(self, **kwargs):
-        """
-        @return: bool, True if successful, False otherwise
-        """
-        job_created_at = kwargs.get('job_created_at') or local_now()
-        status =  None
+        report_names = get_report_names(self.con)
+        logging.info("job report starting: %s" % report_names)
+        for name in report_names:
+            try:
+                self._work(name, con=self.con, act=True, **kwargs)
+            except Exception:
+                logging.info("job: %s failed" % name)
+        logging.info("job report ended for: %s" % report_names)
+        return
 
+    @accounting
+    def _work(self, name, con=None, act=True, **kwargs):
+        name = self._name
+        _obj = get_report_obj(name)
+        report_f = _obj.get_report
+        analyze_f = get_analyze_func(name)
         try:
-            self._work(**kwargs)
-            status = True
-        except UnableGetReporError:
-            logging.info("report job -- %s, failed" % self._name)
-            status = False
-
-        job_ended_at = local_now()
-        start_date, end_date = get_dates(end_date=kwargs['end_date'],
-                                         lookback=kwargs['lookback'])
-        EventReport(event_name=self._name,
-                    db_wrapper=self._db,
-                    job_created_at=job_created_at,
-                    job_ended_at=job_ended_at,
-                    start_date=start_date,
-                    end_date=end_date,
-                    status=status,
-                    table_name='event_report',
-                    ).create_event()
-        return status
-
-    def _work(self, **kwargs):
-        _obj = get_report_obj(self._name)
-        table_name = _obj._table_name
-        key = _obj._get_unique_table_key()
-        try:
-            df = _obj.get_report(**kwargs)
+            df = report_f(**kwargs)
         except Exception as e:
             logging.warn(e)
-            raise UnableGetReporError(e)
-        con = self._db
-        col_names = df.columns.tolist()
-        logging.info("inserting into table: %s, cols: %s" % (table_name, col_names))
-        _sql._write_mysql(df, table_name, col_names, con.cursor(), key=key)
+            raise ReportError
+        df = analyze_f(df)
+        if act:
+            table_name = _obj._table_name
+            created = self._create_reports(df,
+                    con=con,
+                    table_name=table_name,
+                    key=get_unique_keys(con, table_name),
+                    )
+            if created:
+                logging.info("successfully created report for %" % name)
+
+    def _create_reports(self, df, con=None, table_name=None, key=None):
+        cols = df.columns.tolist()
+        logging.info("inserting into table: %s, cols: %s" % (table_name, cols))
+        _sql._write_mysql(df, table_name, cols, con.cursor(), key=key)
         con.commit()
+        return True
+
+
+def get_analyze_func(name):
+    assert name.isalnum()
+    import_str = "from lib.report.analyze.report import analyze_%s as analyze_func" % name
+    exec(import_str)
+    return analyze_func
