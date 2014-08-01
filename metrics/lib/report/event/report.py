cd0f4ddc4182334ed645253534d1bc8e1067b477
thisiswei <99plus2@gmail.com>
2014-08-01 01:38:17 -0400

    add event accounting decorator

--- metrics/lib/report/event/report.py
+++ metrics/lib/report/event/report.py
@@ -1,4 +1,7 @@
+import logging
 from lib.report.event.base import EventBase
+from lib.report.utils.utils import decorator
+from lib.report.utils.utils import local_now
 
 
 class EventReport(EventBase):
@@ -20,3 +23,28 @@ class EventReport(EventBase):
         self.status = status
         self.db_wrapper = db_wrapper
         self.table_name = table_name
+
+@decorator
+def accounting(f):
+    def _f(*args, **kwargs):
+        status = 0
+        job_created_at = local_now()
+        try:
+            res = f(*args, **kwargs)
+            status = 1
+        except Exception as e:
+            logging.warn(e)
+        job_ended_at = local_now()
+        event_name = filter(lambda a: isinstance(a, str), args)[0]
+        start_date, end_date = kwargs.get('start_date'), kwargs.get('end_date')
+        EventReport(event_name=event_name,
+                    db_wrapper=kwargs.get('con'),
+                    job_created_at=job_created_at,
+                    job_ended_at=job_ended_at,
+                    start_date=start_date,
+                    end_date=end_date,
+                    status=status,
+                    table_name='event_report',
+                    ).create_event()
+        return res
+    return _f
