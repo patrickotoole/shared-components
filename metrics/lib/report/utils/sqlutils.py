36fd9ca685eb2128a7d737be2bca9bdbdf663f28
thisiswei <99plus2@gmail.com>
2014-07-31 21:04:11 -0400

    avoid empty unique keys

--- metrics/lib/report/utils/sqlutils.py
+++ metrics/lib/report/utils/sqlutils.py
@@ -1,8 +1,13 @@
-SELECT_UNIQUE_KEY_QUERY = """SELECT group_concat(k.COLUMN_NAME) FROM information_schema.table_constraints t LEFT JOIN information_schema.key_column_usage k USING(constraint_name,table_schema,table_name) WHERE t.constraint_type='UNIQUE' AND t.table_schema=DATABASE() AND t.table_name='{table_name}'"""
+import logging
 
+SELECT_UNIQUE_KEY_QUERY = """SELECT group_concat(k.COLUMN_NAME) FROM information_schema.table_constraints t LEFT JOIN information_schema.key_column_usage k USING(constraint_name,table_schema,table_name) WHERE t.constraint_type='UNIQUE' AND t.table_schema=DATABASE() AND t.table_name='{table_name}'"""
 
 def get_unique_keys(cur, table_name):
     query = SELECT_UNIQUE_KEY_QUERY.format(table_name=table_name)
     res = cur.select(query)
-    keys = res.as_dict()[0].values()
+    try:
+        keys = res.as_dict()[0].values()
+    except Exception:
+        logging.warn("no unique key found for table: %s." % table_name)
+        return []
     return keys
