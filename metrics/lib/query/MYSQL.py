399ba074cc02cfd2233fec9528fa6ce8542b91f4
Ron Jacobson <ronjacobson@gmail.com>
2014-08-04 13:19:05 -0400

    updating reporting query to provide smaller respone for website. sorry patrick

--- metrics/lib/query/MYSQL.py
+++ metrics/lib/query/MYSQL.py
@@ -1,38 +1,40 @@
 BRAND_QUERY = "select external_id id, external_advertiser_id advertiser_id from creative"
 
 IMPS_QUERY = """
-    select 
-        0 is_valid, 
-        v3.* 
-    from 
-        v3_reporting as v3 
-    where 
-        v3.external_advertiser_id = %(advertiser_id)s 
-        and v3.active = 1 
-        and v3.deleted = 0
+select 
+	v3.campaign_id,
+	date_format(date_add(v3.date,interval -4 hour),"%Y-%m-%d %k:00:00") as date,
+	sum(v3.imps) as imps,
+	sum(v3.clicks) as clicks,
+	sum(v3.media_cost*v3.cpm_multiplier) as cost,
+	0 as conversions 
+from 
+	v3_reporting v3
+where 
+	v3.active=1 and 
+	v3.deleted=0 and 
+	v3.external_advertiser_id = %(advertiser_id)s  
+group by 
+	1,2
 """
 
 CONVERSION_QUERY = """
-    select 
-        is_valid, 
-        0 id, 
-        0 imps, 
-        0 clicks, 
-        campaign_id, 
-        creative_id, 
-        0 media_cost, 
-        external_advertiser_id, 
-        timestamp(DATE_FORMAT(conversion_time, "%%Y-%%m-%%d %%H:00:00")) date, 
-        last_activity, 
-        deleted, 
-        0 cpm_multiplier, 
-        active, 
-        NULL notes 
-    from conversion_reporting as cr 
-    where 
-        cr.external_advertiser_id = %(advertiser_id)s 
-        and cr.active = 1 
-        and cr.deleted = 0
+select 
+	campaign_id,
+	date_format(date_add(conversion_time,interval -4 hour),"%Y-%m-%d %k:00:00") as date,
+	0 imps,
+	0 as clicks,
+	0 as cost,
+	count(*) as conversions 
+from 
+	conversion_reporting 
+where 
+	active=1 and 
+	deleted=0 and 
+	external_advertiser_id=306383 and 
+	is_valid=1 
+group 
+	by 1,2;
 """
 
 UNION_QUERY = "%s UNION ALL (%s)" % (IMPS_QUERY, CONVERSION_QUERY)
