d53d9414b142081fca70793247881143dd0687be
rickotoole <rick@rockerbox.com>
2014-08-04 22:47:27 +0000

    Modifying query to return less data. Needed to opitimize reporting load time.

--- metrics/lib/query/MYSQL.py
+++ metrics/lib/query/MYSQL.py
@@ -1,40 +1,42 @@
 BRAND_QUERY = "select external_id id, external_advertiser_id advertiser_id from creative"
 
 IMPS_QUERY = """
-select 
-	v3.campaign_id,
-	date_format(date_add(v3.date,interval -4 hour),"%Y-%m-%d %k:00:00") as date,
-	sum(v3.imps) as imps,
-	sum(v3.clicks) as clicks,
-	sum(v3.media_cost*v3.cpm_multiplier) as cost,
-	0 as conversions 
-from 
-	v3_reporting v3
-where 
-	v3.active=1 and 
-	v3.deleted=0 and 
-	v3.external_advertiser_id = %(advertiser_id)s  
-group by 
-	1,2
+    select 
+        v3.campaign_id,
+        timestamp(DATE_FORMAT(v3.date, "%%Y-%%m-%%d %%H:00:00")) date,
+        sum(v3.imps) as imps,
+        sum(v3.clicks) as clicks,
+        sum(v3.media_cost*v3.cpm_multiplier) as media_cost,
+        1 as cpm_multiplier,
+        0 as is_valid 
+    from 
+        v3_reporting v3
+    where 
+        v3.active=1 and 
+        v3.deleted=0 and 
+        v3.external_advertiser_id = %(advertiser_id)s  
+    group by 
+        1,2
 """
 
 CONVERSION_QUERY = """
-select 
-	campaign_id,
-	date_format(date_add(conversion_time,interval -4 hour),"%Y-%m-%d %k:00:00") as date,
-	0 imps,
-	0 as clicks,
-	0 as cost,
-	count(*) as conversions 
-from 
-	conversion_reporting 
-where 
-	active=1 and 
-	deleted=0 and 
-	external_advertiser_id=306383 and 
-	is_valid=1 
-group 
-	by 1,2;
+    select 
+        campaign_id,
+        timestamp(DATE_FORMAT(conversion_time, "%%Y-%%m-%%d %%H:00:00")) date,
+        0 imps,
+        0 as clicks,
+        0 as media_cost,
+        1 as cpm_multiplier,
+        count(*) as is_valid
+    from 
+        conversion_reporting 
+    where 
+        active=1 and 
+        deleted=0 and 
+        external_advertiser_id= %(advertiser_id)s  and 
+        is_valid=1 
+    group 
+        by 1,2
 """
 
 UNION_QUERY = "%s UNION ALL (%s)" % (IMPS_QUERY, CONVERSION_QUERY)
@@ -47,5 +49,4 @@ BUCKET_QUERY = """
         bucket_name like '%%%(bucket)s%%' 
         and external_advertiser_id = %(advertiser)s
 """
-
 
