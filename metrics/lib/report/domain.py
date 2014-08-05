8c439f3a47ebbd5e773080e75c6e9372ffa8e7d4
thisiswei <99plus2@gmail.com>
2014-08-05 12:20:06 -0400

    raise when request form not found for group; default to advertiser,domain,line_item

--- metrics/lib/report/domain.py
+++ metrics/lib/report/domain.py
@@ -2,12 +2,11 @@
 report the least effective campins/advertiser/domain etc.
 """
 from lib.report.base import ReportBase
-from lib.report.utils.constants import DOMAIN
-from lib.report.analyze.report import analyze_domain
 
 from lib.report.request_json_forms import DOMAIN_JSON_FORM
 from lib.report.request_json_forms import ADVERTISER_DOMAIN_JSON_FORM
 from lib.report.request_json_forms import ADVERTISER_DOMAIN_CAMPAIGN_JSON_FORM
+from lib.report.request_json_forms import ADVERTISER_DOMAIN_LINE_ITEM_JSON_FORM
 
 
 class ReportDomain(ReportBase):
@@ -18,13 +17,16 @@ class ReportDomain(ReportBase):
 
     def get_report(self, *args, **kwargs):
         if not kwargs.get('group'):
-            kwargs['group'] = DOMAIN
+            kwargs['group'] = 'advertiser,domain,line_item'
         return super(ReportDomain, self).get_report(*args, **kwargs)
 
-    def _analyze_helper(self, df, pred=None, metrics=None):
-        return analyze_domain(df, metrics=metrics)
-
     def _get_form_helper(self, group):
-        return (DOMAIN_JSON_FORM if group == 'site_domain' else
-                ADVERTISER_DOMAIN_JSON_FORM if group == 'advertiser,domain' else
-                ADVERTISER_DOMAIN_CAMPAIGN_JSON_FORM)
+        if group == 'domain':
+            return DOMAIN_JSON_FORM
+        elif group == 'advertiser,domain':
+            return ADVERTISER_DOMAIN_JSON_FORM
+        elif group == 'advertiser,domain,campaign':
+            return ADVERTISER_DOMAIN_CAMPAIGN_JSON_FORM
+        elif group == 'advertiser,domain,line_item':
+            return ADVERTISER_DOMAIN_LINE_ITEM_JSON_FORM
+        raise ValueError("No form found for group: %s" % group)
