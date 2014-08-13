8d326ce469bebdf92e746f5bd873cc929876964a
thisiswei <99plus2@gmail.com>
2014-08-13 10:50:51 -0400

    move the api utils to a module

--- metrics/lib/report/base.py
+++ metrics/lib/report/base.py
@@ -4,18 +4,15 @@ import io
 
 import pandas as pd
 
-from lib.report.utils.utils import retry
-from lib.report.reportutils import get_or_create_console
 from lib.report.analyze.report import get_analyze_obj
-
-from lib.report.utils.constants import NUM_TRIES
-from lib.report.utils.constants import SLEEP
 from lib.report.utils.sqlutils import get_unique_keys
+from lib.report.utils.apiutils import get_path
+from lib.report.utils.apiutils import get_report_id
+from lib.report.utils.apiutils import get_report_url
+from lib.report.utils.apiutils import get_report_resp
 
 
 CUR_DIR = os.path.dirname(__file__)
-TMP_DIR = os.path.abspath('/tmp')
-FILE_FMT = '{name}_{group}{start_date}_{end_date}.csv'
 
 def _create_csv(df, path):
     logging.info("creating csv file to path: %s" % path)
@@ -24,75 +21,6 @@ def _create_csv(df, path):
 def _resp_to_df(resp):
     df = pd.read_csv(io.StringIO(unicode(resp)))
     return df
-
-def _get_path(
-        name=None,
-        group=None,
-        start_date=None,
-        end_date=None,
-        ):
-    _len = len("yyyy-mm-dd-hh")
-    def _helper(ts):
-        ts = (ts[:_len]).replace(' ', '-')
-        return ts
-    start_date, end_date = _helper(start_date), _helper(end_date)
-    file_name = FILE_FMT.format(name=name,
-            group=group or "",
-            start_date=start_date,
-            end_date=end_date,
-            ).lower()
-    path = os.path.join(TMP_DIR, file_name)
-    return path
-
-
-@retry(num_retries=NUM_TRIES,
-       sleep_interval=SLEEP,
-       retry_log_prefix='no report id detected, reconnecting',
-       )
-def _get_report_id(request_url, request_json_form):
-    resp = _get_resp(request_url, method='post', forms=request_json_form)
-    json_ = resp.json
-    error = json_.get('response').get('error')
-    if error:
-        logging.warn(error)
-    report_id = json_.get('response').get('report_id')
-    logging.info("Got report id: %s" % report_id)
-    assert report_id
-    return report_id
-
-@retry(num_retries=NUM_TRIES,
-       sleep_interval=SLEEP,
-       )
-def _get_report_url(report_id):
-    url = '/report?id={report_id}'.format(report_id=report_id)
-    resp = _get_resp(url)
-    _resp = resp.json.get('response')
-    url = _resp.get('report').get('url')
-    if not url:
-        status = _resp.get('execution_status')
-        logging.info("report: %s status is %s" % (report_id, status))
-        raise ValueError(status)
-    logging.info("report url is: %s" % url)
-    return url
-
-@retry(num_retries=NUM_TRIES,
-       sleep_interval=SLEEP,
-       retry_log_prefix='retrying getting resp',
-       )
-def _get_report_resp(url):
-    logging.info("getting url: %s" % url)
-    if not url.startswith('/'):
-        url = '/' + url
-    resp = _get_resp(url)
-    return resp.text
-
-def _get_resp(url, method='get', forms=None):
-    c = get_or_create_console()
-    if method == 'get':
-        resp = c.get(url)
-    else:
-        resp = c.post(url, forms)
-    return resp
 
 def _is_empty(df):
     return len(df) == 0
@@ -143,7 +71,7 @@ class ReportBase(object):
         logging.info("Getting start date: %s, end date: %s" % (start_date, end_date))
         _should_create_csv = False
         if cache:
-            path = _get_path(
+            path = get_path(
                     name=self._name,
                     group=group,
                     start_date=start_date,
@@ -192,9 +120,9 @@ class ReportBase(object):
 
     def _get_resp_helper(self, url, form):
         logging.info("requesting data from url: %s" % url)
-        _id = _get_report_id(url, form)
-        url = _get_report_url(_id)
-        resp = _get_report_resp(url)
+        _id = get_report_id(url, form)
+        url = get_report_url(_id)
+        resp = get_report_resp(url)
         return resp
 
     def _get_request_url(self, _id=None):
---
+++ metrics/lib/report/utils/apiutils.py
@@ -0,0 +1,80 @@
+import logging
+import os
+
+from lib.report.utils.utils import retry
+from lib.report.reportutils import get_or_create_console
+
+from lib.report.utils.constants import NUM_TRIES
+from lib.report.utils.constants import SLEEP
+
+FILE_FMT = '{name}_{group}{start_date}_{end_date}.csv'
+TMP_DIR = os.path.abspath('/tmp')
+
+def get_path(
+        name=None,
+        group=None,
+        start_date=None,
+        end_date=None,
+        ):
+    _len = len("yyyy-mm-dd-hh")
+    def _helper(ts):
+        ts = (ts[:_len]).replace(' ', '-')
+        return ts
+    start_date, end_date = _helper(start_date), _helper(end_date)
+    file_name = FILE_FMT.format(name=name,
+            group=group or "",
+            start_date=start_date,
+            end_date=end_date,
+            ).lower()
+    path = os.path.join(TMP_DIR, file_name)
+    return path
+
+
+@retry(num_retries=NUM_TRIES,
+       sleep_interval=SLEEP,
+       retry_log_prefix='no report id detected, reconnecting',
+       )
+def get_report_id(request_url, request_json_form):
+    resp = _get_resp(request_url, method='post', forms=request_json_form)
+    json_ = resp.json
+    error = json_.get('response').get('error')
+    if error:
+        logging.warn(error)
+    report_id = json_.get('response').get('report_id')
+    logging.info("Got report id: %s" % report_id)
+    assert report_id
+    return report_id
+
+@retry(num_retries=NUM_TRIES,
+       sleep_interval=SLEEP,
+       )
+def get_report_url(report_id):
+    url = '/report?id={report_id}'.format(report_id=report_id)
+    resp = _get_resp(url)
+    _resp = resp.json.get('response')
+    url = _resp.get('report').get('url')
+    if not url:
+        status = _resp.get('execution_status')
+        logging.info("report: %s status is %s" % (report_id, status))
+        raise ValueError(status)
+    logging.info("report url is: %s" % url)
+    return url
+
+@retry(num_retries=NUM_TRIES,
+       sleep_interval=SLEEP,
+       retry_log_prefix='retrying getting resp',
+       )
+def get_report_resp(url):
+    logging.info("getting url: %s" % url)
+    if not url.startswith('/'):
+        url = '/' + url
+    resp = _get_resp(url)
+    return resp.text
+
+def _get_resp(url, method='get', forms=None):
+    c = get_or_create_console()
+    if method == 'get':
+        resp = c.get(url)
+    else:
+        resp = c.post(url, forms)
+    return resp
