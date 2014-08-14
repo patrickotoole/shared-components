c1c13f1ff0b0d1e58a4b4d08401ffd5a9efb0cd6
thisiswei <99plus2@gmail.com>
2014-08-13 21:10:09 -0400

    test get_dataframe; good|bad

--- metrics/lib/report/tests/test_report.py
+++ metrics/lib/report/tests/test_report.py
@@ -1,14 +1,19 @@
 import os
+
+import mock
 import pandas as pd
-from mock import patch
 from link import lnk
 from twisted.trial import unittest
-from lib.report.common import get_report_obj
-from lib.report.work.report import ReportWorker
 
 from lib.pandas_sql import s as _sql
+from lib.report.common import get_report_obj
+from lib.report.utils.reportutils import get_path
+from lib.report.reports.base import ReportBase
+from lib.report.utils.reportutils import get_db
+from lib.report.utils.reportutils import empty_frame
 
 CUR_DIR = os.path.realpath(__file__)
+TEST_FILES = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files/'))
 DOMAIN = 'domain'
 DROP = "drop table roclocaltest.v4_reporting;"
 CREATE = """
@@ -36,6 +41,9 @@ CREATE = """
 # we want to use the setup method of the test case
 #db = lnk.dbs.test
 
+def _testfile_path(name):
+    return os.path.join(TEST_FILES, name)
+
 class ReportTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -51,14 +59,9 @@ class ReportTestCase(unittest.TestCase):
         self.db.execute(DROP)
         self.db.commit()
 
-    def test_get_report_obj(self):
-        obj = get_report_obj('domain', self.db)
-        expected = obj._name
-        self.assertEqual(expected, 'domain')
-
     def test_report_domain(self):
         obj = get_report_obj('domain', self.db)
-        csv_path = os.path.realpath(__file__ + '../../test_csv_files/advertiser,site_domain.csv')
+        csv_path = _testfile_path('advertiser,site_domain.csv')
         result = obj.get_report(path=csv_path, limit=10)
         expected = {
                 0: 'realsimple.com',
@@ -91,7 +94,7 @@ class ReportTestCase(unittest.TestCase):
         self.assertEqual(expected, response)
 
     def test_report_datapulling(self):
-        csv_path = os.path.realpath(__file__ + '../../test_csv_files/datapulling349923.csv')
+        csv_path = _testfile_path('datapulling349923.csv')
         obj = get_report_obj('datapulling', self.db)
         resp = obj.get_report(
                 path=csv_path,
@@ -103,7 +106,7 @@ class ReportTestCase(unittest.TestCase):
 
     #TODO fix this report
     def test_write_mysql_insert_query(self):
-        csv_path = os.path.realpath(__file__ + '../../test_csv_files/test2.csv')
+        csv_path = _testfile_path('test2.csv')
         df = pd.read_csv(csv_path, index_col=True)
         cur = self.db.cursor()
         _cur = _sql._write_mysql(df, 'v4_reporting', df.columns.tolist(), cur)
@@ -111,3 +114,41 @@ class ReportTestCase(unittest.TestCase):
         expected_exe = "INSERT INTO v4_reporting (`date`,`line_item_id`,`campaign_id`,`creative_id`,`imps`,`clicks`,`media_cost`,`adx_spend`) VALUES ('2014-07-09 00:00:00',1037447,3657781,14654391,200,0,0.330013,0.13152) ON DUPLICATE KEY UPDATE v4_reporting.`date` = VALUES(v4_reporting.`date`),v4_reporting.`line_item_id` = VALUES(v4_reporting.`line_item_id`),v4_reporting.`campaign_id` = VALUES(v4_reporting.`campaign_id`),v4_reporting.`creative_id` = VALUES(v4_reporting.`creative_id`),v4_reporting.`imps` = VALUES(v4_reporting.`imps`),v4_reporting.`clicks` = VALUES(v4_reporting.`clicks`),v4_reporting.`media_cost` = VALUES(v4_reporting.`media_cost`),v4_reporting.`adx_spend` = VALUES(v4_reporting.`adx_spend`)"
         resp_exe = res.get('_last_executed').replace("\n","")
         self.assertEqual(expected_exe, resp_exe)
+
+    def test_get_path(self):
+        d = dict(name='domain', group='advertiser,domain,line_item',
+                 start_date='2014-08-13 10:00:00', end_date='2014-08-13 11:00:00')
+        resp = get_path(**d)
+        expec = '/tmp/domain_advertiser,domain,line_item2014-08-13-10_2014-08-13-11.csv'
+        self.assertEqual(expec, resp)
+
+    def test_get_dataframes_success(self):
+        path = _testfile_path('mocked_response.txt')
+        mocked_resp = open(path, 'r').read()
+        expected = pd.read_csv(path)
+        with mock.patch.object(ReportBase, "_get_resp_helper") as m:
+            with mock.patch.object(ReportBase, "_get_form_helper") as h:
+                h.return_value = ''
+                m.return_value = mocked_resp
+                resp = ReportBase()._get_dataframe()
+                self.assertTrue(expected.equals(resp))
+
+    def test_get_dataframes_failure(self):
+        with mock.patch.object(ReportBase, "_get_resp_helper") as m:
+            with mock.patch.object(ReportBase, "_get_form_helper") as h:
+                h.return_value = ''
+                m.side_effect = ValueError('blow')
+                resp = ReportBase()._get_dataframe()
+                expected = empty_frame()
+                self.assertTrue(expected.equals(resp))
+
+    def test_get_report_obj(self):
+        obj = get_report_obj('domain', self.db)
+        expected = obj._name
+        self.assertEqual(expected, 'domain')
+
+    def test_get_db(self):
+        pass
+
+    def test_find_insert_report(self):
+        pass
