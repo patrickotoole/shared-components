7b7707c0df9569a8c327c0953e3c1529adbb6bdf
rickotoole <rick@rockerbox.com>
2014-08-27 23:15:02 +0000

    reorganized advertiser pixel to stub out for clients

--- metrics/handlers/reporting/pixel.py
+++ metrics/handlers/reporting/pixel.py
@@ -4,12 +4,11 @@ import StringIO
 import json
 
 from twisted.internet import defer
-
-from base import BaseHandler
+from ..base import BaseHandler
 from lib.helpers import *
 from lib.hive.helpers import run_hive_session_deferred
 
-MYSQL_QUERY="select advertiser_name, pixel_source_name, external_advertiser_id, deleted, active, min_report_date from advertiser where not pixel_source_name = 'None'"
+Q = "select pixel_source_name from advertiser where external_advertiser_id = %s"
 
 HIVE_QUERY='select date, hour, sum(num_imps) as imps, sum(num_conv) as conv, sum(num_rbox_imps) as rbox_imp, sum(num_rbox_conv) as rbox_conv from agg_pixel where source = "{}" and date >= "14-07-01" group by date, hour'
 
@@ -19,12 +18,8 @@ class PixelBaseHandler(BaseHandler):
         self.api = api
         self.hive = hive
 
-class RockerboxPixelHandler(PixelBaseHandler):
-    def get(self):
-        pixel_source_names = self.db.select_dataframe(MYSQL_QUERY)
-        self.render("analysis/_pixel.html", data=pixel_source_names.to_html())
-    
 class PixelAdvertiserHandler(PixelBaseHandler):
+
     @decorators.formattable
     def get_content(self, data):
         if not self.get_argument("format", False):
@@ -37,12 +32,26 @@ class PixelAdvertiserHandler(PixelBaseHandler):
 
     @defer.inlineCallbacks
     def get_data(self, query):
-        result = yield run_hive_session_deferred(self.hive, ["set shark.map.tasks=32", "set mapred.reduce.tasks=3",query])
+        q = [
+            "set shark.map.tasks=32", 
+            "set mapred.reduce.tasks=3",
+            query
+        ]
+        result = yield run_hive_session_deferred(self.hive, q)
         df = pd.DataFrame(result)
         self.get_content(df)
 
+    
+
     @tornado.web.asynchronous
-    def get(self, pixel_source_name):
+    def get(self):
+        advertiser_id = self.get_argument(
+            "advertiser_id",
+            self.current_advertiser
+        )
+        query = Q % advertiser_id
+        pixel_source_name = self.db.select_dataframe(query).values[0][0]
+
         query = HIVE_QUERY.format(pixel_source_name)
         self.get_data(query)
         
