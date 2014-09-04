0d6b2dfdd47e019aebeb326be9694512829b6d70
willwest <wmw71190@gmail.com>
2014-09-04 00:12:59 +0000

    Adapted API and interface to use new batch_request table

--- metrics/handlers/admin/scripts/batch.py
+++ metrics/handlers/admin/scripts/batch.py
@@ -26,37 +26,47 @@ class BatchRequestsHandler(BatchRequestBase):
         activate_request = self.get_argument("activate_request", default=False)
         
         if deactivate_request:
-            query = "UPDATE batch_domain_segments SET active=0 WHERE id={}".format(deactivate_request)    
+            query = "UPDATE batch_request SET active=0 WHERE id={}".format(deactivate_request)
             self.db.execute(query)
             self.db.commit()
             self.redirect("/admin/batch_requests")
         elif activate_request:
-            query = "UPDATE batch_domain_segments SET active=1 WHERE id={}".format(activate_request)
+            query = "UPDATE batch_request SET active=1 WHERE id={}".format(activate_request)
             self.db.execute(query)
             self.db.commit()
             self.redirect("/admin/batch_requests")
         else:
-            requests = self.db.select_dataframe("select * from batch_domain_segments")
+            requests = self.db.select_dataframe("select * from batch_request")
             self.render("../templates/_batch_requests.html", data=requests.to_html(classes=["dataframe"]))
         
         
 class BatchRequestHandler(BatchRequestBase):
-#    @decorators.formattable
     def get(self):
-        segments = self.pull_segments()        
+        segments = self.pull_segments()
+        
         self.render("../templates/_batch_request.html", segments=segments)
 
     def post(self):
-        segment = self.get_argument("segment")
+        segment = self.get_argument("segment", False)
+        hive_query = self.get_argument("hive_query", False)
         expiration = self.get_argument("expiration")
         target_segment = self.get_argument("target_segment")
         target_window = self.get_argument("target_window")
         active = self.get_argument("active")
+        owner = self.get_argument("owner")
+        comment = self.get_argument("comment")
 
-        log = self.db.select_dataframe("select distinct log from domain_list where segment='{}'".format(segment))['log'].tolist()[0]
+        if (segment and hive_query) or not (segment or hive_query):
+            raise StandardError("Missing an argument")
 
-        query = "INSERT INTO batch_domain_segments (log, segment, target_segment, target_window, expiration, active) VALUES ('{}', '{}', '{}', {}, {}, {});".format(log, segment, target_segment, target_window, expiration, active)
-
+        if segment:
+            # I don't think we need this any more, but not positive, so leaving
+            # it for now
+            #log = self.db.select_dataframe("SELECT DISTINCT log FROM domain_list WHERE segment='{}'".format(segment))['log'].tolist()[0]
+            query = "INSERT INTO batch_request (type, content, owner, target_segment, target_window, expiration, active, comment) VALUES ('{}','{}', '{}', '{}', {}, {}, {}, '{}');".format('domain_list', segment, owner, target_segment, target_window, expiration, active, comment)
+        else:
+            query = "INSERT INTO batch_request (type, content, owner, target_segment, target_window, expiration, active, comment) VALUES ('{}','{}', '{}', '{}', {}, {}, {}, '{}');".format('hive_query', hive_query, owner, target_segment, target_window, expiration, active, comment)
+        
         self.db.execute(query)
         self.db.commit()
 
