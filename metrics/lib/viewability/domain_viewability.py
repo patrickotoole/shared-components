d432da2a31f6045063b0d089f9debccd05ca77f7
rickotoole <rick@rockerbox.com>
2014-10-02 20:21:08 +0000

    added ref logging to viewability

--- metrics/lib/viewability/domain_viewability.py
+++ metrics/lib/viewability/domain_viewability.py
@@ -1,11 +1,15 @@
 import logging
+import ujson
 
 RB_API_BASE = "http://portal.getrockerbox.com:8080/admin/advertiser/viewable/reporting"
-URL = RB_API_BASE + "?meta=domain_list&include=date,campaign&type=baublebar_womens_interest&date=%s&format=csv&campaign=%s" 
+URL = RB_API_BASE + "?meta=domain_list&include=date,campaign&type=%s&date=%s&format=csv&campaign=%s" 
+
+LOGGING_SQL = "INSERT INTO domain_list_change_ref %(fields)s VALUES %(values)s"
 
 class DomainAPI(object):
-    def __init__(self,api):
+    def __init__(self,api,db):
         self.api = api
+        self.db = db
 
     def pull_campaign_ids(self,line_item_id):
         logging.info("AppNexus API line-item request: %s" % line_item_id)
@@ -17,38 +21,62 @@ class DomainAPI(object):
         logging.info("AppNexus API campaigns received: %s" % campaign_id_list)
         return campaign_id_list
 
-    def update_domain_list(self,domain_list_id,domains):
-        import ujson
+    def get_existing_domains(self,domain_list_id):
         response = self.api.get("/domain-list?id=%s" % domain_list_id)
         existing_domains = response.json["response"]["domain-list"]["domains"]
+        return existing_domains
 
-        domains = list(set(existing_domains + list(domains.index)))
+    def log_domain_change(self,new_domain_df):
+        
+        records = new_domain_df.to_records()
+        columns = tuple(["domain"] + list(new_domain_df.columns))
+        values  = ", ".join(map(str,records))
+
+        if len(records):
+            params = {
+                "fields": str(columns).replace("'","`"),
+                "values": values
+            }
+            self.db.execute(LOGGING_SQL % params)
+            logging.info("Adding domains: %s" % list(new_domain_df.index))
+        
+
+    def update_domain_list(self,domains,ext_domain_list_id,domain_list,action):
+
+        domains["external_domain_list_id"] = ext_domain_list_id
+        domains["domain_list"] = domain_list
+        domains["action"] = action
+        
+        existing_domains = self.get_existing_domains(ext_domain_list_id)
+        all_domains = list(set(existing_domains + list(domains.index)))
+        new_domains = [d for d in all_domains if d not in existing_domains]
+
+        self.log_domain_change(domains.ix[new_domains])
+
         logging.info("Modifying domain list %s: %s %s" % 
-            (domain_list_id,len(existing_domains),len(domains))
+            (ext_domain_list_id,len(existing_domains),len(all_domains))
         )
 
-        obj = {"domain-list":{"domains":domains}}
-        put_response = self.api.put("/domain-list?id=%s" % domain_list_id, ujson.dumps(obj))
+        obj = {"domain-list":{"domains":all_domains}}
+        put_response = self.api.put("/domain-list?id=%s" % ext_domain_list_id, ujson.dumps(obj))
 
-    def pull_viewability_csv(self,duration="past_month"):
-        # use the API and pull the CSV
+    def pull_viewability_csv(self,domain_list,duration="past_month"):
         import requests
         campaign_ids = self.campaign_ids
         
         campaign_string = ",".join(map(str,campaign_ids))
-        compiled_url = URL % (duration,campaign_string)
-        logging.info("Rockerbox API csv request for (%s): %s" % (duration,campaign_string)) 
+        compiled_url = URL % (domain_list,duration,campaign_string)
+        logging.info("Rockerbox API csv request for %s (%s): %s" % (domain_list,duration,campaign_string)) 
 
         rq = requests.get(compiled_url)
         logging.info("Rockerbox API csv lines received: %s" % rq.text.count("\n"))  
 
         return rq.text.replace("<pre>","").replace("</pre>","")
      
-    def get_viewability_df(self):
-        # convert the csv to a df
+    def get_viewability_df(self,domain_list):
         import pandas
         import StringIO
-        csv = self.pull_viewability_csv()
+        csv = self.pull_viewability_csv(domain_list)
         s = StringIO.StringIO(csv)
         df = pandas.read_csv(s,index_col=0)
  
@@ -56,8 +84,9 @@ class DomainAPI(object):
 
 
 class DomainAnalysis(DomainAPI):
-    def __init__(self,api,**obj):
+    def __init__(self,api,db,**obj):
         self.api = api
+        self.db = db
         self.__dict__.update(**obj)
 
         self._whitelist = None
@@ -125,7 +154,7 @@ class DomainAnalysis(DomainAPI):
         return self._greylist
     
     def get_viewability_report(self):
-        df = self.get_viewability_df()
+        df = self.get_viewability_df(self.domain_list_id)
         return self.calc_percent_visible(df)
 
     def get_greylist(self):
@@ -153,24 +182,29 @@ class DomainAnalysis(DomainAPI):
 
     def push_whitelist(self):
         whitelist = self.whitelist
-        self.update_domain_list(self.white_list_id, whitelist)
+        self.update_domain_list(whitelist,self.white_list_id, self.domain_list_id, "approve")
 
     def push_blacklist(self):
         blacklist = self.blacklist
-        self.update_domain_list(self.black_list_id,blacklist) 
+        self.update_domain_list(blacklist,self.black_list_id, self.domain_list_id, "ban") 
 
 def main():
     from lib.report.utils.loggingutils import basicConfig 
     from link import lnk
+    import pandas as pd
+    pd.set_option('display.max_columns', 100)
+    pd.set_option('display.width', 100)
+
     basicConfig(options={})
-    dlv = lnk.dbs.mysql.select_dataframe("select * from domain_list_viewability")
+    dlv = lnk.dbs.rockerbox.select_dataframe("select * from domain_list_viewability")
     api = lnk.api.console
+    reporting_db = lnk.dbs.reporting
 
     for obj in dlv.iterrows():
-        va = DomainAnalysis(api,**obj[1].to_dict())
+        va = DomainAnalysis(api,reporting_db,**obj[1].to_dict())
         va.push_whitelist()
         va.push_blacklist()
-        print va.greylist[va.greylist.served > 500].sort_index(by="served")
+        #print va.greylist[va.greylist.served > 500].sort_index(by="served")
 
 if __name__ == "__main__":
     main()
