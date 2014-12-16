6beb87a5e5783ab2094f67a26531be2f4834bea6
Lee Yanco <lee@rockerbox.com>
2014-12-16 19:25:02 +0000

    removed /intraweek call from all but reporting and intraweek itself

--- metrics/static/js/mixpanel.js
+++ metrics/static/js/mixpanel.js
@@ -1,14 +1,14 @@
 function addMixpanelEvent(advertiserID, userID, eventName) {
   if (userID.indexOf("a_") !== 0) {
-    $.getJSON("/intraweek?format=json&ioinfo=" + advertiserID, function(data){
+    $.getJSON("/admin/api?table=advertiser&external_advertiser_id=" + advertiserID, function(data){
       if (userID.indexOf("a_") === 0) {
         userID = "admin";
       }
         mixpanel.identify(advertiserID.toString() + "_" + userID);
         mixpanel.people.set({
-            "$name": data[0].advertiser + "_" + userID,
+            "$name": data[0].advertiser_name + "_" + userID,
             "$last_login": new Date(),
-            "Advertiser": data[0].advertiser
+            "Advertiser": data[0].advertiser_name
         });
         mixpanel.track(eventName);
     });
