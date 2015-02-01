c1d2618eab6fab287a4d19b70bff85cae15e9230
rick <rick@rockerbox.com>
2015-02-01 02:08:27 +0000

    adding custom mysql wrapper to correctly handled threaded environment and reduce shared self._wrapped variable

---
+++ metrics/lib/custom_wrappers/mysql_wrapper.py
@@ -0,0 +1,91 @@
+from link.wrappers.dbwrappers import DBConnectionWrapper
+import link.wrappers.defaults as defaults
+
+class MysqlDB(DBConnectionWrapper):
+
+    def __init__(self, wrap_name=None, user=None, password=None,
+            host=None, database=None, port=defaults.MYSQL_DEFAULT_PORT,
+            autocommit=True):
+        """
+        A connection for a Mysql Database.  Requires that
+        MySQLdb is installed
+
+        :param user: your user name for that database 
+        :param password: Your password to the database
+        :param host: host name or ip of the database server
+        :param database: name of the database on that server 
+        """
+        self.user = user
+        self.password = password
+        self.host = host
+        self.database = database
+        self.port=port
+        self.autocommit = autocommit
+        super(MysqlDB, self).__init__(wrap_name=wrap_name)
+
+    def execute(self, query, args = ()):
+        """
+        Creates a cursor and executes the query for you
+        """
+        import MySQLdb
+        try:
+            try:
+                self._wrapped.close()
+            except:
+                pass
+            _wrapped = self.create_connection()
+            with _wrapped:
+                cursor = _wrapped.cursor()
+                return self.CURSOR_WRAPPER(cursor, query, args=args)()
+        except MySQLdb.OperationalError, e:
+            if e[0] == 2006:
+                self._wrapped.close()
+                self._wrapped = self.create_connection()
+                cursor = self._wrapped.cursor()
+                return self.CURSOR_WRAPPER(cursor, query, args=args)()
+
+        return
+
+    def create_connection(self):
+        """
+        Override the create_connection from the DbConnectionWrapper
+        class which get's called in it's initializer
+        """
+        import MySQLdb.connections
+        import MySQLdb.converters
+        import MySQLdb
+
+        # make it so that it uses floats instead of those Decimal objects
+        # these are really slow when trying to load into numpy arrays and 
+        # into pandas
+        conv = MySQLdb.converters.conversions.copy()
+        conv[MySQLdb.constants.FIELD_TYPE.DECIMAL] = float
+        conv[MySQLdb.constants.FIELD_TYPE.NEWDECIMAL] = float
+        conn = MySQLdb.connect(host=self.host, user=self.user,
+                               db=self.database, passwd=self.password,
+                               conv=conv, port=self.port)
+        if self.autocommit:
+            conn.autocommit(True)
+        return conn
+
+    def use(self, database):
+        return self.select('use %s' % database).data
+
+    def databases(self):
+        return self.select('show databases').data
+
+    def tables(self):
+        return self.select('show tables').data
+
+    def now(self):
+        # not sure that the [0][0] will always be true...but it works now
+        return self.select('select now()').data[0][0]
+
+    @property
+    def command(self):
+        """
+        Here is the command for doing the mysql command
+        """
+        return  'mysql -A -u %s -p%s -h %s %s' % (self.user, self.password,
+                                                     self.host, self.database)
+
