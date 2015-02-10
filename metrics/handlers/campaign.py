2dcfc5a4a42e5a5b99a9a71ae09d907d67a6bd8e
rick <rick@rockerbox.com>
2015-02-10 17:19:13 +0000

    added delete functionality to UI for Yoshi campaign mgmt resolves #141

--- metrics/handlers/campaign.py
+++ metrics/handlers/campaign.py
@@ -135,7 +135,10 @@ class YoshiCampaignHandler(BaseHandler):
 
         df = pandas.DataFrame(campaigns)
         if len(df) > 0 and self.get_argument("show_all",False) == False:
+            if not self.get_argument("show_deleted",False):
+                df = df[df.comments != "deleted"]
             df = df[['id','name','base_bid','daily_budget','state']]
+            
 
         if self.get_argument("format",False) == False:     
             pass
