9287978944cac214277ac59ecfe89150a6556c16
rick <rick@rockerbox.com>
2015-03-10 22:05:07 +0000

    upgrading yoshi viewability to include size

--- metrics/handlers/viewable.py
+++ metrics/handlers/viewable.py
@@ -7,7 +7,7 @@ import logging
 from base import BaseHandler
 from twisted.internet import defer
 from lib.helpers import decorators
-QUERY = "SELECT * from rockerbox.tag_viewability "
+QUERY = "SELECT * from rockerbox.tag_size_viewability "
 
 class ViewabilityHandler(BaseHandler):
     def initialize(self, db=None, cassandra=None, **kwargs):
@@ -15,19 +15,27 @@ class ViewabilityHandler(BaseHandler):
         self.cassandra = cassandra
 
     @decorators.deferred
-    def defer_get_viewablity(self,tag_ids,domains):
-        WHERE = "where "
+    def defer_get_viewablity(self,tag_ids,sizes,domains):
+        where = []
         if len(tag_ids):
-            WHERE += (" tag_id in (" + ",".join(tag_ids) + ")")
+            where.append(" tag_id in (" + ",".join(tag_ids) + ")")
 
         if len(domains):
-            WHERE += (" and domain in ('" + "','".join(domains) + "')")
+            where.append(" domain in ('" + "','".join(domains) + "')")
 
-        return self.cassandra.select_dataframe(QUERY + WHERE )
+
+        WHERE = "where " + " and ".join(where)
+        logging.info(QUERY + WHERE)
+        df = self.cassandra.select_dataframe(QUERY + WHERE )
+
+        if len(sizes):
+            return df[df['size'].isin(sizes)]
+
+        return df
 
     @defer.inlineCallbacks
-    def get_viewability(self,tag_ids,domains):
-        viewability = yield self.defer_get_viewablity(tag_ids,domains)
+    def get_viewability(self,tag_ids,sizes,domains):
+        viewability = yield self.defer_get_viewablity(tag_ids,sizes,domains)
         viewability['percent_viewable'] = viewability['num_visible']/viewability['num_loaded']
 
         viewability_list = viewability.T.to_dict().values()
@@ -37,4 +45,4 @@ class ViewabilityHandler(BaseHandler):
     #@tornado.web.authenticated
     @tornado.web.asynchronous
     def get(self):
-        self.get_viewability(self.get_arguments("tag_id"),self.get_arguments("domain",[]))
+        self.get_viewability(self.get_arguments("tag_id"),self.get_arguments("size",[]),self.get_arguments("domain",[]))
