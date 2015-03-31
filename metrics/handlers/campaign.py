fe4e27f6b71b447ff77d88acd32ba77092768822
rick <rick@rockerbox.com>
2015-03-31 16:32:24 +0000

    adding more tests around the campaign API fro yoshi

--- metrics/handlers/campaign.py
+++ metrics/handlers/campaign.py
@@ -13,11 +13,65 @@ from lib.helpers import *
 LINE_ITEM_QUERY = "select * from advertiser_line_item where line_item_name like '%%Yoshi%%' and %s "
 CAMPAIGN_BUCKET = "INSERT INTO campaign_bucket (external_advertiser_id, campaign_id, bucket_name) VALUES (%s, %s, '%s')"
 
-class YoshiCampaignHandler(BaseHandler):
+def make_name(domain_targets, sizes):
+    base = "Yoshi"
+    domains = ",".join([d['domain'] for d in domain_targets[:4]]) 
+    domains += (", and %s more" % len(domain_targets)) if len(domain_targets) > 4 else ""
+    sizes = ",".join(sizes)
+  
+    return base + " | " + domains + " | " + sizes
 
-    def initialize(self, db=None, api=None, **kwargs):
-        self.db = db 
-        self.api = api
+class CampaignHandler(BaseHandler):
+
+    @decorators.deferred
+    def get_line_item_id(self,advertiser_id):
+        where = (" external_advertiser_id = %s" % advertiser_id)
+        line_item_df = self.db.select_dataframe(LINE_ITEM_QUERY % where)
+        return line_item_df.line_item_id[0] 
+
+    @decorators.deferred
+    def defer_get_campaigns(self,advertiser_id,line_item_id):
+        URL = "/campaign?advertiser_id=%s&line_item_id=%s" % (advertiser_id,line_item_id)
+        data = self.api.get(URL)
+        return data.json['response']['campaigns']
+
+    @decorators.deferred
+    def defer_modify_campaign(self,advertiser_id,campaign_id,campaign):
+        URL = "/campaign?advertiser_id=%s&id=%s" % (advertiser_id,campaign_id)
+        data = self.api.put(URL,data=ujson.dumps({"campaign":campaign}))
+        return data.json['response']
+     
+    @decorators.deferred
+    def set_campaign_profile_id(self,advertiser_id,campaign_id,profile_id):
+        URL = "/campaign?advertiser_id=%s&id=%s" % (advertiser_id,campaign_id) 
+
+        to_post = {"campaign": {"id": campaign_id, "profile_id": profile_id }}
+        data = self.api.put(URL,data=ujson.dumps(to_post))
+        return data.json
+
+
+    @defer.inlineCallbacks
+    def modify_campaign(self,advertiser_id,campaign_id,campaign):
+        campaign = yield self.defer_modify_campaign(advertiser_id,campaign_id,campaign)
+        self.get_content(pandas.DataFrame([campaign]),advertiser_id)
+
+    @defer.inlineCallbacks
+    def get_campaigns(self,advertiser_id):
+
+        line_item_id = yield self.get_line_item_id(advertiser_id)
+        campaigns = yield self.defer_get_campaigns(advertiser_id,line_item_id)
+
+        df = pandas.DataFrame(campaigns)
+        if len(df) > 0 and self.get_argument("show_all",False) == False:
+            if not self.get_argument("show_deleted",False):
+                df = df[df.comments != "deleted"]
+            df = df[['id','name','base_bid','daily_budget','state','creatives']]
+            
+
+        if self.get_argument("format",False) == False:     
+            pass
+
+        self.get_content(df,advertiser_id)
 
     @decorators.formattable
     def get_content(self,data,advertiser_id):
@@ -28,34 +82,12 @@ class YoshiCampaignHandler(BaseHandler):
 
         yield default, (data,)
 
-    @decorators.deferred
-    def get_line_item_id(self,advertiser_id):
-        where = (" external_advertiser_id = %s" % advertiser_id)
-        line_item_df = self.db.select_dataframe(LINE_ITEM_QUERY % where)
-        return line_item_df.line_item_id[0]
 
+class AdminCampaignHandler(CampaignHandler):
 
-    @decorators.deferred
-    def create_campaign(self,line_item_id,advertiser_id,name="test",bid_price=1,creatives=[]):
-        data = {
-            "campaign" : {
-                "name": name,
-                "state": "inactive",
-                "advertiser_id":  advertiser_id,
-                "line_item_id": line_item_id,
-                "lifetime_budget": 5000,
-                "daily_budget": 50,
-                "inventory_type": "real_time",
-                "cpm_bid_type": "base",
-                "base_bid": bid_price,
-                "creatives": creatives
-            }
-        }
-        logging.info(data)
-        URL = "/campaign?advertiser_id=%s&line_item=%s" % (advertiser_id,line_item_id) 
-        data = self.api.post(URL,data=ujson.dumps(data))
-        logging.info(data.content)
-        return data.json['response']['campaign']
+    def initialize(self, db=None, api=None, **kwargs):
+        self.db = db 
+        self.api = api
 
     @decorators.deferred
     def create_admin_campaign(self,line_item_id,advertiser_id,name="test",bid_price=2,creatives=[]):
@@ -78,23 +110,6 @@ class YoshiCampaignHandler(BaseHandler):
         data = self.api.post(URL,data=ujson.dumps(data))
         logging.info(data.content)
         return data.json['response']['campaign'] 
-
-    @decorators.deferred
-    def create_profile(self,advertiser_id,campaign_id,profile):
-        URL = "/profile?advertiser_id=%s&campaign_id=%s" % (advertiser_id,campaign_id) 
-
-        data = {
-            "profile" : profile
-        }
-        data['profile']['country_action'] = "include"
-        data['profile']['country_targets'] = [{"country": "US"}]
-        data['profile']['device_type_targets'] = ["phone"]
-        data['profile']['supply_type_targets'] = ["mobile_app", "mobile_web"]
-        data['profile']['max_day_imps'] = 5
-        data['profile']['max_lifetime_imps'] = 15
-
-        data = self.api.post(URL,data=ujson.dumps(data))
-        return data.json['response']['profile']
 
     @decorators.deferred
     def create_admin_profile(self,advertiser_id,campaign_id,profile):
@@ -129,86 +144,104 @@ class YoshiCampaignHandler(BaseHandler):
         data = self.api.post(URL,data=ujson.dumps(data))
         return data.json['response']['profile']
 
-    @decorators.deferred
-    def set_campaign_profile_id(self,advertiser_id,campaign_id,profile_id):
-        URL = "/campaign?advertiser_id=%s&id=%s" % (advertiser_id,campaign_id) 
 
-        data = {
-            "campaign" : {
-                "id": campaign_id,
-                "profile_id": profile_id
-            }
-        }
+    @defer.inlineCallbacks  
+    def admin_create(self, name, line_item_id, advertiser_id, price, campaign, profile): 
+        creatives = campaign.get("creatives",[])
 
-        data = self.api.put(URL,data=ujson.dumps(data))
-        return data.json
-     
-    @decorators.deferred
-    def defer_get_campaigns(self,advertiser_id,line_item_id):
-        URL = "/campaign?advertiser_id=%s&line_item_id=%s" % (advertiser_id,line_item_id)
-        data = self.api.get(URL)
-        return data.json['response']['campaigns']
-
-    @decorators.deferred
-    def defer_modify_campaign(self,advertiser_id,campaign_id,campaign):
-        URL = "/campaign?advertiser_id=%s&id=%s" % (advertiser_id,campaign_id)
-        data = self.api.put(URL,data=ujson.dumps({"campaign":campaign}))
-        return data.json['response']
-     
-
-    @defer.inlineCallbacks 
-    def make_campaign(self,advertiser_id,profile,details,campaign):
-        name = "Yoshi"
-        name += " | " + ",".join([d['domain'] for d in profile['domain_targets'][:4]]) + (", and %s more" % len(profile['domain_targets']) if len(profile['domain_targets']) > 4 else "")
-        name += " | " + ",".join(details['sizes'])
-
-        price = 1 # need to get this from details
-
-        line_item_id = yield self.get_line_item_id(advertiser_id) 
-        if details.get('username','').startswith("a_"):
-            campaign = yield self.create_admin_campaign(line_item_id,advertiser_id,name,price,campaign.get("creatives",[])) 
-            profile = yield self.create_admin_profile(advertiser_id,campaign['id'],profile)
-        else:
-            campaign = yield self.create_campaign(line_item_id,advertiser_id,name,price,campaign.get("creatives",[]))
-            profile = yield self.create_profile(advertiser_id,campaign['id'],profile)
-
+        campaign = yield self.create_admin_campaign(line_item_id,advertiser_id,name,price,creatives)
+        profile = yield self.create_admin_profile(advertiser_id,campaign['id'],profile)
         campaign_with_profile = yield self.set_campaign_profile_id(advertiser_id,campaign['id'],profile['id'])
 
-        obj = {
-            "campaign":campaign_with_profile,
-            "profile":profile
+        obj = { "campaign":campaign_with_profile, "profile":profile }
+        df = pandas.DataFrame(obj)
+
+        self.write(ujson.dumps(obj))
+        self.finish()  
+
+
+
+class UserCampaignHandler(CampaignHandler):
+
+    def initialize(self, db=None, api=None, **kwargs):
+        self.db = db 
+        self.api = api 
+
+    @decorators.deferred
+    def create_campaign(self,line_item_id,advertiser_id,name="test",bid_price=1,creatives=[]):
+        data = {
+            "campaign" : {
+                "name": name,
+                "state": "inactive",
+                "advertiser_id":  advertiser_id,
+                "line_item_id": line_item_id,
+                "lifetime_budget": 5000,
+                "daily_budget": 50,
+                "inventory_type": "real_time",
+                "cpm_bid_type": "base",
+                "base_bid": bid_price,
+                "creatives": creatives
+            }
         }
+        logging.info(data)
+        URL = "/campaign?advertiser_id=%s&line_item=%s" % (advertiser_id,line_item_id) 
+        data = self.api.post(URL,data=ujson.dumps(data))
+        logging.info(data.content)
+        return data.json['response']['campaign']
+
+    @decorators.deferred
+    def create_profile(self,advertiser_id,campaign_id,profile):
+        URL = "/profile?advertiser_id=%s&campaign_id=%s" % (advertiser_id,campaign_id) 
+
+        data = {
+            "profile" : profile
+        }
+        data['profile']['country_action'] = "include"
+        data['profile']['country_targets'] = [{"country": "US"}]
+        data['profile']['device_type_targets'] = ["phone"]
+        data['profile']['supply_type_targets'] = ["mobile_app", "mobile_web"]
+        data['profile']['max_day_imps'] = 5
+        data['profile']['max_lifetime_imps'] = 15
+
+        data = self.api.post(URL,data=ujson.dumps(data))
+        return data.json['response']['profile']
+
+    @defer.inlineCallbacks 
+    def create(self, name, line_item_id, advertiser_id, price, campaign, profile):
+
+        #import ipdb; ipdb.set_trace()
+        campaign = yield self.create_campaign(line_item_id,advertiser_id,name,price,campaign.get("creatives",[]))
+        profile = yield self.create_profile(advertiser_id,campaign['id'],profile)
+        campaign_with_profile = yield self.set_campaign_profile_id(advertiser_id,campaign['id'],profile['id'])
+
+        obj = { "campaign":campaign_with_profile, "profile":profile }
         df = pandas.DataFrame(obj)
         
-        if not details.get('username','').startswith("a_"):
-            self.db.execute(CAMPAIGN_BUCKET % (advertiser_id, campaign['id'], name))
+        self.db.execute(CAMPAIGN_BUCKET % (advertiser_id, campaign['id'], name))
+
         self.write(ujson.dumps(obj))
-        self.finish()
-        #self.get_content(df,advertiser_id)
-       
-    @defer.inlineCallbacks
-    def modify_campaign(self,advertiser_id,campaign_id,campaign):
-        campaign = yield self.defer_modify_campaign(advertiser_id,campaign_id,campaign)
-        self.get_content(pandas.DataFrame([campaign]),advertiser_id)
+        self.finish() 
 
-    @defer.inlineCallbacks
-    def get_campaigns(self,advertiser_id):
 
-        line_item_id = yield self.get_line_item_id(advertiser_id)
-        campaigns = yield self.defer_get_campaigns(advertiser_id,line_item_id)
+    
 
-        df = pandas.DataFrame(campaigns)
-        if len(df) > 0 and self.get_argument("show_all",False) == False:
-            if not self.get_argument("show_deleted",False):
-                df = df[df.comments != "deleted"]
-            df = df[['id','name','base_bid','daily_budget','state','creatives']]
-            
+class YoshiCampaignHandler(AdminCampaignHandler,UserCampaignHandler):
 
-        if self.get_argument("format",False) == False:     
-            pass
-            #self.write("<h4>Yoshi campaigns</h4>")
+    def initialize(self, db=None, api=None, **kwargs):
+        self.db = db 
+        self.api = api
+        
+    @defer.inlineCallbacks  
+    def make_campaign(self,advertiser_id,profile,details,campaign):
 
-        self.get_content(df,advertiser_id)
+        price = 1
+        name = make_name(profile['domain_targets'], details['sizes'])
+        is_admin = details.get('username','').startswith("a_")
+
+        line_item_id = yield self.get_line_item_id(advertiser_id) 
+
+        create_func = self.admin_create if is_admin else self.create
+        create_func(name, line_item_id, advertiser_id, price, campaign, profile)
 
     @tornado.web.authenticated
     @tornado.web.asynchronous
@@ -234,19 +267,21 @@ class YoshiCampaignHandler(BaseHandler):
         
         if advertiser_id and campaign_id and campaign:
             self.modify_campaign(advertiser_id,campaign_id,campaign)
+        else:
+            self.finish()
 
 
     @tornado.web.asynchronous
     def post(self):
         advertiser_id = self.current_advertiser
         obj = ujson.loads(self.request.body)
-        print obj
         profile = obj.get('profile',False)
         details = obj.get('details',{})
         campaign = obj.get('campaign',{})
+
+        #import ipdb; ipdb.set_trace()
         if advertiser_id and profile is not False:
             self.make_campaign(advertiser_id,profile,details,campaign)
         else:
             self.write("need to be logging and supply a profile object in the json you post")
             self.finish()
-
--- metrics/tests/handlers/yoshi/test_campaign.py
+++ metrics/tests/handlers/yoshi/test_campaign.py
@@ -14,6 +14,7 @@ class CampaignTest(AsyncHTTPTestCase):
 
     def get_app(self):
         self.db = lnk.dbs.test
+        self.db.execute(FIXTURES.CREATE_BUCKET_TABLE)
 
         self.mock_api = mock.MagicMock()
 
@@ -21,15 +22,18 @@ class CampaignTest(AsyncHTTPTestCase):
         campaign.YoshiCampaignHandler.current_user = 1
 
         self.app = Application([
-            ('/', campaign.YoshiCampaignHandler, dict(reporting_db=self.db, api=self.mock_api)),
-            ('/(.*?)', campaign.YoshiCampaignHandler, dict(reporting_db=self.db, api=self.mock_api)) 
+            ('/', campaign.YoshiCampaignHandler, dict(db=self.db, api=self.mock_api)),
+            ('/(.*?)', campaign.YoshiCampaignHandler, dict(db=self.db, api=self.mock_api)) 
           ], cookie_secret="rickotoole"
         )
 
 
         return self.app
 
+
     def tearDown(self):
+        self.db.execute("DROP TABLE test.campaign_bucket")
+        
         pass
 
     @mock.patch.object(campaign.YoshiCampaignHandler, 'defer_get_campaigns', autospec=True) 
@@ -45,3 +49,89 @@ class CampaignTest(AsyncHTTPTestCase):
             set(ujson.loads(response)[0].keys()), 
             set([u'id',u'name',u'base_bid',u'daily_budget',u'state',u'creatives'])
         ) 
+
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'defer_modify_campaign', autospec=True) 
+    def test_update_campaign(self, mock_modified_campaign):
+        mock_modified_campaign.side_effect = lambda s, aid, cid, c: dict(c.items() + [("aid",aid), ("cid",cid)])
+        
+        response = self.fetch("/?id=1&format=json", method="PUT",body='{"campaign":{"a":"b"}}').body
+        self.assertEqual(ujson.dumps([{"a":"b","aid":1,"cid":"1"}]),response)
+
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'current_user', autospec=True)  
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'get_line_item_id', autospec=True) 
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'set_campaign_profile_id', autospec=True)  
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'create_admin_profile', autospec=True) 
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'create_admin_campaign', autospec=True) 
+    def test_post_admin_campaign(self, campaign, profile, set_profile, line_item, current_user): 
+
+        CID = 100
+        PID = 101
+
+        line_item.return_value = 1
+        campaign.side_effect = lambda s, lid, aid, n, bp, cr: {"id":CID,"advertiser_id":aid,"name":n, "creatives":cr}
+        profile.side_effect = lambda s, aid, cid, p: {"id":PID, "advertiser_id":aid, "campaign_id":cid}
+        set_profile.side_effect = lambda s, aid, cid, pid: {"id":cid, "profile_id":pid, "advertiser_id":aid}
+
+        to_post = { 
+            "campaign":{
+                "creatives": []
+            },
+            "profile":{
+                "domain_targets": [{"domain":"test_domain"}]
+            },
+            "details":{
+                "sizes": ["300x250"],
+                "username": "a_admin"
+            }
+        }
+
+        expect = {
+            u"profile": {u"advertiser_id":1,u"id":PID,u"campaign_id":CID},
+            u"campaign": {u"advertiser_id":1,u"profile_id":PID,u"id":CID}
+        }
+        
+        response = self.fetch("/?id=1&format=json", method="POST",body=ujson.dumps(to_post)).body
+        df = self.db.select_dataframe("SELECT * from campaign_bucket")
+
+        self.assertEqual(expect,ujson.loads(response))
+        self.assertEqual(len(df),0)
+
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'get_line_item_id', autospec=True) 
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'set_campaign_profile_id', autospec=True)  
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'create_profile', autospec=True) 
+    @mock.patch.object(campaign.YoshiCampaignHandler, 'create_campaign', autospec=True) 
+    def test_post_campaign(self, campaign, profile, set_profile, line_item):
+
+        CID = 100
+        PID = 101
+
+        line_item.return_value = 1
+        campaign.side_effect = lambda s, lid, aid, n, bp, cr: {"id":CID,"advertiser_id":aid,"name":n, "creatives":cr}
+        profile.side_effect = lambda s, aid, cid, p: {"id":PID, "advertiser_id":aid, "campaign_id":cid}
+        set_profile.side_effect = lambda s, aid, cid, pid: {"id":cid, "profile_id":pid, "advertiser_id":aid}
+
+        to_post = { 
+            "campaign":{
+                "creatives": []
+            },
+            "profile":{
+                "domain_targets": [{"domain":"test_domain"}]
+            },
+            "details":{
+                "sizes": ["300x250"] 
+            }
+        }
+
+        expect = {
+            u"profile": {u"advertiser_id":1,u"id":PID,u"campaign_id":CID},
+            u"campaign": {u"advertiser_id":1,u"profile_id":PID,u"id":CID}
+        }
+        
+        response = self.fetch("/?id=1&format=json", method="POST",body=ujson.dumps(to_post)).body
+        df = self.db.select_dataframe("SELECT * from campaign_bucket")
+        bucket_name = df.ix[0,"bucket_name"]
+        cid = df.ix[0,"campaign_id"] 
+
+        self.assertEqual(expect,ujson.loads(response))
+        self.assertEqual("Yoshi | test_domain | 300x250",bucket_name)
+        self.assertEqual(CID,cid)
