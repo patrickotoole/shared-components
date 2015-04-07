aca9bb1e40b279cad83b759d67462db20e87c0ba
willwest <wmw71190@gmail.com>
2015-04-07 21:49:01 +0000

    Adding content formatter for timeseries data

--- metrics/handlers/analytics/available.py
+++ metrics/handlers/analytics/available.py
@@ -7,6 +7,7 @@ import logging
 from ..base import BaseHandler
 from twisted.internet import defer
 from lib.helpers import decorators
+from lib.helpers import *
 
 QUERY = "SELECT * from rockerbox.domain_tag_imps "
 
@@ -14,6 +15,23 @@ class AvailabilityHandler(BaseHandler):
     def initialize(self, db=None, cassandra=None, **kwargs):
         self.db = db
         self.cassandra = cassandra
+
+    @decorators.formattable
+    def get_content(self, data):
+        def default(self, data):
+            df = Convert.df_to_json(data)
+            self.render("analysis/timeseries.html", data=df)
+        yield default, (data,)
+
+
+    @defer.inlineCallbacks
+    def get_availability(self, tag_id, sellers, sizes, domains):
+        df = yield self.defer_get_availability(tag_id, sellers, sizes, domains)
+        if len(df):
+            df['ecp'] = df.ecp / df.imps / 1000
+            df['eap'] = df.eap / df.imps / 1000
+
+        self.get_content(df)
 
     @decorators.deferred
     def defer_get_availability(self, tag_id, sellers, sizes, domains):
@@ -37,22 +55,17 @@ class AvailabilityHandler(BaseHandler):
 
         return df
 
-    @defer.inlineCallbacks
-    def get_availability(self, tag_id, sellers, sizes, domains):
-        df = yield self.defer_get_availability(tag_id, sellers, sizes, domains)
-        if len(df):
-            df['ecp'] = df.ecp / df.imps / 1000
-            df['eap'] = df.eap / df.imps / 1000
-
-        availability_list = df.T.to_dict().values()
-        self.write(ujson.dumps(availability_list))
-        self.finish()
 
     @tornado.web.asynchronous
     def get(self):
-        self.get_availability(
-            self.get_arguments("tag", []),
-            self.get_arguments("seller",[]),
-            self.get_arguments("size",[]),
-            self.get_arguments("domain",[])
-        )
+        formatted = self.get_argument("format", False)
+        
+        if formatted:
+            self.get_availability(
+                self.get_arguments("tag", []),
+                self.get_arguments("seller",[]),
+                self.get_arguments("size",[]),
+                self.get_arguments("domain",[])
+            )
+        else:
+            self.get_content(pandas.DataFrame())
