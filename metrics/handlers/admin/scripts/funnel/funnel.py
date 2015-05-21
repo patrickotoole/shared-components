4a9b554aa26c5f6f6d97e76d7461c1992d6dc99d
rick <rick@rockerbox.com>
2015-05-21 22:00:35 +0000

    reorganization of funnel to include a base and helper classes for clarity

--- metrics/handlers/admin/scripts/funnel/funnel.py
+++ metrics/handlers/admin/scripts/funnel/funnel.py
@@ -1,62 +1,12 @@
 import tornado.web
 import ujson
 import pandas
+
 from lib.helpers import Convert
+from MYSQL_FUNNEL import *
+from funnel_base import FunnelBase, FunnelHelpers
 
-GET = """
-SELECT  
-    a.funnel_id,
-    a.funnel_name,
-    a.owner,
-    a.pixel_source_name,
-    b.action_name,
-    b.operator,
-    c.order,
-    d.*
-FROM funnel a 
-    LEFT JOIN funnel_actions c ON a.funnel_id = c.funnel_id
-    LEFT JOIN action b ON b.action_id = c.action_id
-    LEFT JOIN action_patterns d ON b.action_id = d.action_id
-WHERE
-%(where)s
-"""
-
-GET_FUNNEL_ACTION = """
-SELECT * FROM funnel_actions
-WHERE funnel_id = %s
-"""
-
-INSERT_FUNNEL = """
-INSERT INTO funnel
-    (funnel_name, owner, pixel_source_name) 
-VALUES 
-    ("%(funnel_name)s", "%(owner)s", "%(advertiser)s")
-"""
-
-UPDATE_FUNNEL_ACTION = """
-UPDATE funnel_actions SET %(fields)s
-WHERE funnel_id = %(funnel_id)s and action_id = %(action_id)s
-"""
-
-DELETE_FUNNEL_ACTION = """
-DELETE FROM funnel_actions 
-WHERE funnel_id = %(funnel_id)s and action_id = %(action_id)s
-"""
- 
-
-UPDATE_FUNNEL = """
-UPDATE funnel SET %(fields)s
-WHERE funnel_id = %(funnel_id)s
-"""
-
-INSERT_FUNNEL_ACTIONS = """
-INSERT INTO funnel_actions
-    (funnel_id, action_id, `order`)
-VALUES
-    (%(funnel_id)s, %(action_id)s, %(order)s)
-"""
-
-class FunnelHandler(tornado.web.RequestHandler):
+class FunnelHandler(FunnelBase,FunnelHelpers):
 
     def initialize(self, db=None, **kwargs):
         self.db = db
@@ -68,55 +18,7 @@ class FunnelHandler(tornado.web.RequestHandler):
             "owner"
         ]
 
-    def get_all(self):
-        where = "1=1"
-        return self.db.select_dataframe(GET.format(where))       
-
-    def get_advertiser_funnels(self, advertiser):
-        where = "a.pixel_source_name = '{}'".format(advertiser)
-        Q = GET % {"where": where} 
-        return self.db.select_dataframe(Q)
-
-    def get_funnel_by_id(self, _id):
-        where = "a.funnel_id = '{}'".format(_id)
-        Q = GET % {"where": where} 
-        return self.db.select_dataframe(Q)
-     
-
-    def add_grouper_column(self,df,columns=["order","funnel_id"]):
-        # makes a grouper column because pandas #sucks
-
-        serieses = map(lambda col: df[col],columns)
-        df["+".join(columns)] = reduce(lambda s1,s2: s1.map(str) + s2.map(str),serieses)
-
-        return "+".join(columns)
-
-    def column_to_list(self,df,column="url_pattern",groupby=['order','funnel_id']):
-        # converts a column in a dataframe to a list by grouping
-        # final dataframe has same columns, but compressed along the groupby dimensions
-        # and a list for the compressed column
-
-        agg_dict = {col: max for col in df.columns}
-        agg_dict[column] = lambda x: list(x)
-
-        grouper_name = self.add_grouper_column(df,groupby)
-
-        grouped = df.groupby(grouper_name)
-        result = grouped.agg(agg_dict).reset_index(drop=True)
-
-        return result
-
-    def group_to_dict(self,columns,accepted=lambda x: True):
-        # made for use in grouped.apply(fn)
-        # converts a dataframe grouping to a dictionary
-        # final product of grouped.apply => series[dict]
-        
-        def g2d(df):
-            values = df[columns].T.to_dict().values()
-            return [v for v in values if accepted(v)]
-
-        return  g2d
-        
+      
 
     def get(self,*args):
         advertiser = self.get_argument("advertiser", False)
@@ -148,6 +50,8 @@ class FunnelHandler(tornado.web.RequestHandler):
         self.write(as_json)
         self.finish()
 
+    
+
     def make_to_update(self,body):
 
         obj = ujson.loads(body)
@@ -168,10 +72,7 @@ class FunnelHandler(tornado.web.RequestHandler):
                 "fields":funnel_fields
             }
             
-            try:
-                cur.execute(UPDATE_FUNNEL % to_update)
-            except Exception as e:
-                print e
+            cur.execute(UPDATE_FUNNEL % to_update)
 
             actions_df = pandas.DataFrame(obj["actions"])
             actions_df['order'] = actions_df.index + 1
@@ -182,7 +83,7 @@ class FunnelHandler(tornado.web.RequestHandler):
 
             merged = actions_df.merge(existing_df,how="outer",suffixes=("","_old"),on="action_id")
 
-            to_add = merged[merged['order_old'].isnull()]  
+            to_add = merged[merged['order_old'].isnull()] 
             to_remove = merged[merged['order'].isnull()]
 
             to_update = merged[
@@ -190,38 +91,11 @@ class FunnelHandler(tornado.web.RequestHandler):
                 ~(merged['order_old'].isnull()) &
                 ~(merged['order'].isnull())
             ]
-            funnel_action_excludes = ["funnel_id","action_id"]
 
-
-            for i,values in to_remove[["action_id","order"]].iterrows():
-                remove = values.to_dict()
-
-                cur.execute(DELETE_FUNNEL_ACTION % {
-                    "order": remove['order'],
-                    "action_id": remove['action_id'],
-                    "funnel_id": funnel_id
-                })
-
-            for i,values in to_add[["action_id","order"]].iterrows():
-                add = values.to_dict()
-
-                cur.execute(INSERT_FUNNEL_ACTIONS % {
-                    "order": add['order'],
-                    "action_id": add['action_id'],
-                    "funnel_id": funnel_id
-                })
-
-            for i,values in to_update[["action_id","order"]].iterrows():
-                items = values.to_dict().items()
-                action_fields = ", ".join(["`%s`=\"%s\"" % (i,j) for i,j in items if not (i in excludes)])
-                action_id = values.to_dict()['action_id']
-
-                cur.execute(UPDATE_FUNNEL_ACTION % {
-                    "fields": action_fields,
-                    "funnel_id": funnel_id,
-                    "action_id": action_id 
-                })
-
+            self.insert_funnel_actions(cur,to_add,funnel_id) 
+            self.delete_funnel_actions(cur,to_remove,funnel_id) 
+            self.update_funnel_actions(cur,to_update,funnel_id)
+            
             obj['advertiser'] = obj['pixel_source_name']
             del obj['pixel_source_name']                 
 
---
+++ metrics/handlers/admin/scripts/funnel/funnel_base.py
@@ -0,0 +1,94 @@
+import tornado.web
+import ujson
+import pandas
+
+from lib.helpers import Convert
+from MYSQL_FUNNEL import *
+
+class FunnelHelpers(object):
+
+    def add_grouper_column(self,df,columns=["order","funnel_id"]):
+        # makes a grouper column because pandas #sucks
+
+        serieses = map(lambda col: df[col],columns)
+        df["+".join(columns)] = reduce(lambda s1,s2: s1.map(str) + s2.map(str),serieses)
+
+        return "+".join(columns)
+
+    def column_to_list(self,df,column="url_pattern",groupby=['order','funnel_id']):
+        # converts a column in a dataframe to a list by grouping
+        # final dataframe has same columns, but compressed along the groupby dimensions
+        # and a list for the compressed column
+
+        agg_dict = {col: max for col in df.columns}
+        agg_dict[column] = lambda x: list(x)
+
+        grouper_name = self.add_grouper_column(df,groupby)
+
+        grouped = df.groupby(grouper_name)
+        result = grouped.agg(agg_dict).reset_index(drop=True)
+
+        return result
+
+    def group_to_dict(self,columns,accepted=lambda x: True):
+        # made for use in grouped.apply(fn)
+        # converts a dataframe grouping to a dictionary
+        # final product of grouped.apply => series[dict]
+        
+        def g2d(df):
+            values = df[columns].T.to_dict().values()
+            return [v for v in values if accepted(v)]
+
+        return  g2d
+ 
+class FunnelBase(tornado.web.RequestHandler):
+
+    def update_funnel_actions(self,cur,to_update,funnel_id):
+        excludes = ["funnel_id","action_id"]
+        for i,values in to_update[["action_id","order"]].iterrows():
+            items = values.to_dict().items()
+            action_fields = ", ".join(["`%s`=\"%s\"" % (i,j) for i,j in items if not (i in excludes)])
+            action_id = values.to_dict()['action_id']
+
+            cur.execute(UPDATE_FUNNEL_ACTION % {
+                "fields": action_fields,
+                "funnel_id": funnel_id,
+                "action_id": action_id 
+            })
+     
+    def insert_funnel_actions(self,cur,to_add,funnel_id):
+        for i,values in to_add[["action_id","order"]].iterrows():
+            add = values.to_dict()
+
+            cur.execute(INSERT_FUNNEL_ACTIONS % {
+                "order": add['order'],
+                "action_id": add['action_id'],
+                "funnel_id": funnel_id
+            })
+             
+    def delete_funnel_actions(self,cur,to_remove,funnel_id):
+        for i,values in to_remove[["action_id","order"]].iterrows():
+            remove = values.to_dict()
+
+            cur.execute(DELETE_FUNNEL_ACTION % {
+                "order": remove['order'],
+                "action_id": remove['action_id'],
+                "funnel_id": funnel_id
+            })
+
+    def get_from_db(self,where):
+        return self.db.select_dataframe(GET % {"where": where})
+
+    def get_all(self):
+        where = "1=1"
+        return self.get_from_db(where)
+
+    def get_advertiser_funnels(self, advertiser):
+        where = "a.pixel_source_name = '{}'".format(advertiser)
+        return self.get_from_db(where)
+
+    def get_funnel_by_id(self, _id):
+        where = "a.funnel_id = '{}'".format(_id)
+        return self.get_from_db(where)
+      
+ 
