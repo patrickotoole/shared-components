beda19035df27ba01871e0fe007f7dd12dbf58b4
stephen <stphnma@gmail.com>
2015-06-03 19:44:18 +0000

    Pushing all whois contact info to mongodb

--- metrics/lib/optimization/domain_mongo/domain_meta.py
+++ metrics/lib/optimization/domain_mongo/domain_meta.py
@@ -3,8 +3,6 @@ import requests
 import time
 import json
 from link import lnk
-# h = lnk.dbs.hive
-import logging
 
 '''
 Domain Meta Table
@@ -21,11 +19,18 @@ def get_whois(domain):
 	r = requests.get(url%(domain,API_KEY))
 	return r.json()
 
-def extract_whois_field(whois_json, field_name):
+# def extract_whois_field(whois_json, field_name):
+
+# 	if 'contacts' in whois_json.keys() and len(whois_json['contacts']) > 0:
+# 		N = len(whois_json['contacts'])
+# 		return whois_json['contacts'][N-1][field_name]
+# 	else:
+# 		return None
+
+def extract_whois_contacts(whois_json):
 
 	if 'contacts' in whois_json.keys() and len(whois_json['contacts']) > 0:
-		N = len(whois_json['contacts'])
-		return whois_json['contacts'][N-1][field_name]
+		return whois_json['contacts']
 	else:
 		return None
 
@@ -35,11 +40,10 @@ class DomainWhois():
 	def __init__(self, start_date, end_date, fields):
 
 		self.hive = lnk.dbs.hive
-		#self.logger = logging.getLogger("opt")
 		self.start_date = start_date
 		self.end_date = end_date	
-		self.fields = fields
 		
+		self.whois_col = 'whois_contacts'
 		self.domain_list = None
 		self.data = None
 
@@ -60,13 +64,13 @@ class DomainWhois():
 		r = requests.get(url%domain)
 		return r.json()
 
-	def check_field_exists(self, domain_data):
+	def check_whois_field_exists(self, domain_data):
 
 		if len(domain_data) == 0:
 			return False
-		elif any(field not in domain_data[0].keys() for field in self.fields):
+		elif self.whois_col in domain_data[0].keys():
 			return False
-		elif any(domain_data[0][field] == 0 for field in self.fields):
+		elif domain_data[0][self.whois_col] == 0:
 			return False
 		else:
 			return True
@@ -76,26 +80,23 @@ class DomainWhois():
 		filtered = []
 		for domain in self.domain_list:
 			domain_data = self.pull_domain_data(domain)
-			if self.check_field_exists(domain_data):
+			if self.check_whois_field_exists(domain_data):
 				print "Data already exists for %s" %domain
 			else:
 				filtered.append(domain)
 		self.domain_list =  filtered
 
 
-	def extract_whois(self):
+	def add_whois(self):
 
 		df = []
-
 		for domain in self.domain_list:
 
 			print "Extracting whois for %s" %domain
 			domain_data = {}
 			domain_data['domain'] = domain
 			whois_json = get_whois(domain)
-			for field_name in self.fields:		
-				domain_data[field_name] = extract_whois_field(whois_json, field_name)
-			
+			domain_data[self.whois_col] = extract_whois_contacts(whois_json)
 			df.append(domain_data)
 			time.sleep(90)
 		
