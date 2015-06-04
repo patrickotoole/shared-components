6bc222a16d44996955aadd286f711eafcc69160e
stephen <stphnma@gmail.com>
2015-06-04 20:14:45 +0000

    pushing empty dict to table if no whois exists

--- metrics/lib/optimization/domain_list_opt/run.py
+++ metrics/lib/optimization/domain_list_opt/run.py
@@ -3,10 +3,10 @@ import geo_domainlist
 
 if __name__ == "__main__":
 
-	domain_list_id = 414939
-	G = geo_domainlist.DomainWhoisGeo(domain_list_id)
-	G.load_data()
-	G.filter()
-	G.push()
+    domain_list_id = 414939
+    G = geo_domainlist.DomainWhoisGeo(domain_list_id)
+    G.load_data()
+    G.filter()
+    G.push()
 
 
