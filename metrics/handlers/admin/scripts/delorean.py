1709b0418da185bbea687fd4cc6b48f6bf096d3e
willwest <wmw71190@gmail.com>
2015-06-24 20:27:23 +0000

    Adding delete operation and testing module

--- metrics/handlers/admin/scripts/delorean.py
+++ metrics/handlers/admin/scripts/delorean.py
@@ -1,13 +1,14 @@
 import tornado.web
 import ujson
 import functools
+import copy
 from lib.helpers import *
 from filter import FilterHandler
 from tornado.httpclient import HTTPClient
 from twisted.internet import defer
 
 class DeloreanHandler(FilterHandler):
-
+    
     @decorators.deferred
     def defer_get_tree(self):
         http_client = HTTPClient()
@@ -21,32 +22,75 @@ class DeloreanHandler(FilterHandler):
         return response
 
     @defer.inlineCallbacks
-    def append_to_tree(self, filter_label, to_append):
+    def edit_tree(self, filter_label, edits, replace=False):
         available = yield self.defer_get_available()
         self.server = "http://" + available[0]['host'] + ":9999"
 
         tree = yield self.defer_get_tree()
         
-        new_tree = self.search_tree(tree, filter_label, to_append)
-        response = yield self.defer_post_tree(new_tree)
+        if replace:
+            new_tree = self.replace_node(filter_label, tree["pattrenTree"], edits)
+        else:
+            new_tree = self.append_to_node(filter_label, tree["pattrenTree"], edits)
+
+        self.push_tree(new_tree)
+
+    @defer.inlineCallbacks
+    def delete_from_tree(self, filter_label, to_delete):
+        available = yield self.defer_get_available()
+        self.server = "http://" + available[0]['host'] + ":9999"
+
+        tree = yield self.defer_get_tree()
+
+        new_tree = self.delete_from_node(filter_label, tree["pattrenTree"], to_delete)
+        self.push_tree(new_tree)
+
+    @defer.inlineCallbacks
+    def push_tree(self, tree):
+        response = yield self.defer_post_tree(tree)
 
         if response.body == '"1"':
             self.write("1")
         else:
             self.write(response.body)
         self.finish()
-
-    def search_tree(self, tree, filter_label, to_append):
-        return self.append_to_node(filter_label, tree["pattrenTree"], to_append)
-
-    def append_to_node(self, label, tree, to_append):
+        
+    def append_to_node(self, label, old_tree, to_append):
+        tree = copy.deepcopy(old_tree)
         for node in tree["children"]:
             if node["node"]["label"] == label:
                 node["children"].extend(to_append["children"])
         return tree
 
+    def replace_node(self, label, old_tree, replacement):
+        tree = copy.deepcopy(old_tree)
+        for node in tree["children"]:
+            if node["node"]["label"] == label:
+                node["children"] = replacement["children"]
+        return tree
+
+    def delete_from_node(self, label, old_tree, to_delete):
+        tree = copy.deepcopy(old_tree)
+        for node in tree["children"]:
+            if node["node"]["label"] == label:
+                node["children"] = [ c for c in node["children"] 
+                                     if c not in to_delete["children"] ]
+        return tree
+
     @tornado.web.asynchronous
     def post(self, *args):
-        payload = tornado.escape.json_decode(self.request.body)
+        '''When delete=True, given a label and a list of nodes, delete all 
+        nodes in the tree that exactly match any of the given nodes'''
+
         label = self.get_argument("label")
-        self.append_to_tree(label, payload)
+        replace = self.get_argument("replace", False)
+        delete = self.get_argument("delete", False)
+
+        payload = tornado.escape.json_decode(self.request.body)
+
+        if delete and delete.lower() == "true":
+            self.delete_from_tree(label, payload)
+        elif replace and replace.lower() == "true":
+            self.edit_tree(label, payload, replace=True)
+        else:
+            self.edit_tree(label, payload, replace=False)
