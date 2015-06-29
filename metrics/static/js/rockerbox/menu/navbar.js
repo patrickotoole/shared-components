d6e2539e7e8a0b541a56960227f7c297d613ef09
rick <rick@rockerbox.com>
2015-06-29 17:56:52 +0000

    added logout button

--- metrics/static/js/rockerbox/menu/navbar.js
+++ metrics/static/js/rockerbox/menu/navbar.js
@@ -34,6 +34,22 @@ RB.menu.navbar = (function(navbar) {
         .text(function(x){return x.name})
         .on("click", navbar.methods.pushState.bind(this,selectbar)) 
            
+    },
+    logout: function(target) {
+      var logo_wrapper = d3_updateable(target,".logout","a")
+        .classed("logout",true)
+        .style("bottom","0px")
+        .style("position","absolute")
+        .style("width","100%")
+        
+      return d3_updateable(logo_wrapper,"a.logout-item","a") 
+        .classed("logout-item",true)
+        .text("Logout")
+        .attr("href","/logout")
+        .on("click",function(){
+          document.cookie = 'user=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+          location.reload()
+        })
     }
   }
 
@@ -41,7 +57,7 @@ RB.menu.navbar = (function(navbar) {
 
     var wrapper = navbar.components.wrapper(target)
     //var selectbar = menu.selectbar.render(target) 
-
+    navbar.components.logout(wrapper)
     navbar.components.logo(wrapper)
     navbar.components.items(wrapper,menu.selectbar.render.bind(false,target))
 
