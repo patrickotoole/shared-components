ff24e2eb8870e47fe50ca6c329a705d0a208cf76
rick <rick@rockerbox.com>
2015-07-06 16:41:46 +0000

    added cassandra as a connector for the health check

--- metrics/handlers/health.py
+++ metrics/handlers/health.py
@@ -14,18 +14,28 @@ class HealthHandler(BaseHandler):
         
 
     def check_mysql(self):
-        df = self.db.select_dataframe("select 1 status from advertiser")
-        d = Convert.df_to_values(df)[0]
-        self.write(ujson.dumps(d))
+        if self.db is None: self.write('{"status":0}')
+        else:
+            df = self.db.select_dataframe("select 1 status from advertiser")
+            d = Convert.df_to_values(df)[0]
+            self.write(ujson.dumps(d))
         self.finish()
         
     def check_spark_sql(self):
-        df = self.spark_sql.select_dataframe("select 1 status from advertiser_ref limit 1")
-        d = Convert.df_to_values(df)[0]
-        self.write(ujson.dumps(d))
+        if self.spark_sql is None: self.write('{"status":0}')
+        else:
+            df = self.spark_sql.select_dataframe("select 1 status from advertiser_ref limit 1")
+            d = Convert.df_to_values(df)[0]
+            self.write(ujson.dumps(d))
         self.finish()
          
-       
+    def check_cassandra(self):
+        if self.cassandra is None: self.write('{"status":0}')
+        else:
+            df = self.cassandra.select_dataframe("SELECT * FROM rockerbox.visitor_domains limit 1")
+            if len(df) == 1: self.write('{"status":1}')
+        self.finish()
+        
     @tornado.web.asynchronous
     def get(self,meta=""):
        check = "check_%s" % meta
