af833f283e0b7c29175e5a805a65ed369eba1529
willwest <wmw71190@gmail.com>
2015-07-24 18:25:03 +0000

    Adding timeout option and parameterized advertiser field

--- metrics/handlers/analytics/visit_uids_v2.py
+++ metrics/handlers/analytics/visit_uids_v2.py
@@ -3,7 +3,6 @@ import ujson
 import pandas
 import StringIO
 import logging
-import signal
 
 from ..base import BaseHandler
 from analytics_base import AnalyticsBase
@@ -12,6 +11,7 @@ from lib.helpers import decorators
 from lib.helpers import *
 
 from cassandra import ReadTimeout
+from cassandra import OperationTimedOut
 
 DEFAULT_INTERVAL = "minute"
 
@@ -26,18 +26,13 @@ class VisitUidsV2Handler(BaseHandler, AnalyticsBase):
         self.cassandra = cassandra
         self.limit = None
         self.query = QUERY
-        signal.signal(signal.SIGALRM, self.timeout_handler)
-        signal.alarm(2)
-
-    def timeout_handler(self, signum, frame):
-        print "We're done here.."
-        raise Exception("Timeout")
 
     @decorators.formattable
-    def get_content(self, data):
+    def get_content(self, data, advertiser):
         def default(self, data):
             df = Convert.df_to_json(data)
-            self.render("analysis/bloodhound_test.html", data=df)
+            self.render("analysis/bloodhound_test.html", data=df, 
+                        advertiser=advertiser)
         yield default, (data,)
 
     def write_json(self, data):
@@ -61,21 +56,27 @@ class VisitUidsV2Handler(BaseHandler, AnalyticsBase):
             date_clause, 
             logic
         )
-
-        users = df[["timestamp","uid"]].groupby(["timestamp"]).agg({"uid": lambda x: len(set(x))})
+        
+        # Get the user counts for each timestamp
+        users = df[["timestamp","uid"]].groupby(["timestamp"])
+        
+        # Note: this lambda may look hacky, but it is MUCH faster than nunique
+        users = users.agg({"uid": lambda x: len(set(x))})
         users = users.reset_index()
         users["num_users"] = users.uid
         del users["uid"]
-        print users
 
+        # Get counts for url/timestamp combinations
         visits = df.groupby(["timestamp", "url"]).agg({"uid": lambda x: len(set(x))})
         visits = pandas.DataFrame(visits).reset_index()
         del visits["url"]
 
+        # Now get the sum of visits for each url
         visits = visits.groupby("timestamp").sum().sort().reset_index()
         visits["num_visits"] = visits.uid
         del visits["uid"]
 
+        # Combine the users/visits counts for each timestamp
         counts = visits.set_index("timestamp").join(users.set_index("timestamp")).reset_index()
 
         response = {
@@ -86,10 +87,22 @@ class VisitUidsV2Handler(BaseHandler, AnalyticsBase):
         self.write_json(response)
 
     @defer.inlineCallbacks
-    def get_urls(self, advertiser, terms, date_clause, logic="should"):
-        df = yield self.defer_execute("url, uid", advertiser, terms, date_clause, logic)
+    def get_urls(self, advertiser, terms, date_clause, logic="should", 
+                 timeout=60):
 
-        if len(df) == 0:
+        df = yield self.defer_execute( "url, uid", advertiser, terms, 
+                                       date_clause, logic, timeout=timeout)
+
+        if not isinstance(df, pandas.DataFrame) and not df:
+            response = [
+                {
+                    "url":"Timeout", 
+                    "count": "try making your query more specific"
+                }
+            ]
+            self.write_json(response)
+
+        elif len(df) == 0:
             self.write_json([])
         else:
 
@@ -132,7 +145,7 @@ class VisitUidsV2Handler(BaseHandler, AnalyticsBase):
         self.write_json(response)
 
     @decorators.deferred
-    def defer_execute(self, selects, advertiser, pattern, date_clause, logic):
+    def defer_execute(self, selects, advertiser, pattern, date_clause, logic, timeout=60):
         if not pattern:
             raise Exception("Must specify search term using search=")
 
@@ -142,8 +155,13 @@ class VisitUidsV2Handler(BaseHandler, AnalyticsBase):
 
         q = QUERY.format(selects) + where
         print q
+        
+        try:
+            data = self.cassandra.execute(q,None,timeout=timeout)
+        except OperationTimedOut:
+            print "CAUGHT IT"
+            return False
 
-        data = self.cassandra.execute(q,None,60)
         df = pandas.DataFrame(data)
         return df
 
@@ -156,44 +174,48 @@ class VisitUidsV2Handler(BaseHandler, AnalyticsBase):
         end_date = self.get_argument("end_date", "")
         date = self.get_argument("date", "")
         advertiser = self.get_argument("advertiser", "")
+        timeout = self.get_argument("timeout", 60)
 
         date_clause = self.make_date_clause("timestamp", date, start_date, end_date)
 
-        if terms:
-            terms = terms.split(',')
-
-        if formatted:
-            if api_type=="uids":
-                self.get_uids(
-                    advertiser,
-                    terms,
-                    date_clause,
-                    logic=logic
-                    )
-            elif api_type == "urls":
-                self.get_urls(
-                    advertiser,
-                    terms,
-                    date_clause,
-                    logic=logic
-                    )
-            elif api_type=="count":
-                self.get_count(
-                    advertiser,
-                    terms,
-                    date_clause,
-                    logic=logic
-                    )
-            elif api_type=="timeseries":
-                self.get_timeseries(
-                    advertiser,
-                    terms,
-                    date_clause,
-                    logic=logic
-                    )
+        try:
+            if terms:
+                terms = terms.split(',')
+                
+            if formatted:
+                if api_type=="uids":
+                    self.get_uids(
+                        advertiser,
+                        terms,
+                        date_clause,
+                        logic=logic
+                        )
+                elif api_type == "urls":
+                    self.get_urls(
+                        advertiser,
+                        terms,
+                        date_clause,
+                        logic=logic,
+                        timeout=int(timeout)
+                        )
+                elif api_type=="count":
+                    self.get_count(
+                        advertiser,
+                        terms,
+                        date_clause,
+                        logic=logic
+                        )
+                elif api_type=="timeseries":
+                    self.get_timeseries(
+                        advertiser,
+                        terms,
+                        date_clause,
+                        logic=logic
+                        )
+                else:
+                    raise Exception("Invalid api call")
             else:
-                raise Exception("Invalid api call")
-        else:
-            self.get_content(pandas.DataFrame())
+                self.get_content(pandas.DataFrame(), advertiser)
+        except Exception as e:
+            print "CAUGHT IT"
 
-        
