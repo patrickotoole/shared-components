cdc4678b3646906982b3f66f3cc89121c75cff4e
rickotoole <rick@rockerbox.com>
2015-09-10 23:29:47 +0000

    cache updates to populate tables

--- metrics/lib/cassandra_helpers/cache.py
+++ metrics/lib/cassandra_helpers/cache.py
@@ -1,9 +1,35 @@
-from cassandra_range_query import CassandraRangeQuery, PreparedCassandraRangeQuery
-from cassandra_helper import FutureHelpers
+from lib.cassandra_helpers.range_query import CassandraRangeQuery, PreparedCassandraRangeQuery
+from lib.cassandra_helpers.helpers import FutureHelpers
 
-FUTURES    = 300
+FUTURES    = 60
 NUM_DAYS   = 20
 INSERT_UDF = "insert into full_replication.function_patterns (function,pattern) VALUES ('state_group_and_count','%s')"
+INSERT = "INSERT INTO rockerbox.action_occurrence_u1 (source,date,action,uid,u1,url,occurrence) VALUES (?,?,?,?,?,?,?)"
+
+def build_datelist(numdays):
+    import datetime
+    
+    base = datetime.datetime.today()
+    date_list = [base - datetime.timedelta(days=x) for x in range(0, numdays)]
+    dates = map(lambda x: str(x).split(" ")[0] + " 00:00:00",date_list)
+
+    return dates
+
+
+
+def select_callback(result,*args):
+    advertiser, pattern, inserts = args
+    
+    result = result[0]
+    res = result['rockerbox.group_and_count(url, uid)']
+    print result['date'], len(res)
+    
+    date = result["date"]
+    for url_uid in res:
+        if "[:]" in url_uid:
+            url, uid = url_uid.split("[:]")
+            inserts += [[advertiser,date,pattern,uid,int(uid[-2:]),url,int(res[url_uid])]]
+
 
 class CassandraCache(PreparedCassandraRangeQuery):
 
@@ -15,13 +41,17 @@ class CassandraCache(PreparedCassandraRangeQuery):
         self.insert_query = insert_query
 
     def build_udf(self,pattern):
+        print "build udf"
         self.cassandra.execute(INSERT_UDF % pattern)
 
    
     def build_cache(self,advertiser,pattern,callback,*args):
+        _, _, inserts = self.run_select(advertiser,pattern,callback,*args)
+        self.run_inserts(inserts)
 
-        _, _, inserts = run_select(advertiser,pattern,callback,*args)
-        self.build_inserts(inserts)
+    def execute(self,data):
+        bound = self.default_statement.bind(data)
+        return self.cassandra.execute_async(bound)
 
     def run_select(self,advertiser,pattern,callback,*args):
 
@@ -35,7 +65,7 @@ class CassandraCache(PreparedCassandraRangeQuery):
 
     def run_inserts(self,inserts):
         import random 
-
+        print "starting insert"
         if len(inserts) > 0:
             insert_statement = self.build_statement(INSERT,"","")
             bound_insert = self.bind_and_execute(insert_statement)
@@ -43,3 +73,22 @@ class CassandraCache(PreparedCassandraRangeQuery):
                 print x
             random.shuffle(inserts)
             FutureHelpers.future_queue(inserts,bound_insert,cb,FUTURES)
+
+def main(advertiser,pattern):
+    
+    from link import lnk
+    c = lnk.dbs.cassandra
+    SELECT = "SELECT date, group_and_count(url,uid) FROM rockerbox.visit_uids_lucene_timestamp_u2_clustered"
+    FIELDS = ["source","date"]
+    INSERT = "INSERT INTO rockerbox.action_occurrence_u1 (source,date,action,uid,u1,url,occurrence) VALUES (?,?,?,?,?,?,?)"
+
+    cache = CassandraCache(c,SELECT,FIELDS,"u2",INSERT)
+    cache.build_cache(advertiser,pattern,select_callback,advertiser,pattern,[])
+
+
+if __name__ == "__main__":
+    import sys
+    print sys.argv
+    advertiser, pattern = sys.argv[1:]
+
+    main(advertiser, pattern)
