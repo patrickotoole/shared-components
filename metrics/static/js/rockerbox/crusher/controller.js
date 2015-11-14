d81f122beb1558879959c139eeedf8fc33bbb7c3
rickotoole <rick@rockerbox.com>
2015-11-14 19:26:37 +0000

    more cleanup of controller to respective UI controllers

--- metrics/static/js/rockerbox/crusher/controller.js
+++ metrics/static/js/rockerbox/crusher/controller.js
@@ -22,89 +22,68 @@ RB.crusher.controller = (function(controller) {
     
   }
 
+  var build_header = function(obj) {
+    
+    var target = d3.selectAll(".container")
+
+    var funnelRow = d3_splat(target,".row","div",[obj],function(x){return x.id})
+      .classed("row funnels",true)
+
+    funnelRow.exit().remove()
+
+    var heading = d3_updateable(funnelRow,".heading","h5")
+
+    heading.text(function(x){return x.name})
+      .attr("style","margin-top:-15px;padding-left:20px;height: 70px;line-height:70px;border-bottom:1px solid #f0f0f0;margin-left:-15px")
+      .classed("heading heading",true)
+
+    d3_updateable(funnelRow,".pixel-description","div")
+      .classed("pixel-description",true)
+      .style("margin-top","15px")
+      .style("margin-bottom","15px")
+      .html(function(x){return x.description})
+
+    return funnelRow
+
+  }
+
   controller.initializers = {
     "settings/advertiser": function() {
-      var target = d3.selectAll(".container")
 
-      var funnelRow = d3_splat(target,".row","div",[{"id":"advertiser"}],function(x){return x.id})
-        .classed("row funnels",true)
-
-      funnelRow.exit().remove()
-
-      var heading = d3_updateable(funnelRow,".heading","h5")
-
-      heading.text("Manage Advertiser")
-        .attr("style","margin-top:-15px;padding-left:20px;height: 70px;line-height:70px;border-bottom:1px solid #f0f0f0;margin-left:-15px")
-        .classed("heading heading",true)
-
+      var funnelRow = build_header({"id":"settings/advertiser","name":"Manage Advertiser"})
       crusher.ui.settings.advertiser(funnelRow)
+
     },
-
     "settings/subscription": function() {
-      var target = d3.selectAll(".container")
 
-      var funnelRow = d3_splat(target,".row","div",[{"id":"subscription"}],function(x){return x.id})
-        .classed("row funnels",true)
-
-      funnelRow.exit().remove()
-
-      var heading = d3_updateable(funnelRow,".heading","h5")
-
-      heading.text("Manage Subscription")
-        .attr("style","margin-top:-15px;padding-left:20px;height: 70px;line-height:70px;border-bottom:1px solid #f0f0f0;margin-left:-15px")
-        .classed("heading heading",true)
-
+      var funnelRow = build_header({"id":"settings/subscription","name":"Manage Subscription"})
       crusher.ui.settings.subscription(funnelRow)
 
     },
     "settings": function() {
-      var target = d3.selectAll(".container")
-
-      var funnelRow = d3_splat(target,".row","div",[{"id":"settings"}],function(x){return x.id})
-        .classed("row funnels",true)
-
-      funnelRow.exit().remove()
-
-      var heading = d3_updateable(funnelRow,".heading","h5")
-
-      heading.text("Account Overview")
-        .attr("style","margin-top:-15px;padding-left:20px;height: 70px;line-height:70px;border-bottom:1px solid #f0f0f0;margin-left:-15px")
-        .classed("heading heading",true)
-
-      d3_updateable(funnelRow,".pixel-description","div")
-        .classed("pixel-description",true)
-        .style("margin-top","15px")
-        .style("margin-bottom","15px")
-        .html("Below is a summary of the settings associated with your account.")
-
-
-      crusher.subscribe.add_subscriber(
-        ["advertiser","actions", "funnels"], 
-        function(advertiser_data,action_data,funnel_data){
-
-          crusher.ui.settings.main(funnelRow,advertiser_data,action_data,funnel_data)
-
-        },
-        "overview",true,false
-      )
-
-
+      
+      var funnelRow = build_header({
+        "id":"settings",
+        "name":"Account Overview",
+        "description":"Below is a summary of the settings associated with your account."
+      })
+      
+      var subscription = function (advertiser,actions,funnels) {
+        crusher.ui.settings.main(funnelRow,advertiser,actions,funnels)
+      }
+      crusher.subscribe.add_subscriber(["advertiser","actions", "funnels"], subscription, "settings",true,true)
 
     },
     "settings/pixel/setup": function() {
-      var target = d3.selectAll(".container")
 
-      var funnelRow = d3_splat(target,".row","div",[{"id":"setup"}],function(x){return x.id})
-        .classed("row funnels",true)
-
-      funnelRow.exit().remove()
+      var funnelRow = build_header({"id":"setup","name":"Pixel Setup"})
 
       crusher.subscribe.add_subscriber(
         ["pixel_status","advertiser","an_uid"], 
         function(status_data,advertiser_data,uid){
           crusher.ui.pixel.setup(funnelRow,status_data,advertiser_data,uid)
         },
-        "settings",true,true
+        "pixel_settings",true,true
       )
 
     },
@@ -206,132 +185,13 @@ RB.crusher.controller = (function(controller) {
 
       d3.select("body").classed("hide-select",true)
 
-      var target = d3.selectAll(".container")
+      var funnelRow = build_header({"id":"home","name":"Welcome to Crusher"})
+      crusher.ui.home.main(funnelRow)
+      
+      var wrapper = funnelRow.selectAll(".tutorial-description")
+      var subscription = crusher.ui.home.status.bind(false,wrapper)
 
-      var funnelRow = d3_splat(target,".row","div",[{"id":"home"}],function(x){return x.id})
-        .classed("row funnels",true)
-
-      funnelRow.exit().remove()
-
-
-
-      var heading = d3_updateable(funnelRow,".welcome-heading","h5")
-
-      heading.text("Welcome to Crusher")
-        .attr("style","margin-top:-15px;padding-left:20px;height: 70px;line-height:70px;border-bottom:1px solid #f0f0f0;margin-left:-15px")
-        .classed("welcome-heading heading",true)
-
-      var info = d3_updateable(funnelRow,".row","div")
-
-      info.attr("style","padding-bottom:15px;padding-top:5px")
-        .classed("row",true)
-
-      var descriptionWrap = d3_updateable(info,".crusher-about","div")
-        .classed("crusher-about col-md-6",true)
-
-      var description = d3_updateable(descriptionWrap,".ct-chart","div")
-        .classed("ct-chart",true)
-        .style("padding-bottom","15px")
-
-      d3_updateable(description,".about-heading","")
-        .classed("about-heading chart-title",true)
-        .text("What is Crusher?")
-
-      d3_updateable(description,".about-description","div")
-        .classed("about-description chart-description",true)
-        .html(
-          "<br>Crusher is a tool to help you understand the off-site interests and opportunities to advertise to users in your audience based on differences in on-site user activity." + 
-          "<br><br>We built crusher because we believe that understanding what you audience does when they are not on your site is the is the best way to craft relevant, meaningful advertisements."
-        )
-
-      var descriptionWrap = d3_updateable(info,".crusher-how","div")
-        .classed("crusher-how col-md-6",true)
-
-      var description = d3_updateable(descriptionWrap,".ct-chart","div")
-        .classed("ct-chart",true)
-        .style("padding-bottom","15px")
-
-      d3_updateable(description,".about-heading","")
-        .classed("about-heading chart-title",true)
-        .text("How to use Crusher")
-
-      d3_updateable(description,".about-description","div")
-        .classed("about-description chart-description",true)
-        .html(
-          "<br>Crusher data provides a better understanding of your audience which can be used to: <br><br>" +
-          "<ul><li>provide demographic insight about your audience</li><li>influence creative development</li><li>recommend topics for content marketing</li><li>highlight opportunities for direct advertising deals</li><li>make programmatic buys similar to your current audience</li></ul>"
-        )
-
-      var descriptionWrap = d3_updateable(info,".crusher-tutorial","div")
-        .classed("crusher-tutorial col-md-12",true)
-
-      var description = d3_updateable(descriptionWrap,".ct-chart","div")
-
-        .classed("crusher-tutorial ct-chart",true)
-        .style("padding-bottom","15px")
-
-      d3_updateable(description,".tutorial-heading","")
-        .classed("tutorial-heading chart-title",true)
-        .text("Getting started with Crusher")
-
-      var tutDesc = d3_updateable(description,".tutorial-description","div")
-        .classed("tutorial-description chart-description",true)
-
-      crusher.subscribe.add_subscriber(["pixel_status","advertiser","actions"], function(status_data,advertiser_data,actions){
-
-        var item1 = d3_updateable(tutDesc,".item-1","div")
-          .classed("item-1",true)
-          .style("margin-top","10px")
-
-        d3_updateable(item1,".status","span")
-          .classed("glyphicon status",true)
-          .style("font-size","24px")
-          .style("float","left")
-          .classed("glyphicon-ok-circle green",function(x) {
-            return status_data.filter(function(x){return x.last_fired_seconds != undefined}).length 
-          })
-          .classed("glyphicon-remove-circle red",function(x) {
-            return status_data.filter(function(x){return x.last_fired_seconds != undefined}).length == 0 
-          })
-
-        d3_updateable(item1,".desc","a")
-          .classed("desc",true)
-          .style("line-height","24px")
-          .style("text-align","center")
-          .style("color","rgb(90, 90, 90)")
-          .style("padding-left","10px")
-          .html("Implement the Rockerbox pixel on your website. ")
-          .on("click",function(){
-            RB.routes.navigation.forward(controller.states["/crusher/settings/pixel_setup"])  
-          })
-
-        var item2 = d3_updateable(tutDesc,".item-2","div")
-          .classed("item-2",true)
-          .style("margin-top","10px")
-
-        d3_updateable(item2,".status","span")
-          .classed("glyphicon status",true)
-          .style("font-size","24px")
-          .style("float","left")
-          .classed("glyphicon-ok-circle green",function(x) {
-            return crusher.cache.actionData.length 
-          })
-          .classed("glyphicon-remove-circle red",function(x) {
-            return crusher.cache.actionData.length == 0 
-          })
-
-        d3_updateable(item2,".desc","a")
-          .classed("desc",true)
-          .style("line-height","24px")
-          .style("text-align","center")
-          .style("color","rgb(90, 90, 90)")
-          .style("padding-left","10px")
-          .html("Implement your first action")
-          .on("click",function(){
-            RB.routes.navigation.forward(controller.states["/crusher/action"])  
-          })
-         
-      },"home",true,false)
+      crusher.subscribe.add_subscriber(["pixel_status","advertiser","actions"],subscription,"home",true,false)
 
     },
     "action": function(){
@@ -339,103 +199,19 @@ RB.crusher.controller = (function(controller) {
       RB.component.export(RB.crusher.ui.funnel.show, RB.crusher.ui.funnel)
       RB.component.export(RB.crusher.ui.action.show, RB.crusher.ui.action)
 
-      var target = d3.selectAll(".container")
+      var funnelRow = build_header({"id":"action_about","name":"Action Dashboard"})
+      var subscription = RB.crusher.ui.action.dashboard.show.bind(false,funnelRow)
 
-      var funnelRow = d3_splat(target,".row","div",[{"id":"action_about"}],function(x){return x.id})
-        .classed("row funnels",true)
-
-      funnelRow.exit().remove()
-
-      crusher.subscribe.add_subscriber(["recommended_actions","actions"],function(reccomended,existing) {
-
-        RB.crusher.ui.action.dashboard.show(funnelRow,reccomended,existing)
-        
-      },"actionDashboard",true,true)
+      crusher.subscribe.add_subscriber(["recommended_actions","actions"],subscription ,"actionDashboard",true,true)
 
     },
     "analytics": function(){
-      var target = d3.selectAll(".container")
 
-      var funnelRow = d3_splat(target,".row","div",[{"id":"analytics_overview"}],function(x){return x.id})
-        .classed("row funnels",true)
+      var funnelRow = build_header({"id":"analytics_overview","name":"Dashboard"})
+      var subscription = crusher.ui.home.dashboard.bind(false,funnelRow)
 
-      crusher.subscribe.add_subscriber(["dashboardStats"],function(data) {
+      crusher.subscribe.add_subscriber(["dashboardStats"],subscription,"dashboard",true,true)
 
-        var heading = d3_updateable(funnelRow,".heading","h5")
-        heading
-          .attr("style","margin-top:-15px;padding-left:20px;height: 70px;line-height:70px;border-bottom:1px solid #f0f0f0;margin-left:-15px")
-          .text("On-page Analytics Overview")
-          .classed("heading",true)
-
-        var info = d3_updateable(funnelRow,".col-md-12","div")
-
-        info.attr("style","padding-bottom:15px;padding-top:5px")
-          .classed("col-md-12",true)
-          .text("High level stats about on-page activity")
-
-        var chart_options = [{
-            "title":"Views",
-            "field":"views",
-            "description":"Number of pageviews per day generated by visitors to your site",
-            "type":"line",
-            "summary":"total",
-            "format":false
-          },{
-            "title":"Engaged",
-            "field":"engaged",
-            "description":"Number of engaged users (visited 5 or more pages) on your site",
-            "type":"line",
-            "summary":"total",
-            "format":false
-          },{
-            "title":"Visitors",
-            "field":"visitors",
-            "description":"Number of distinct users visiting your site per day",
-            "type":"line",
-            "summary":"total",
-            "format":false
-          },{
-            "title":"Engagement",
-            "field":"engagement",
-            "description":"Ratio of visitors who are considered to be engaged users",
-            "type":"line",
-            "summary":"average",
-            "format":d3.format(".1%")
-          },{
-            "title":"Ad opportunities",
-            "field":"advertising_ops",
-            "description":"Number of advertising opportunities Rockerbox has seen for your users",
-            "type":"line",
-            "summary":"total",
-            "format":false
-          },{
-            "title":"Views per user",
-            "field":"views_per_user",
-            "description":"This is the average number of pageviews per visitor to your site",
-            "type":"line",
-            "summary":"average",
-            "format":d3.format(".3r")
-          }
-        ]
-
-        var chart_wrappers = d3_splat(funnelRow,".col-md-4","div",chart_options,function(x){return x.field})
-          .classed("col-md-4",true)
-
-        var charts = d3_updateable(chart_wrappers,".ct-chart","div")
-          .attr("id",function(x){return x.field})
-          .classed("ct-chart",true)
-
-        chart_wrappers.data().map(function(d) {
-          RB.rho.ui.buildChart(
-            ".ct-chart#" + d.field, data, "date", d.field, d.title, d.description, d.type, d.summary, d.format
-          )
-        })
-
-        
-      },"dashboard",true,true)
-
-
-      funnelRow.exit().remove()
     },
     "funnel/existing": function(funnel) {
 
@@ -460,7 +236,7 @@ RB.crusher.controller = (function(controller) {
       RB.component.export(RB.crusher.ui.funnel.show, RB.crusher.ui.funnel)
       RB.component.export(RB.crusher.ui.action.show, RB.crusher.ui.action)
 
-      if (action) {
+      if (action.id) {
 
         crusher.ui.action.buildBase()
 
