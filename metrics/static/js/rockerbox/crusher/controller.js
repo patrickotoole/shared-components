fc0621762ea077ca4f9b9f08f930dc8523036dff
Farid <farid@rockerbox.com>
2015-12-10 11:21:19 -0500

    HEAP authentication

--- metrics/static/js/rockerbox/crusher/controller.js
+++ metrics/static/js/rockerbox/crusher/controller.js
@@ -16,16 +16,30 @@ RB.crusher.controller = (function(controller) {
         switch(user_type) {
           case 'rockerbox':
             heap.load("3611187932");
+            heap.identify({
+              handler: 'Rockerbox',
+              name: 'Rockerbox',
+              email: 'support@rockerbox.com'
+            });
+
             window.intercomSettings = {app_id: "rvo8kuih",name: "Rockerbox",email: "support@rockerbox.com",created_at: 1312182000};
             (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/kbtc5999';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{l(false)}}})()
             break;
           case 'client':
             setTimeout(function() {
               var user = {
+                'id': advertiser_data.id,
                 'name': advertiser_data.contact_name,
                 'email': advertiser_data.email
               };
+
               heap.load("1793449886");
+              heap.identify({
+                handler: user.id,
+                name: user.name,
+                email: user.email
+              });
+
               window.intercomSettings = {app_id: "o6ats3cn",name: user.name,email: user.email,created_at: 1312182000};
               (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/xu33kr1z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{l(false)}}})()
             }, 1000)
