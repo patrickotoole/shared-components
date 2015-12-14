97fcef0fa0f1a2ca9907c38794e7aee4a7caf296
Farid <farid@rockerbox.com>
2015-12-14 10:58:15 -0500

    Track page loads

--- metrics/static/js/rockerbox/crusher/controller.js
+++ metrics/static/js/rockerbox/crusher/controller.js
@@ -51,6 +51,8 @@ RB.crusher.controller = (function(controller) {
               (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/xu33kr1z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{l(false)}}})()
               break;
           }
+
+          Intercom('trackEvent', 'Page Load', {'url': window.location.href});
         }, 2000)
       }, 'advertiser-name-email', true, false);
 
