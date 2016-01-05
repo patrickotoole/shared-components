6e275a07a2c02a5555227655b8b8727b3241d88a
rickotoole <rick@rockerbox.com>
2016-01-05 02:09:35 +0000

    added description to domain similarity clusters

--- metrics/static/js/rockerbox/crusher/ui.action.clusters.js
+++ metrics/static/js/rockerbox/crusher/ui.action.clusters.js
@@ -298,6 +298,71 @@ RB.crusher.ui.action = (function(action) {
     
   }
 
+  action.show_similarity = function(wrapper) {
+
+    var title = "Domain Similarity Clusters",
+      series = ["clusters"],
+      formatting = ".col-md-12.domain-clusters",
+      description = "Domain similarity is calculated by a visit to multiple domains by a single user"
+
+    console.log(wrapper.datum())
+
+    var domains = wrapper.datum().domains
+
+    var domain_dict = d3.nest()
+      .key(function(x){return x.domain})
+      .map(domains)
+
+    var target = RB.rho.ui.buildSeriesWrapper(wrapper.selectAll(".action-body"), title, series, [wrapper.datum()], formatting, description)
+
+    var parentNode = wrapper.selectAll(".action-body").selectAll(".domain-clusters")
+    parentNode.selectAll(".loading-icon").remove()
+
+    parentNode.classed("hidden",false)
+      .style("visibility","hidden")
+
+
+    setTimeout(function(){
+      parentNode.classed("hidden",!parentNode.classed("selected"))
+        .style("visibility",undefined)
+    },1)
+
+
+    var similarity_cluster = d3_updateable(target,".similarity-cluster","div")
+      .classed("similarity-cluster row",true)
+
+    var left = d3_updateable(similarity_cluster,".left","div")
+      .classed("left col-md-6",true)
+
+    var right = d3_updateable(similarity_cluster,".right","div")
+      .classed("right col-md-6",true)
+
+    similarity_cluster.each(function(x){
+      var that = this;
+
+      buildCooccurence(x[0][0].similarity,d3.select(this).selectAll(".right"),function(data){
+        data = data.map(function(x){
+          try {
+            var cat = JSON.parse(JSON.stringify(domain_dict[x.name][0]))
+          } catch(e) {
+            var cat = {"category_name":"NA","parent_category_name":"NA","domain":"","count":1,"weighted":0}
+          }
+
+          cat.domain = x.name
+          cat.count = x.count
+
+          return cat
+        })
+
+        RB.rho.ui.buildBarTable(d3.select(that).selectAll(".left"),data,"asdf","domain",false,20,action.category_colors)
+
+      })
+    })
+
+ 
+
+
+  }
 
   action.show_clusters = function(wrapper) {
 
@@ -361,202 +426,172 @@ RB.crusher.ui.action = (function(action) {
       })
     })
 
-   d3_updateable(target,".similarity-cluster-title","div")
-      .classed("similarity-cluster-title title",true)
-       .text("Domain Similarity")
-       .style("margin-top","20px")
+   // d3_updateable(target,".similarity-cluster-title","div")
+   //    .classed("similarity-cluster-title title",true)
+   //     .text("Domain Similarity")
+   //     .style("margin-top","20px")
 
-   d3_updateable(target,".similarity-cluster-value","div")
-      .classed("similarity-cluster-value value",true)
+   // d3_updateable(target,".similarity-cluster-value","div")
+   //    .classed("similarity-cluster-value value",true)
 
-   d3_updateable(target,".similarity-cluster-description","div")
-      .classed("similarity-cluster-description description",true)
-      .text("Similary between domains based on domain co-visiting by users")
+   // d3_updateable(target,".similarity-cluster-description","div")
+   //    .classed("similarity-cluster-description description",true)
+   //    .text("Similary between domains based on domain co-visiting by users")
 
-
-
-    var similarity_cluster = d3_updateable(target,".similarity-cluster","div")
-      .classed("similarity-cluster row",true)
-
-    var left = d3_updateable(similarity_cluster,".left","div")
-      .classed("left col-md-6",true)
-
-    var right = d3_updateable(similarity_cluster,".right","div")
-      .classed("right col-md-6",true)
-
-    similarity_cluster.each(function(x){
-      var that = this;
-
-      buildCooccurence(x[0][0].similarity,d3.select(this).selectAll(".right"),function(data){
-        data = data.map(function(x){
-          try {
-            var cat = JSON.parse(JSON.stringify(domain_dict[x.name][0]))
-          } catch(e) {
-            var cat = {"category_name":"NA","parent_category_name":"NA","domain":"","count":1,"weighted":0}
-          }
-
-          cat.domain = x.name
-          cat.count = x.count
-
-          return cat
-        })
-
-
-        RB.rho.ui.buildBarTable(d3.select(that).selectAll(".left"),data,"asdf","domain",false,20,action.category_colors)
-
-      })
-    })
+   action.show_similarity(wrapper)
 
 
 
     
-    var clusters = d3_splat(target,".cluster","div",
-        function(x){return x[0][0].clusters},
-        function(x,i){
-          x.domains_with_category = x.domains.map(function(y){
-            try {
-              return domain_dict[y][0]
-            } catch(e) {
-              return {"category_name":"NA","parent_category_name":"NA","domain":y,"count":1,"weighted":0}
-            }
-          })
-
-          x.cluster_categories = d3.nest()
-            .key(function(y){return y.parent_category_name})
-            .rollup(function(y){
-              return {
-                "parent_category_name": y[0].parent_category_name,
-                "domains":y.length,
-                "users": d3.sum(y.map(function(z){return z.count }))
-              }
-            })
-            .entries(x.domains_with_category).map(function(x){return x.values})
-            .map(function(x){
-              x.value = Math.log(x.users*x.users)
-              return x
-            })
-            .sort(function(x,y){return y.users - x.users})
-
-          x.index = i
-
-          return x.cluster
-        }
-      )
-      .classed("cluster row",true)
-
-    d3_updateable(clusters,".cluster-name","h4")
-      .classed("cluster-name value",true)
-      .style("font-size","20px")
-      .text(function(x,i){return "Cluster " + (x.index + 1) + ": " + x.cluster_categories[0].parent_category_name})
 
     
+  //   var clusters = d3_splat(target,".cluster","div",
+  //       function(x){return x[0][0].clusters},
+  //       function(x,i){
+  //         x.domains_with_category = x.domains.map(function(y){
+  //           try {
+  //             return domain_dict[y][0]
+  //           } catch(e) {
+  //             return {"category_name":"NA","parent_category_name":"NA","domain":y,"count":1,"weighted":0}
+  //           }
+  //         })
+
+  //         x.cluster_categories = d3.nest()
+  //           .key(function(y){return y.parent_category_name})
+  //           .rollup(function(y){
+  //             return {
+  //               "parent_category_name": y[0].parent_category_name,
+  //               "domains":y.length,
+  //               "users": d3.sum(y.map(function(z){return z.count }))
+  //             }
+  //           })
+  //           .entries(x.domains_with_category).map(function(x){return x.values})
+  //           .map(function(x){
+  //             x.value = Math.log(x.users*x.users)
+  //             return x
+  //           })
+  //           .sort(function(x,y){return y.users - x.users})
+
+  //         x.index = i
+
+  //         return x.cluster
+  //       }
+  //     )
+  //     .classed("cluster row",true)
+
+  //   d3_updateable(clusters,".cluster-name","h4")
+  //     .classed("cluster-name value",true)
+  //     .style("font-size","20px")
+  //     .text(function(x,i){return "Cluster " + (x.index + 1) + ": " + x.cluster_categories[0].parent_category_name})
+
+  //   
 
 
-    var cat = d3_updateable(clusters,".categories","div")
-      .classed("categories col-md-4",true)
-      .style("padding","0px")
-      .datum(function(z){
-        return z.cluster_categories
-      })
+  //   var cat = d3_updateable(clusters,".categories","div")
+  //     .classed("categories col-md-4",true)
+  //     .style("padding","0px")
+  //     .datum(function(z){
+  //       return z.cluster_categories
+  //     })
 
-    var dom = d3_updateable(clusters,".cluster-domains","div")
-      .classed("cluster-domains col-md-8",true)
-      .style("padding","0px")
-      .datum(function(z){
-        return z.domains_with_category
-      })
+  //   var dom = d3_updateable(clusters,".cluster-domains","div")
+  //     .classed("cluster-domains col-md-8",true)
+  //     .style("padding","0px")
+  //     .datum(function(z){
+  //       return z.domains_with_category
+  //     })
 
-    var filterCat = function(nodeData) {
-      var current = this;
-      var current_title = title.filter(function(x){
-        return this.parentNode == current
-      })
-      current_title.text(nodeData.parent_category_name)
-        .style("font-weight","bold")
+  //   var filterCat = function(nodeData) {
+  //     var current = this;
+  //     var current_title = title.filter(function(x){
+  //       return this.parentNode == current
+  //     })
+  //     current_title.text(nodeData.parent_category_name)
+  //       .style("font-weight","bold")
 
-      var current_dom = dom.filter(function(){return this.parentNode == current.parentNode})
+  //     var current_dom = dom.filter(function(){return this.parentNode == current.parentNode})
 
-      var data = current_dom.datum()
+  //     var data = current_dom.datum()
 
-      var filtered_data = data.filter(function(x){
-        return x.parent_category_name == nodeData.parent_category_name
-      })
+  //     var filtered_data = data.filter(function(x){
+  //       return x.parent_category_name == nodeData.parent_category_name
+  //     })
 
-      buildCurrent(current_dom,filtered_data) 
+  //     buildCurrent(current_dom,filtered_data) 
 
-      //var dom_row = d3_splat(current_dom,".domain-row","div",function(x){return x},function(x){return x.domain})
-      //  .classed("domain-row",true)
-      //  .text(JSON.stringify) 
+  //     //var dom_row = d3_splat(current_dom,".domain-row","div",function(x){return x},function(x){return x.domain})
+  //     //  .classed("domain-row",true)
+  //     //  .text(JSON.stringify) 
 
-      //dom_row.filter(function(x){return x.parent_category_name != nodeData.parent_category_name})
-      //  .remove()
+  //     //dom_row.filter(function(x){return x.parent_category_name != nodeData.parent_category_name})
+  //     //  .remove()
 
-    }
+  //   }
 
-    var buildCurrent = function(current_dom,data) {
+  //   var buildCurrent = function(current_dom,data) {
 
-      RB.rho.ui.buildBarTable(current_dom,data,"asdf","domain",false,12,action.category_colors)
+  //     RB.rho.ui.buildBarTable(current_dom,data,"asdf","domain",false,12,action.category_colors)
 
-    }
+  //   }
 
-      dom.map(function(x){
-        var current_dom = d3.select(x[0])
-        var data = x[0].parentNode.__data__.domains_with_category
+  //     dom.map(function(x){
+  //       var current_dom = d3.select(x[0])
+  //       var data = x[0].parentNode.__data__.domains_with_category
  
-        buildCurrent(current_dom,data)
-      })
+  //       buildCurrent(current_dom,data)
+  //     })
 
-    //d3_splat(dom,".domain-row","div",function(x){return x},function(x){return x.domain})
-      //.classed("domain-row",true)
-      //.text(JSON.stringify)
+  //   //d3_splat(dom,".domain-row","div",function(x){return x},function(x){return x.domain})
+  //     //.classed("domain-row",true)
+  //     //.text(JSON.stringify)
 
-    var HOVER_TEXT = "(hover to refine category)"
+  //   var HOVER_TEXT = "(hover to refine category)"
 
-    var reset = d3_updateable(cat,".categories-reset","div")
-      .classed("categories-reset pull-right btn btn-xs btn-default",true)
-      .style("font-size","12px")
-      .style("font-weight","normal")
-      .text("reset")
-      .on("click",function(x){
-        var current_cat = d3.select(this.parentNode)
-        current_cat.selectAll(".categories-title")
-          .text(HOVER_TEXT)
-          .style("font-weight",undefined)
+  //   var reset = d3_updateable(cat,".categories-reset","div")
+  //     .classed("categories-reset pull-right btn btn-xs btn-default",true)
+  //     .style("font-size","12px")
+  //     .style("font-weight","normal")
+  //     .text("reset")
+  //     .on("click",function(x){
+  //       var current_cat = d3.select(this.parentNode)
+  //       current_cat.selectAll(".categories-title")
+  //         .text(HOVER_TEXT)
+  //         .style("font-weight",undefined)
 
-        action.bubbles(current_cat,filterCat,action.category_colors)
+  //       action.bubbles(current_cat,filterCat,action.category_colors)
 
-        var current = this.parentNode
-        var current_dom = dom.filter(function(){return this.parentNode == current.parentNode})
-        var data = current_dom.datum()
-  
-        buildCurrent(current_dom,data,action.category_colors) 
-        
-      })
+  //       var current = this.parentNode
+  //       var current_dom = dom.filter(function(){return this.parentNode == current.parentNode})
+  //       var data = current_dom.datum()
+  // 
+  //       buildCurrent(current_dom,data,action.category_colors) 
+  //       
+  //     })
 
 
 
-    var title = d3_updateable(cat,".categories-title","div")
-      .classed("categories-title",true)
-      .style("font-size","12px")
-      .text(HOVER_TEXT)
+  //   var title = d3_updateable(cat,".categories-title","div")
+  //     .classed("categories-title",true)
+  //     .style("font-size","12px")
+  //     .text(HOVER_TEXT)
 
-    action.bubbles(cat,filterCat,action.category_colors)
+  //   action.bubbles(cat,filterCat,action.category_colors)
 
-     
+  //    
 
-    d3_updateable(clusters,".domains","div")
-      .classed("domains",true)
-      .datum(function(x){
-        return x.domains_with_category.map(function(x){
-          return {
-            "domain":x.domain,
-            "count": x.count,
-            "parent_category_name":x.parent_category_name,
-            "category_name":x.category_name,
-          }
-        })
-      })
-      //.text(JSON.stringify)
+  //   d3_updateable(clusters,".domains","div")
+  //     .classed("domains",true)
+  //     .datum(function(x){
+  //       return x.domains_with_category.map(function(x){
+  //         return {
+  //           "domain":x.domain,
+  //           "count": x.count,
+  //           "parent_category_name":x.parent_category_name,
+  //           "category_name":x.category_name,
+  //         }
+  //       })
+  //     })
+  //     //.text(JSON.stringify)
     
   }
 
