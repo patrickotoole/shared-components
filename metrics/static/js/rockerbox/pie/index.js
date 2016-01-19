1869204d686903075db24c9cc6cdfe82db9aabbb
rickotoole <rick@rockerbox.com>
2016-01-19 18:39:00 +0000

    added node module example for pie

---
+++ metrics/static/js/rockerbox/pie/index.js
@@ -0,0 +1,1 @@
+export {default as pie} from "./src/pie";
---
+++ metrics/static/js/rockerbox/pie/src/d3_splat.js
@@ -0,0 +1,12 @@
+export default function(target,selector,type,data,joiner) {
+  var type = type || "div"
+  var updateable = target.selectAll(selector).data(
+    data || function(x){return x},
+    joiner || function(x){return x}
+  )
+
+  updateable.enter()
+    .append(type)
+
+  return updateable
+}
