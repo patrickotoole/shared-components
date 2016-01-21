413c4614a3e5382d1368cb5f2b7a8258937902da
Rich Archer <richarcher1@ricks-MacBook-Pro.local>
2016-01-21 17:39:38 -0500

    fix vendor dashboard unit tests

--- metrics/tests/lib/test_vendor_cache.py
+++ metrics/tests/lib/test_vendor_cache.py
@@ -1,10 +1,7 @@
 import mock, pandas, json
 import unittest
-
+from mock import patch
 import requests
-requests = mock.MagicMock()
-requests.get.side_effect = lambda x : {}
-requests.post.side_effect = lambda x : {}
 import lib.caching.vendor_patterns as vps
 import lib.caching.vendor_dashboard as vdb
 import lib.caching.action_dashboard_cache as adc
@@ -12,12 +9,17 @@ import lib.caching.action_dashboard_cache as adc
 URL_1 = "randomsite/refer=kfkf&source=nothing&utm_source=MySource&somethingelse"
 JSON_FIXTURE_1 = {}
 DA = {"url_patterns":"arandompatternorstring","vendor":["vendor"]}
+RESPONSE_1 = dict({'response':[{'url_pattern':['url']}]})
+
+mock_response = mock.MagicMock()
+mock_response2 = mock.MagicMock()
+mock_response2.__iter__ = mock.Mock(return_value = "url")
+mock_response.json.side_effect = mock_response2
 
 class VendorTests(unittest.TestCase):
 
     def setUp(self):
         self.instance = vps.VendorPattern({}, "test")
-        requests = mock.MagicMock()
     
     def test_parse(self):
         utm_source = self.instance.parseRefer(URL_1)
@@ -27,7 +29,8 @@ class VendorTests(unittest.TestCase):
         df = pandas.DataFrame({"url":[URL_1]})
         df.T.apply(self.instance.applyRefer)
         self.assertEquals(self.instance.data['test'][0], "MySource")
-    
+
+    @mock.patch('requests.get', mock.MagicMock(side_effect = lambda x,cookies: mock_response))
     def test_iter_vendors(self):
         AC = adc.ActionCache("username", "password", mock.MagicMock())
         AC.cookie = {}
