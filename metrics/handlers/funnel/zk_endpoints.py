bd90573055cdc59bc44ae9e5eb1bc4248c2ec851
rarcher2011 <richarcher1@ricks-MacBook-Pro.local>
2016-01-25 17:24:58 -0500

    add zk_endpoints to write helper functions to interface with zookeeper

---
+++ metrics/handlers/funnel/zk_endpoints.py
@@ -0,0 +1,37 @@
+import tornado.web
+import ujson
+import pandas
+import lib.zookeeper.zk_endpoint as zk
+from handlers.base import BaseHandler
+from action_auth import ActionAuth
+from lib.helpers import APIHelpers
+
+class ZKHandler(BaseHandler, ActionAuth,APIHelpers):
+
+    def initialize(self, db=None, **kwargs):
+            self.db = db
+
+    def initialize(self, db=None, **kwargs):
+        self.db = db
+
+    @tornado.web.authenticated
+        def post(self):
+            data = self.request.body
+            found = zk.find_advertiser_child_num(data["advertiser"])
+            if(found >=0):
+                zk.add_advertiser_pattern(data["advertiser"],data["pattern"])
+            else:
+                zk.add_advertiser(data["advertiser"])
+                if data["pattern"]:
+                    zk.add_advertiser_pattern(data["advertiser"],data["pattern"])
+            self.write_response(True)
+            except Exception, e:
+                self.write_response(str(e),e)
+
+    @tornado.web.authenticated
+    def get(self):
+        try:
+            self.write_response(zk.zk.get_tree())
+            #self.write_response(Convert.df_to_values(results))
+        except Exception, e:
+            self.write_response(str(e),e)
