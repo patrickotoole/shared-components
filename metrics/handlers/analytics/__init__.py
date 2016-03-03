1e8a8e34d9deba33be93243566c21ed569aa98f7
rarcher2011 <richardjamesarcher@gmail.com>
2016-03-03 19:58:44 +0000

    add keyword cache script, handler and endpoint

--- metrics/handlers/analytics/__init__.py
+++ metrics/handlers/analytics/__init__.py
@@ -6,7 +6,7 @@ from domains.user.handler import VisitDomainsHandler
 from visit_events import VisitEventsHandler
 from served_events import ServedEventsHandler
 
-from mongo_domains import DomainsMongoHandler
+#from mongo_domains import DomainsMongoHandler
 from visit_avails import VisitAvailsHandler
 from search import *
 from domain_idf import DomainIDFHandler
