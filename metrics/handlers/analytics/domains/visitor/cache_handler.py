e6006f56f0099d0b7ab5b6139c870385f82c1f5c
rickotoole <rick@rockerbox.com>
2016-03-05 17:51:35 +0000

    BUGFIX: before and after display

--- metrics/handlers/analytics/domains/visitor/cache_handler.py
+++ metrics/handlers/analytics/domains/visitor/cache_handler.py
@@ -27,6 +27,7 @@ class ActionDashboardHandler(BaseHandler):
             q1 = SQL_QUERY_1 % (advertiser, action_type, number)
             segments = self.db.select_dataframe(q1)
             data = {'domains':[]}
+            logging.info("Starting domain cache with limit %s..." % int(number))
             for current_segment in segments.ix[:int(number)].iterrows():
                 c_seg = current_segment[1]["url_pattern"]
                 q2 = SQL_QUERY_4 % (c_seg, advertiser)
@@ -36,6 +37,8 @@ class ActionDashboardHandler(BaseHandler):
                 current_data = seg_data.T.to_dict().values()
                 to_append = {"key":current_segment[1]["url_pattern"], "action_type": current_segment[1]["action_type"], "values":current_data}
                 data['domains'].append(to_append)
+            logging.info("Finished domain cache with limit %s." % int(number))
+
             return data
         else:
             print SQL_QUERY_4 % (advertiser, url_pattern)
