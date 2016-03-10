adc80b48fbc2eec29b0abaa0bf32a3b0cad7b337
rarcher2011 <richardjamesarcher@gmail.com>
2016-03-10 20:09:22 +0000

    update vendor pattern and vendor to action with new cache script imports
    Conflicts:
    	metrics/lib/caching/vendor_to_action.py

--- metrics/lib/caching/vendor_to_action.py
+++ metrics/lib/caching/vendor_to_action.py
@@ -1,6 +1,6 @@
 from link import lnk
 import requests, json, logging
-import action_dashboard_cache as adc
+import cache_interface as ci
 from kazoo.client import KazooClient
 
 VENDOR_QUERY = "select url_patterns, vendor from vendor_patterns where medium is not null and medium_type is not null"
@@ -16,25 +16,25 @@ def sumResults(results_list):
         sum_list.append(i['visits'])
     return sum(sum_list)
 
-def buildIter(AC,advertiser):
+def buildIter(crusher,advertiser):
     def iter_vendors(series):
         logging.info("making request for %s" % series.url_patterns)
         import ipdb; ipdb.set_trace()
         if len(series.url_patterns[0])>1:
-            rr = requests.get(PATTERN_URL.format(series.url_patterns[0]), cookies=AC.cookie)
+            rr = crusher.get(PATTERN_URL.format(series.url_patterns[0]))
         else:
             rr = crusher.get(PATTERN_URL.format(series.url_patterns))
         results_count = sumResults(rr['results'])
         logging.info("received data for advertiser %s and pattern %s" % (advertiser, series.url_patterns))
         existing_urls ={"urls":[]}
-        exists = requests.get(INSERT_URL, cookies=AC.cookie)
+        exists = crusher.get(INSERT_URL)
         try:
             for e in exists.json['response']:
                 existing_urls['urls'].append(e["action_name"])
             if results_count>0 and series.vendor not in existing_urls["urls"]:
                 json_obj["action_name"] = series.url_patterns
                 json_obj["url_pattern"] = [series.url_patterns]
-                r = requests.post(INSERT_URL, data = json.dumps(json_obj), cookies=AC.cookie)
+                r = crusher.post(INSERT_URL, data = json.dumps(json_obj))
                 logging.info("status from response was %s and response was %s" % (r.status_code, r.json()))
             else:
                 logging.info("empty json results for %s or series vendor already existings for advertiser" % (series.url_patterns, advertiser))
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     if not options.username:
         sql = lnk.dbs.rockerbox
         vendors = sql.select_dataframe(VENDOR_QUERY)
-        advertisers = adc.get_all_advertisers(sql)
+        advertisers = ci.get_all_advertisers(sql)
         for advertiser in advertisers:
             crusher = lnk.api.crusher
             crusher.base_url = "http://beta.crusher.getrockerbox.com"
@@ -70,7 +70,7 @@ if __name__ == "__main__":
             crusher.password = advertiser[1]
             crusher.authenticate()
             logging.info("populating action table for vendors for advertiser %s" % advertiser)
-            vendors.T.apply(buildIter(AC,advertiser))
+            vendors.T.apply(buildIter(crusher,advertiser))
     else:
         sql = lnk.dbs.rockerbox
         vendors = sql.select_dataframe(VENDOR_QUERY)
@@ -81,5 +81,5 @@ if __name__ == "__main__":
         crusher.authenticate()
         advertiser = options.username.replace("a_","")
         logging.info("populating action table for vendors for advertiser %s" % advertiser)
-        vendors.T.apply(buildIter(AC,advertiser))
+        vendors.T.apply(buildIter(crusher,advertiser))
 
