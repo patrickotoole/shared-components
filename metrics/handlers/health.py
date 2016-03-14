b68d113c9c8f7ec0bce1b85780e77a6925cd5ffa
rarcher2011 <richardjamesarcher@gmail.com>
2016-03-14 16:06:18 +0000

    update health handler to respon with 0 or 1

--- metrics/handlers/health.py
+++ metrics/handlers/health.py
@@ -18,37 +18,45 @@ class HealthHandler(BaseHandler):
         
 
     def check_buffers(self):
-        if self.db is None: self.write('{"status":0}')
+        if self.db is None: self.write('0')
         else:
             bools = {
                 key: 1 if value and (value > (datetime.now() - timedelta(seconds=60))) else 0
                 for key,value in kafka_queue.ACTIVE_QUEUES.items() 
             }
+            #self.write("1")
             self.write(ujson.dumps(bools))
         self.finish()
  
 
     def check_mysql(self):
-        if self.db is None: self.write('{"status":0}')
+        if self.db is None: self.write('0')
         else:
             df = self.db.select_dataframe("select 1 status from advertiser")
             d = Convert.df_to_values(df)[0]
-            self.write(ujson.dumps(d))
+            if ujson.dumps(d['status'])=='1':
+                self.write('1')
+            else:
+                self.write('0')
         self.finish()
         
     def check_spark_sql(self):
-        if self.spark_sql is None: self.write('{"status":0}')
+        if self.spark_sql is None: self.write('0')
         else:
             df = self.spark_sql.select_dataframe("select 1 status from advertiser_ref limit 1")
             d = Convert.df_to_values(df)[0]
-            self.write(ujson.dumps(d))
+            if ujson.dumps(d['status'])=='1':
+                self.write('1')
+            else:
+                self.write('0')
         self.finish()
          
     def check_cassandra(self):
-        if self.cassandra is mocks.cassandra.CASSANDRA: self.write('{"status":0}')
+        if self.cassandra is mocks.cassandra.CASSANDRA: self.write('0')
         else:
-            df = self.cassandra.select_dataframe("SELECT * FROM rockerbox.visitor_domains limit 1")
-            if len(df) == 1: self.write('{"status":1}')
+            df = self.cassandra.select_dataframe("SELECT * FROM rockerbox.visitor_domains_full limit 1")
+            if len(df) == 1:
+                self.write('1')
         self.finish()
         
     @tornado.web.asynchronous
@@ -59,5 +67,5 @@ class HealthHandler(BaseHandler):
            fn(self)
            return 
 
-       self.write('{"status":0}')
+       self.write('0')
        self.finish() 
