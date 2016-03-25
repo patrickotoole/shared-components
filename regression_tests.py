f59c3ba1a68590eea0acea22f0d92f3669c2cdff
rarcher2011 <richardjamesarcher@gmail.com>
2016-03-25 23:06:09 +0000

    update regression tests to include partial sql checks

--- regression_tests.py
+++ regression_tests.py
@@ -105,6 +105,133 @@ def checkAdvertiserCache(advertiser, crusher):
     print results
     return True
 
+def checkAdvertiserDB(advertiser):
+    db = lnk.dbs.rockerbox
+    results = {}
+    segments = db.select_dataframe(SQL1.format(advertiser))
+    for pattern in segments.iterrows():
+        print pattern[1]['url_pattern']
+        results[pattern[1]['url_pattern']] = {}
+        #domains cache
+        try:
+            sl = "select * from action_dashboard_cache where advertiser={} and url_pattern={}"
+            r1 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r1) >0:
+                results[pattern[1]['url_pattern']]['domains'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['domains'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['domains'] = "Fail"
+            print "fail"
+        #domains full
+        try:
+            sl = "select * from full_domain_cache_test where advertiser={} and url_pattern={}"
+            r2 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r2) >0:
+                results[pattern[1]['url_pattern']]['domains_full'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['domains_full'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['domains_full'] = "Fail"
+            print "fail"
+        #keywords
+        try:
+            sl = "select * from keyword_cache_test where advertiser={} and url_pattern={}"
+            r3 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r3) >0:
+                results[pattern[1]['url_pattern']]['keywords'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['keywords'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['keywords'] = "Fail"
+            print "fail"
+        #onsite sessions
+        try:
+            sl = "select * from uids_only_sessions_cache where advertiser={} and url_pattern={}"
+            r4 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r4) >0:
+                results[pattern[1]['url_pattern']]['onsite_sessions'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['onsite_sessions'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['onsite_sessions'] = "Fail"
+            print "fail"
+        #onsite visits
+        try:
+            sl = "select * from uids_only_visits_cache where advertiser={} and url_pattern={}"
+            r5 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r5) >0:
+                results[pattern[1]['url_pattern']]['onsite_visits'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['onsite_visits'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['onsite_visits'] = "Fail"
+            print "fail"
+        #before and after
+        try:
+            sl = "select * from transform_before_and_after_cache where advertiser={} and url_pattern={}"
+            r6 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r6) >0:
+                results[pattern[1]['url_pattern']]['before_n_after'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['before_n_after'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['before_n_after'] = "Fail"
+            print "fail"
+        #hourly
+        try:
+            sl = "select * from transform_hourly_cache where advertiser={} and url_pattern={}"
+            r7 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r7) >0:
+                results[pattern[1]['url_pattern']]['hourly'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['hourly'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['hourly'] = "Fail"
+            print "fail"
+        #sessions
+        try:
+            sl = "select * from transform_sessions_cache where advertiser={} and url_pattern={}"
+            r8 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r8) >0:
+                results[pattern[1]['url_pattern']]['sessions'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['sessions'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['sessions'] = "Fail"
+            print "fail"
+        #models
+        try:
+            sl = "select * from transform_model_cache where advertiser={} and url_pattern={}"
+            r9 = db.select_dataframe(sl.format(advertiser, pattern[1]['url_pattern']))
+            if len(r9) >0:
+                results[pattern[1]['url_pattern']]['models'] = "Win"
+                print "win"
+            else:
+                results[pattern[1]['url_pattern']]['models'] = "Fail"
+                print "fail"
+        except:
+            results[pattern[1]['url_pattern']]['models'] = "Fail"
+            print "fail"
+    print results
+    return True
+
 def runRegressions(advertiser, url):
     return True
 
