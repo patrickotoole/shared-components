b6cd72e7dd96f8efd035f85bede52e0191d3bf01
rickotoole <rick@rockerbox.com>
2016-03-25 18:57:19 +0000

    added subscription to permissions

--- metrics/handlers/subscription.py
+++ metrics/handlers/subscription.py
@@ -76,11 +76,12 @@ class SubscriptionDatabase:
             return False
 
     @decorators.deferred
-    def bill_customer(self,user_id,customer_id,amount):
+    def bill_customer(self,user_id,customer_id,subscription_id,amount):
         billing = stripe.Charge.create(
             amount=amount,
             currency="usd",
-            customer=customer_id
+            customer=customer_id,
+            metadata= {"subscription_id": subscription_id}
         )
 
         billing_id = billing.id
@@ -88,26 +89,108 @@ class SubscriptionDatabase:
 
         Q = insert_into_or_update(
             "user_stripe_customer_billing",
-            ["user_id","customer_token","billing_token","last_activity","status","amount_cents","response"],
+            ["user_id","customer_token","billing_token","subscription_id","last_activity","status","amount_cents","response"],
             ["user_id","customer_token","billing_token"]
         )
-        prepped = Q % (user_id,customer_id,billing_id,self.time_string(),billing.status,billing.amount,response_json)
+        prepped = Q % (user_id,customer_id,billing_id,subscription_id,self.time_string(),billing.status,billing.amount,response_json)
         logging.info(prepped)
         self.db.execute(prepped)
 
         if billing.status == "succeeded":
-
+            prepped = "UPDATE subscription set active = 1 where subscription_id = %s" % subscription_id
+            self.db.execute(prepped)
             return True
         return False
 
+    def create_permissions(self,user_name,user_id,subscription_id,cursor=None):
+        Q = "SELECT permissions_id from permissions_subscription where subscription_id = %s"
+        permissions_id = self.db.select_dataframe(Q % subscription_id).ix[0,'permissions_id']
+        if permissions_id:
 
-        
+            pixel_source_name = self.current_advertiser_name
 
-        
-        
-        
+            Q = insert_into_or_update("permissions",["name","last_activity"])
+            prepped = Q % ("Subscription: %s" % pixel_source_name, self.time_string())
+            cursor.execute(prepped)     
+            permissions_id = cursor.lastrowid
 
-    
+            Q = insert_into_or_update(
+                "permissions_subscription",
+                ["permissions_id","subscription_id","last_activity"]
+            )
+            prepped = Q % (permissions_id,subscription_id,self.time_string())
+            cursor.execute(prepped)     
+
+            Q = insert_into_or_update(
+                "permissions_advertiser",
+                ["permissions_id","external_advertiser_id","last_activity"]
+            )
+            prepped = Q % (permissions_id,self.current_advertiser,self.time_string())
+            cursor.execute(prepped)     
+
+            Q = insert_into_or_update(
+                "user_permissions",
+                ["user_id","permissions_id","last_activity"]
+            )
+            prepped = Q % (user_id,permissions_id,self.time_string())
+            cursor.execute(prepped)     
+
+            logging.info("Created permissions: %s" % permissions_id)
+        else:
+            logging.info("Using previous permissions: %s" % permissions_id)
+
+
+        return permissions_id
+
+
+
+
+    @decorators.deferred
+    @decorators.multi_commit_cursor
+    def create_plan(self,user_name,user_id,amount,cursor=None):
+        Q = "SELECT subscription_id, active from subscription where owner_user_id = %s"
+        df = self.db.select_dataframe(Q % user_id)
+        subscription_id = df.ix[0,"subscription_id"]
+        active = df.ix[0,"active"]
+
+        if subscription_id and not active:
+            pixel_source_name = self.current_advertiser_name
+
+            Q = insert_into_or_update("subscription", ["name","owner_user_id","last_activity","amount_cents"])
+            prepped = Q % ("%s - %s" % (user_name,pixel_source_name),user_id,self.time_string(),amount)
+            logging.info(prepped)
+            cursor.execute(prepped)
+            subscription_id = cursor.lastrowid
+            logging.info("Created / Updated subscription: %s" % subscription_id)
+        else:
+            logging.info("Using previous subscription: %s" % permissions_id)
+
+        permissions_id = self.create_permissions(user_name,user_id,subscription_id,cursor)
+
+        return (subscription_id, active)
+
+
+    @decorators.deferred
+    @decorators.multi_commit_cursor
+    def disable_plan(self,user_name,user_id,amount,cursor=None):
+        Q = "SELECT subscription_id, active from subscription where owner_user_id = %s"
+        df = self.db.select_dataframe(Q % user_id)
+        subscription_id = df.ix[0,"subscription_id"]
+        active = df.ix[0,"active"]
+
+        if subscription_id and active:
+            pixel_source_name = self.current_advertiser_name
+
+            Q = "UPDATE subscription set active = 0 where owner_user_id = %s "
+            prepped = Q % user_id
+            logging.info(prepped)
+            cursor.execute(prepped)
+
+        return (subscription_id, active)
+
+
+            
+
         
 
 class SubscriptionHandler(BaseHandler,SubscriptionDatabase):
@@ -120,15 +203,22 @@ class SubscriptionHandler(BaseHandler,SubscriptionDatabase):
         obj = ujson.loads(self.request.body)
 
         token = obj['token']
+        AMOUNT = obj['amount_cents']
         user_name = self.current_user
 
         user_id = yield self.get_user_id(user_name)
         token = yield self.save_token(user_id,token)
-        customer = yield self.token_to_customer(user_name,user_id,token)
+        customer_id = yield self.token_to_customer(user_name,user_id,token)
+        subscription_id, active_subscription = yield self.create_plan(user_name,user_id,AMOUNT)
 
-        if customer is not False:
-            AMOUNT = obj['amount_cents']
-            bill = yield self.bill_customer(user_id,customer,AMOUNT)
+        if active_subscription:
+            self.write('{"error":"already have an active subscription"}')
+            self.finish()
+            return
+
+        if customer_id is not False:
+            
+            bill = yield self.bill_customer(user_id,customer_id,subscription_id,AMOUNT)
 
             if bill is True:
                 self.write('{"success":true}')
@@ -138,9 +228,22 @@ class SubscriptionHandler(BaseHandler,SubscriptionDatabase):
         self.write('{"failure":true}')
         self.finish()
  
+    @defer.inlineCallbacks
+    def unsubscribe(self):
+        obj = ujson.loads(self.request.body)
+
+        token = obj['token']
+        user_name = self.current_user
+        self.disable_plan(user_name,user_id)
+
 
 
     @tornado.web.authenticated
     @tornado.web.asynchronous
     def post(self):
         self.save()
+
+    @tornado.web.authenticated
+    @tornado.web.asynchronous
+    def delete(self):
+        self.unsubscribe()
