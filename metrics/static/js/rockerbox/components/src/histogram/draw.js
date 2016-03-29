18b0bd0769a7e5ed6aa29d1ce1c93f8d5493042c
Farid el Nasire <iam@faridelnasire.com>
2016-03-29 11:42:32 -0400

    Finished histogram tooltips

--- metrics/static/js/rockerbox/components/src/histogram/draw.js
+++ metrics/static/js/rockerbox/components/src/histogram/draw.js
@@ -54,7 +54,46 @@ function draw() {
       })
       .attr('stroke', 'white')
       .attr('stroke-width', '2px')
-      .attr('height', bar_height);
+      .attr('height', bar_height)
+      .on('mouseenter', function(x, i, y, z) {
+        // debugger;
+        d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip')
+          .attr('display', 'block')
+
+        d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip rect')
+          .attr('y', function() {
+            return -(bar_height + 5) + (i * bar_height);
+          })
+
+        var a1 = 1;
+        var a2 = -7;
+        var b1 = 11;
+        var b2 = -7;
+        var c1 = 1;
+        var c2 = 1;
+
+        var a = (a1 + 75) + ',' + (a2 + (i * bar_height));
+        var b = (b1 + 75) + ',' + (b2 + (i * bar_height));
+        var c = (c1 + 75) + ',' + (c2 + (i * bar_height));
+
+        d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip polygon')
+          .attr('y', function() {
+            return -(bar_height + 5) + (i * bar_height);
+          })
+          .attr('points', '75,75 91,75 83,85')
+          .attr('points', a + ' ' + b + ' ' + c)
+
+
+        d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip text')
+          .attr('y', function() {
+            return -12 + (i * bar_height);
+          })
+          .text(x.value)
+      })
+      .on('mouseleave', function() {
+        d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip')
+          .attr('display', 'none')
+      });
 
     this._base.svg.selectAll('.label')
       .data(data, function(x) {
@@ -74,6 +113,41 @@ function draw() {
       })
       .attr('line-height', bar_height)
       .style('color', '#000');
+
+
+    var tooltip = d3.select(this._base.svg)[0][0].append('g')
+      .attr('display', 'none')
+      .attr('class', 'histogram-tooltip');
+
+    tooltip
+      .append('rect')
+      .attr('fill', '#000')
+      .attr('width', '71')
+      .attr('height', '18')
+      .attr('x', '76')
+      .attr('fill-opacity', '.9')
+      .attr('y', function() {
+        // return 0 + (x.key * 15);
+        return 0;
+      })
+
+    tooltip
+      .append('text')
+      .attr('x', '110')
+      .attr('fill', '#fff')
+      .attr('text-anchor', 'middle')
+      .text('4')
+
+    tooltip
+      .append('polygon')
+      .attr('fill', '#000')
+      .attr('width', '16')
+      .attr('height', '10')
+      .attr('fill-opacity', '.9')
+
+      // <text x="70px" text-anchor="end" y="74" width="75" line-height="20" style="color: rgb(0, 0, 0);">5 - 6</text>
+      // .attr('stroke', '#ccc')
+      // .attr('stroke-width', '1px')
   }
 }
 
