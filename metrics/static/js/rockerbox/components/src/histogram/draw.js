494773ac7ebbb811468f0f662c961ae2fcf096cd
Farid el Nasire <iam@faridelnasire.com>
2016-03-30 15:35:48 -0400

    Turned tooltip into a polygon

--- metrics/static/js/rockerbox/components/src/histogram/draw.js
+++ metrics/static/js/rockerbox/components/src/histogram/draw.js
@@ -60,33 +60,34 @@ function draw() {
         d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip')
           .attr('display', 'block')
 
-        d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip rect')
-          .attr('y', function() {
-            return -(bar_height + 5) + (i * bar_height);
-          })
-
         var a1 = 1;
-        var a2 = -7;
-        var b1 = 11;
-        var b2 = -7;
-        var c1 = 1;
-        var c2 = 1;
+        var a2 = -25;
+        var b1 = 70;
+        var b2 = -25;
+        var c1 = 70;
+        var c2 = -5;
+        var d1 = 7;
+        var d2 = -5;
+        var e1 = 1;
+        var e2 = 2;
 
         var a = (a1 + 75) + ',' + (a2 + (i * bar_height));
         var b = (b1 + 75) + ',' + (b2 + (i * bar_height));
         var c = (c1 + 75) + ',' + (c2 + (i * bar_height));
+        var d = (d1 + 75) + ',' + (d2 + (i * bar_height));
+        var e = (e1 + 75) + ',' + (e2 + (i * bar_height));
 
         d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip polygon')
           .attr('y', function() {
             return -(bar_height + 5) + (i * bar_height);
           })
           .attr('points', '75,75 91,75 83,85')
-          .attr('points', a + ' ' + b + ' ' + c)
+          .attr('points', a + ' ' + b + ' ' + c + ' ' + d + ' ' + e)
 
 
         d3.select(d3.select(this)[0][0].parentElement).select('.histogram-tooltip text')
           .attr('y', function() {
-            return -12 + (i * bar_height);
+            return -11 + (i * bar_height);
           })
           .text(x.value)
       })
@@ -120,16 +121,11 @@ function draw() {
       .attr('class', 'histogram-tooltip');
 
     tooltip
-      .append('rect')
+      .append('polygon')
       .attr('fill', '#000')
-      .attr('width', '71')
-      .attr('height', '18')
-      .attr('x', '76')
+      .attr('width', '16')
+      .attr('height', '10')
       .attr('fill-opacity', '.9')
-      .attr('y', function() {
-        // return 0 + (x.key * 15);
-        return 0;
-      })
 
     tooltip
       .append('text')
@@ -137,17 +133,6 @@ function draw() {
       .attr('fill', '#fff')
       .attr('text-anchor', 'middle')
       .text('4')
-
-    tooltip
-      .append('polygon')
-      .attr('fill', '#000')
-      .attr('width', '16')
-      .attr('height', '10')
-      .attr('fill-opacity', '.9')
-
-      // <text x="70px" text-anchor="end" y="74" width="75" line-height="20" style="color: rgb(0, 0, 0);">5 - 6</text>
-      // .attr('stroke', '#ccc')
-      // .attr('stroke-width', '1px')
   }
 }
 
