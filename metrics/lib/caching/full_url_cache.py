b002cc49ac4a2ff0728db6da1e9b537bc3606136
rarcher2011 <richardjamesarcher@gmail.com>
2016-04-08 20:40:13 +0000

    update caching scripts with batch insert

--- metrics/lib/caching/full_url_cache.py
+++ metrics/lib/caching/full_url_cache.py
@@ -1,5 +1,8 @@
 import pickle, pandas, json
 from link import lnk
+from lib.pandas_sql import s as _sql
+import datetime
+import logging
 
 INSERT_QUERY ="INSERT INTO full_domain_cache_test (advertiser, url, count, uniques, url_pattern) VALUES ('{}', '{}',{}, {}, '{}')"
 REPLACE_QUERY="REPLACE INTO full_domain_cache_test (advertiser, url, count, uniques, url_pattern) VALUES ('{}', '{}',{}, {}, '{}')"
@@ -11,6 +14,32 @@ def get_connectors():
         "zk": {},
         "cassandra": lnk.dbs.cassandra
     }
+
+
+def insert(df, advertiser, segment_name, db):
+    current_datetime = datetime.datetime.now()
+    df_filtered = df.filter(['url', 'count'])
+    table_name ="full_domain_cache_test"
+    keys = ['advertiser', 'url_pattern']
+    batch_num = int(len(df_filtered) / 50)+1
+    for batch in range(0, batch_num):
+        if batch==0:
+            to_insert = df_filtered.ix[0:50]
+            to_insert['ts'] = [current_datetime] * len(to_insert)
+            to_insert['url_pattern'] = [segment_name] * len(to_insert)
+            to_insert['advertiser'] = [advertiser] * len(to_insert)
+        else:
+            to_insert = df_filtered.ix[batch*50+1:(batch+1)*50]
+            to_insert['ts'] = [current_datetime] * len(to_insert)
+            to_insert['url_pattern'] = [segment_name] * len(to_insert)
+            to_insert['advertiser'] = [advertiser] * len(to_insert)
+        if len(to_insert)>0:
+            try:
+                to_insert['url'] = to_insert['url'].map(lambda x : x.encode('utf-8'))
+                _sql._write_mysql(to_insert, table_name, list(to_insert.columns), db, keys)
+                logging.info("inserted %s records for advertiser username (includes a_) %s" % (len(to_insert), advertiser))
+            except:
+                logging.info("error with df for %s and %s" % (segment_name, advertiser))
 
 def make_request(advertiser,pattern, base_url, featured):
     crusher = lnk.api.crusher
@@ -44,7 +73,9 @@ def runner(advertiser_name, pattern, base_url, cache_date, indentifiers="test", 
     db = connectors['crushercache']
     featured = featured_seg(advertiser_name, pattern)
     urls = make_request(advertiser_name, pattern, base_url, featured)
-    for url in urls:
-        add_to_table(advertiser_name, pattern, url, db)
+    df = pandas.DataFrame(urls)
+    insert(df, advertiser_name, pattern, db)
+    #for url in urls:
+    #add_to_table(advertiser_name, pattern, url, db)
 
 
