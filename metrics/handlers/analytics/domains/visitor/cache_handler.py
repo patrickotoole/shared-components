484424b2e52fb73fefd7725531de59fb8a161c22
rarcher2011 <richardjamesarcher@gmail.com>
2016-04-21 18:30:30 +0000

    fix several bugs with cache and full cache handler

--- metrics/handlers/analytics/domains/visitor/cache_handler.py
+++ metrics/handlers/analytics/domains/visitor/cache_handler.py
@@ -11,10 +11,9 @@ from lib.helpers import Convert
 from twisted.internet import defer
 from lib.helpers import decorators
 
-DOMAINS = "SELECT domain, count FROM action_dashboard_cache WHERE url_pattern = '%s' and advertiser = '%s'"
-DOMAINS_DATE = "SELECT domain, count FROM domains_cache WHERE url_pattern = '%s' and advertiser = '%s' and record_date='%s'"
-DOMAINS_FILTER = "SELECT zipped FROM cache_domains_w_filter_id WHERE url_pattern = '%s' and advertiser = '%s' and filter_id = '%s'"
-CATEGORIES = "SELECT c.domain, c.category_name, p.parent_category_name FROM domain_category c JOIN category p ON c.category_name = p.category_name where c.domain in (%s)"
+DOMAINS = "SELECT domain, count FROM action_dashboard_cache WHERE url_pattern = '{}' and advertiser = '{}'"
+DOMAINS_DATE = "SELECT domain, count FROM domains_cache WHERE url_pattern = '{}' and advertiser = '{}' and record_date='{}'"
+DOMAINS_FILTER = "SELECT zipped FROM cache_domains_w_filter_id WHERE url_pattern = '{}' and advertiser = '{}' and filter_id = '{}'"
 DATE_FALLBACK = "select distinct record_date from domains_cache where url_pattern='{}' and advertiser='{}' order by record_date DESC"
 
 class ActionDashboardHandler(BaseHandler):
@@ -37,7 +36,7 @@ class ActionDashboardHandler(BaseHandler):
 
 
     def get_one_filter_id(self, url_pattern, advertiser, action_id):
-        zipped_data = self.crushercache.select_dataframe(DOMAINS_FILTER % (url_pattern,advertiser, action_id))
+        zipped_data = self.crushercache.select_dataframe(DOMAINS_FILTER.format(url_pattern,advertiser, action_id))
         decode_data = codecs.decode(zipped_data.ix[0]['zipped'], 'hex')
         final_data =zlib.decompress(decode_data)
         return final_data
@@ -45,21 +44,21 @@ class ActionDashboardHandler(BaseHandler):
     @decorators.deferred
     def defer_get_actions(self, advertiser, number, action_type, url_pattern, filter_date=False):
         if filter_date:
-            seg_data = self.crushercache.select_dataframe(DOMAINS_DATE % (url_pattern,advertiser, filter_date))
+            results = self.crushercache.select_dataframe(DOMAINS_DATE.format(url_pattern,advertiser, filter_date))
         else:
             import datetime
             now = datetime.datetime.now()- datetime.timedelta(days=1)
-            now_date = now.strftime('%Y-%m-%d')
-            seg_data = self.crushercache.select_dataframe(DOMAINS_DATE % (url_pattern,advertiser, now_date))
-            if len(seg_data)==0:
+            now_date = str(now.strftime('%Y-%m-%d'))
+            results = self.crushercache.select_dataframe(DOMAINS_DATE.format(url_pattern,advertiser, now_date))
+            if len(results)==0:
                 datefallback = self.crushercache.select_dataframe(DATE_FALLBACK.format(url_pattern, advertiser))
                 if len(datefallback) >0:
                     now_date = str(datefallback['record_date'][0])
-                    seg_data = self.crushercache.select_dataframe(DOMAINS_DATE % (url_pattern,advertiser, now_date))
-        categories = self.get_idf(self.db, list(set(seg_data.domain)))
-        joined = seg_data.merge(categories,on="domain")
-        seg_data = joined.fillna(0)
-        return seg_data
+                    results = self.crushercache.select_dataframe(DOMAINS_DATE.format(url_pattern,advertiser, now_date))
+        categories = self.get_idf(self.db, list(set(results.domain)))
+        joined = results.merge(categories,on="domain")
+        results = joined.fillna(0)
+        return results
 
     @custom_defer.inlineCallbacksErrors
     def get_actions(self, advertiser, number, action_type, url_pattern, action_id=False, filter_date=False):
