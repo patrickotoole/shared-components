1d110b94b816a96e5719eb7e6d1eee6635a7e9e9
Farid el Nasire <iam@faridelnasire.com>
2016-04-22 12:08:58 -0400

    Changed SASS compile script to not minify CSS
    Also made sure sass cache folder isn't being included

--- metrics/start_sass.sh
+++ metrics/start_sass.sh
@@ -1,1 +1,2 @@
-sass --watch static/sass/style.scss:static/css/style.css --style compressed
+sass --watch static/sass/style.scss:static/css/style.css
+# sass --watch static/sass/style.scss:static/css/style.css --style compressed
