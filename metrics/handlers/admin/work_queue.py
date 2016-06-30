2f276b11fb30e0d0d707494e1294e6e0355b1b02
rickotoole <rick@rockerbox.com>
2016-06-30 01:32:36 +0000

    added UI to submit jobs and table for explaination

--- metrics/handlers/admin/work_queue.py
+++ metrics/handlers/admin/work_queue.py
@@ -1,5 +1,5 @@
 import tornado.web
-import ujson
+import json
 import pandas
 import StringIO
 
@@ -11,14 +11,14 @@ from lib.helpers import *
 class WorkQueueHandler(tornado.web.RequestHandler):
 
     def initialize(self, zookeeper=None, *args, **kwargs):
+        self.db = kwargs.get("crushercache",None)
         self.zookeeper = zookeeper
 
     @decorators.formattable
     def get_content(self,data):
         
         def default(self,data):
-            o = Convert.df_to_json(data)
-            html = data.to_html(classes=["dataframe"])
+            o = json.dumps([{"class":"script","type":"select","key":"script","values":data}])
 
             paths = """
             <div class="col-md-3">
@@ -44,7 +44,143 @@ class WorkQueueHandler(tornado.web.RequestHandler):
             <br><br>
             """
 
-            self.render("admin/datatable.html", data=html, paths=paths)
+            js = """
+<style>
+.content .label {
+  min-width:100px;color:black;display:inline-block
+}
+</style>
+<script>
+var content = d3.select(".content");
+
+var data = """ + o + """
+
+var draw_parameters = function(target,params) {
+  target.text("")
+  var param_row = target.selectAll(".param").data(params)
+
+  param_row.enter()
+    .append("div")
+    .classed("param",true)
+
+  param_row.exit().remove()
+
+  param_row.append("div").classed("label",true)
+    .text(function(x){ return x.key })
+
+  param_row.append("input")
+    .style("width","160px")
+
+
+  param_row.append("div")
+    .style("display","inline-block")
+    .style("font-size","11px")
+    .style("margin-left","10px")
+
+    .text(function(x){return x.description })
+
+
+}
+
+var select_target = content.append("div").classed("selector",true)
+
+var params_target = content.append("div").classed("params",true)
+
+var submit_target = content.append("div").classed("submit",true)
+
+var resp_target = content.append("div").classed("response",true)
+
+
+submit_target.append("button")
+  .text("submit")
+  .style("width","160px")
+  .style("margin-left","100px")
+  .on("click",function(x){
+
+   var d = params_target.datum()
+
+    var obj = {
+      "udf": d.key
+    }
+
+    params_target.selectAll(".param").each(function(y){
+      obj[y.key] = d3.select(this).selectAll("input").node().value;
+    })
+
+    d3.xhr("/jobs")
+      .post(
+        JSON.stringify(obj),
+        function(err,x) {
+          var j = JSON.parse(x.response)
+
+          resp_target.html("Job Submitted: <br><pre>" + x.response + "</pre><br>")
+         
+
+          resp_target.append("a")
+            .attr("href","/jobs/" + j.job_id)
+            .attr("target","_blank")
+            .text("View job status")
+          
+        }
+      )
+
+  })
+
+
+
+
+
+data.map(function(item){
+  var name = item.key,
+    cls= item.class,
+    type = item.type;
+  
+  var label = select_target.selectAll("div.label." + cls)
+    .data([item])
+    .enter()
+    .append("div")
+    .classed("label " + cls,true)
+    .style("color","black")
+    .text(function(x){return x.key })
+
+  var input = select_target.selectAll(type + "." + cls)
+    .data([item])
+    .enter()
+    .append(type)
+    .classed(cls,true)
+    .style("width","160px")
+    .on("change",function(x){
+      var value = this.value
+      var filtered = x.values.filter(function(y){ return y.key == value });
+      if (filtered.length > 0) draw_parameters(params_target.datum(filtered[0]),filtered[0].parameters);
+      select_target.selectAll("div.desc").text(filtered[0].description)
+      return 
+    0})
+
+  var option = input.selectAll("option").data(function(x){return x.values}).enter()
+    .append("option")
+    .text(function(x){
+      return x.key
+    })
+
+  var desc = select_target.selectAll("div.desc." + cls)
+    .data([item])
+    .enter()
+    .append("div")
+    .classed("desc " + cls,true)
+    .style("color","black")
+    .style("margin-left","10px")
+    .style("display","inline-block")
+
+
+
+})
+
+
+</script>
+            """
+
+            self.render("admin/datatable.html", data="", paths=js)
             
 
         yield default, (data,)
@@ -68,28 +204,11 @@ class WorkQueueHandler(tornado.web.RequestHandler):
     def get_data(self):
         import pickle
         import hashlib
-        path_queue = [c for c in self.zookeeper.get_children("/python_queue") ]
-        def parse(x):
-            try:
-                values = pickle.loads(self.zookeeper.get("/python_queue/" + path)[0])
-                job_id = hashlib.md5(self.zookeeper.get("/python_queue/" + path)[0]).hexdigest()
-                print values
-                rvals = values[1]
-                rvals['job_id'] = job_id
-                rvals['path'] = path
-                return rvals
-            except:
-                print "Error parsing pickle job"
-                return False
 
-        in_queue = [parse(path) for path in path_queue]
-        in_queue = [i for i in in_queue if i]
+        df = self.db.select_dataframe("select name `key`, parameters, description from rpc_function_details where active = 1 and deleted = 0")
+        df['parameters'] = df['parameters'].map(ujson.loads)
         
-        if len(in_queue) > 0:
-            df = pandas.DataFrame(in_queue)
-        else:
-            df = pandas.DataFrame({"items":[]})
-        self.get_content(df)
+        self.get_content(df.to_dict("records"))
          
     def get_complete(self,q):
         try:
