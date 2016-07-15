50f599bf4fcc93c8e6b1a8ceecb7f26a2717c782
Farid el Nasire <iam@faridelnasire.com>
2016-07-15 17:57:05 -0400

    Show advertiser_id next to button

--- authentication/slack/server.py
+++ authentication/slack/server.py
@@ -56,6 +56,8 @@ def apivalidation(x):
 
 class IndexHandler(web.RequestHandler):
     def get(self):
+        advertiser_id = self.get_secure_cookie('advertiser')
+        self.write(advertiser_id)
         self.write('<a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=2171079607.55132364375"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>')
         self.finish()
 
