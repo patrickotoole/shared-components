38c244f008151475a51b5002567bd076f9e91673
rarcher2011 <richardjamesarcher@gmail.com>
2016-07-20 17:40:49 +0000

    update content scrapper to push metrics to graphite

--- metrics/lib/content_scrapper.py
+++ metrics/lib/content_scrapper.py
@@ -3,6 +3,8 @@ import logging
 from lib.kafka_queue import KafkaQueue
 from link import lnk
 from twisted.internet import threads
+import graphitewriter as gw
+import sys
 
 URL="http://scrapper.crusher.getrockerbox.com?url=%s"
 
@@ -16,34 +18,64 @@ class counter():
     def get(self, url):
         return self.counter_dict.get(url,0)
 
-class countSend():
-    #add metrics to graphite
-    #and log num deffers, num finished, num pulled, etc.
-    def __init__(self):
-        self.sent = 0
-        self.pulled=0
-
-    def pull(self):
-        self.pulled=self.pulled+1
-    def send(self):
-        self.sent = self.sent+1
-    def getpull(self):
-        return self.pulled
-    def getsent(self):
-        return self.sent
-
-def run(consumer, count):
-    CS = countSend()
+class GraphiteSender():
     import datetime
-    in_5 = datetime.datetime.now() + datetime.timedelta(minutes = 0.5)
-    def get(url):
-        _resp = requests.get((URL % url), auth=('rockerbox', 'rockerbox'))
-        data = _resp.json()
+
+    def __init__(self, cs):
+        self.GW = gw.GraphiteWriter()
+        self.sent_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
+        self.cs = cs
+
+    def start(self):
+        while True:
+            if datetime.datetime.now() > self.sent_time:
+                self.sent_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
+                with self.GW:
+                    self.GW.send("scrapper.content_loop.metrics.pulled_from_kafka", self.cs.get_num_pulled())
+                    self.GW.send("scrapper.content_loop.metrics.num_deferred_created", self.cs.get_num_defer())
+                    self.GW.send("scrapper.content_loop.metrics.responses_from_scrapper", self.cs.get_num_responses())
+                    self.GW.send("scrapper.content_loop.metrics.num_cotent_added_to_kafka", self.cs.get_num_added())
+
+class countSend():
+    def __init__(self):
+        self.removed_from_kafka = 0
+        self.defered_created = 0
+        self.response_from_scrapper = 0
+        self.sent_to_kafka = 0
+
+    def bump_pulled(self):
+        self.removed_from_kafka+=1
+    def bump_defer(self):
+        self.defered_created+=1
+    def deck_defer(self):
+        self.defered_created-=1
+    def bump_responses(self):
+        self.response_from_scrapper+=1
+    def bump_added(self):
+        self.sent_to_kafka+=1
+    def get_num_pulled(self):
+        return self.removed_from_kafka
+    def get_num_defer(self):
+        return self.defered_created
+    def get_num_responses(self):
+        return self.response_from_scrapper
+    def get_num_added(self):
+        return self.sent_to_kafka
+
+def run(consumer, count, CS):
+    import datetime
+    in_5 = datetime.datetime.now() + datetime.timedelta(minutes = 5)
+    def get(url, CS):
         try:
+            _resp = requests.get((URL % url), auth=('rockerbox', 'rockerbox'))
+            CS.bump_responses()
+            data = _resp.json()
             title = data['result']['title']
-        except:
+        except Exception as e:
+            print str(e)
             print "could not get title"
             title = None
+        CS.deck_defer()
         return (title, url)
 
     def send(result,  producer, CS):
@@ -52,16 +84,14 @@ def run(consumer, count):
             msg = {"url":url.encode('ascii',  errors='ignore'), "title": title.encode('ascii', errors='ignore')}
             print str(msg)
             producer.send_message(json.dumps(msg))
-            CS.send()
+            CS.bump_added()
             print "Sent"
 
     for message in consumer:
         if message is not None:
-            CS.pull()
+            CS.bump_pulled()
             if datetime.datetime.now() > in_5:
-                print "Pulled: "+ str(CS.getpull())
-                print "Sent: "+ str(CS.getsent())
-                break
+                sys.exit(0)
                 raise Exception
             message_object = json.loads(message.value)
             try:
@@ -70,7 +100,8 @@ def run(consumer, count):
                 count.bump(url)
                 if limit ==25:
                     print url
-                    defr = threads.deferToThread(get, url)
+                    defr = threads.deferToThread(get, url, CS)
+                    CS.bump_defer()
                     defr.addCallback(send, producer,CS)
             except Exception as e:
                 print str(e)
@@ -113,8 +144,10 @@ if __name__ == '__main__':
     consumer = topic.get_simple_consumer()
     connectors = {"crushercache":lnk.dbs.crushercache}
     
-    count = counter()    
+    count = counter()
+    CS = countSend()
+    GS = GraphiteSender(CS)
     from twisted.internet import reactor
-    
-    reactor.callInThread(run, consumer, count)
+    reactor.callInThread(run, consumer, count, CS)
+    reactor.callInThread(GS.start)
     reactor.run()
