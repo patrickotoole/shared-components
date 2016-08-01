1a07a1aef851f3049fa5d1d973a5cc4fbb89268d
rarcher2011 <richardjamesarcher@gmail.com>
2016-08-01 18:03:37 +0000

    add logging instead of prints to topic runner and fix bug with hourly key checks for udf runner add coret file instead of topic runner

--- metrics/lib/caching/add_topic_runner_wq.py
+++ metrics/lib/caching/add_topic_runner_wq.py
@@ -2,27 +2,32 @@ from link import lnk
 import requests
 import ujson
 import time
+import logging
 
 QUERY = "select * from topic_runner_segments"
 
 def check_job(job_id):
-    j = requests.get("http://workqueue.crusher.getrockerbox.com/jobs/%s" % job_id,auth=("rockerbox","rockerbox")).json()
-    return (len(j['entries']) == 0) and (len(j['running']) == 0)
+    try:
+        j = requests.get("http://workqueue.crusher.getrockerbox.com/jobs/%s" % job_id,auth=("rockerbox","rockerbox")).json()
+        return (len(j['entries']) == 0) and (len(j['running']) == 0)
+    except:
+        logging.info("error checking on status")
+        return True 
 
 def run_job(d):
     
-    print "submitting"
+    logging.info("submitting")
     resp = requests.post("http://workqueue.crusher.getrockerbox.com/jobs",data=d,auth=("rockerbox","rockerbox"))
     job_id = resp.json()['job_id']
-    print "submitted!"
+    logging.info("submitted!")
     
     done = False
     while not done:
         time.sleep(10)
-        print "checking..."
+        logging.info("checking...")
         done = check_job(job_id)
 
-    print "job_complete!"
+    logging.info("job_complete!")
 
 def create_json(ADVERTISER, PATTERN, FILTER_ID):
     d1 = ujson.dumps({
@@ -49,10 +54,10 @@ if __name__ == "__main__":
 
     db = lnk.dbs.crushercache
     df = db.select_dataframe(QUERY)
-
+    
     for advertiser_segment in df.iterrows():
         d1, d2 = create_json(advertiser_segment[1]['advertiser'], advertiser_segment[1]['pattern'], advertiser_segment[1]['filter_id'])
-        print "job 1:"
+        logging.info("job 1:")
         run_job(d1)
-        print "job 2:"
+        logging.info("job 2:")
         run_job(d2)
