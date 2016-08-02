315fee1b4ecdbf74ab0732e8faf258b7fcf52f8e
Farid el Nasire <iam@faridelnasire.com>
2016-08-02 13:40:43 -0400

    Fixed table reference in settings

--- authentication/shopify/server.py
+++ authentication/shopify/server.py
@@ -25,7 +25,7 @@ class DBQuery:
 
     def getShop(self, shop_domain):
         sql = "SELECT * FROM %(db_table)s WHERE `shop_domain` = '%(shop_domain)s'" % {
-            "db_table": SETTINGS['db_table'],
+            "db_table": SETTINGS['db']['name'],
             "shop_domain": shop_domain
         }
         df = self.db.select_dataframe(sql)
@@ -136,7 +136,7 @@ class IndexHandler(web.RequestHandler, DBQuery):
         # Save script ID in our db
         new_script_id = data['script_tag']['id']
         sql = 'UPDATE %(db_table)s SET script_id=%(new_script_id)s WHERE shop_id = %(shop_id)d' % {
-            'db_table': SETTINGS['db_table'],
+            'db_table': SETTINGS['db']['name'],
             'new_script_id': new_script_id,
             'shop_id': int(shop['shop_id'])
         }
@@ -157,7 +157,7 @@ class AuthenticationCallbackHandler(web.RequestHandler, DBQuery):
         shop_id = DBQuery.getShopifyID(self, shop_domain, access_token)
 
         sql = "INSERT INTO `%(db_table)s` (`shop_id`, `shop_domain`, `advertiser_id`, `script_id`, `access_token`, `ts_created`) VALUES ('%(shop_id)s', '%(shop_domain)s', '%(advertiser_id)s', NULL, '%(access_token)s', NOW()) ON DUPLICATE KEY UPDATE shop_domain='%(shop_domain)s', access_token='%(access_token)s'" % {
-            'db_table': SETTINGS['db_table'],
+            'db_table': SETTINGS['db']['name'],
             'shop_id': shop_id,
             'shop_domain': shop_domain,
             'advertiser_id': advertiser_id,
