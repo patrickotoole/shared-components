bbc50f6492b7891c316e2a1051bd9dd63e180304
Farid el Nasire <iam@faridelnasire.com>
2016-08-10 13:42:17 -0400

    Fixed integrations landing page

--- metrics/handlers/integrations/handler.py
+++ metrics/handlers/integrations/handler.py
@@ -11,7 +11,9 @@ class IntegrationHandler(BaseHandler):
 
     def get(self):
         advertiser = self.current_advertiser
-        df = self.db.select_dataframe("select * from advertiser_slack where advertiser_id = %s" % advertiser)
+        if advertiser != None:
+            df = self.db.select_dataframe("select * from advertiser_slack where advertiser_id = %s" % advertiser)
+        else:
+            df = []
         self.render("_integrations.html",has_advertiser=len(df))
         return
-
