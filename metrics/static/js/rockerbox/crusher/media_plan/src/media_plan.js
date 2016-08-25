7c44979b79dd556c590a7416daee48145c5a2c63
rickotoole <rick@rockerbox.com>
2016-08-25 20:30:17 +0000

    added media plan and build media plan button to RHS

--- metrics/static/js/rockerbox/crusher/media_plan/src/media_plan.js
+++ metrics/static/js/rockerbox/crusher/media_plan/src/media_plan.js
@@ -164,10 +164,14 @@ function transformData(data) {
        var scale = d3.scale.linear()
          .domain([min,max])
          .range([0,100])
-       var hourly = d3.range(0,24).map(function(i) { return v[i] ? scale(v[i].count) : 0 })
-       var reordered = hourly.slice(3)
-       reordered = reordered.concat(hourly.slice(0,3))
-       return reordered
+       
+       var hours = d3.range(0,24)
+       hours = hours.slice(3).concat(hours.slice(0,3))
+
+       return {
+           "normed": hours.map(function(i) { return v[i] ? scale(v[i].count) : 0 })
+         , "count": hours.map(function(i) { return v[i] ? v[i].count : 0 })
+       }
        //return hourly
     })
     .entries(category_hour)
@@ -182,46 +186,239 @@ MediaPlan.prototype = {
       //debugger
       if (this.data().category_hour == undefined) return this
 
-      this.render_left()
-      this.render_right()
+          var _d = this.data()
+          _d.display_categories = _d.display_categories || {"values":[]}
+          var dd = buildDomains(_d)
+
+      var scaled = transformData(this.data())
+
+      
+      scaled.map(function(x) {
+
+        x.count = x.values.count
+        x.values= x.values.normed
+
+      })
+
+
+      this.render_left(scaled)
+      this.render_right(dd,scaled)
 
 
       return this
     }
-  , render_right: function(d) {
+  , render_right: function(d,row_data) {
 
       var wrapper = d3_updateable(this._target,".rhs","div")
         .classed("rhs col-md-5",true)
 
       var head = d3_updateable(wrapper, "h3","h3")
         .style("margin-bottom","15px")
-        .style("margin-top","10px")
-        .text("Details")
+        .style("margin-top","20px")
+        .text("About the plan")
 
-      var plan_target = d3_updateable(wrapper,".plan-target","div")
+
+      d3_updateable(wrapper,".desc","div")
+        .classed("desc",true)
+        .style("padding","10px")
+        .text("Hindsight has automatically determined the best sites and times where you should be targeting users. The media plan presented below describes the optimizations that can be made to any prospecting or retargeting campaign to lower CPA and save money.")
+
+      var plan_target = d3_updateable(wrapper,".plan-target","div",row_data,function(){return 1})
         .classed("plan-target",true)
+        .style("line-height","20px")
+        .style("min-height","100px")
 
-       var rows = d3_splat(plan_target,".row","div",d,function(x) { return x.key })
+
+      if (row_data.length > 1) {
+        var remainders = row_data.map(function(r) {
+        
+          var to_target = d3.sum(r.mask.map(function(x,i){ return x ? r.count[i] : 0}))
+          var total = d3.sum(r.count)
+          return {
+              total: total
+            , to_target: to_target
+          }
+        })
+
+        var cut = d3.sum(remainders,function(x){ return x.to_target*1.0 })
+        var total = d3.sum(remainders,function(x){ return x.total }) 
+        var percent = cut/total
+
+        var head = d3_updateable(plan_target, "h3.summary","h3")
+          .classed("summary",true)
+          .style("margin-bottom","15px")
+          .style("margin-top","20px")
+          .text("Plan Summary")
+
+
+
+        d3_updateable(plan_target,".what","div")
+          .classed("what",true)
+          .html(function(x) {
+            return "<div style='font-weight:bold;width:200px;padding-left:10px;text-transform:uppercase;display:inline-block'>Potential Ads Served:</div>" + d3.format(",")(total)
+          })
+
+        d3_updateable(plan_target,".amount","div")
+          .classed("amount",true)
+          .html(function(x) {
+            return "<div style='font-weight:bold;width:200px;padding-left:10px;text-transform:uppercase;display:inline-block'>Optimized Ad Serving:</div>" + d3.format(",")(cut) + " (" + d3.format("%")(percent) + ")"
+          })
+
+        d3_updateable(plan_target,".cpa","div")
+          .classed("cpa",true)
+          .html(function(x) {
+            return "<div style='font-weight:bold;width:200px;padding-left:10px;text-transform:uppercase;display:inline-block'>Estimated CPA reduction:</div>" + d3.format("%")(1-percent)
+          })
+
+
+
+
+
+       
+        return
+      }
+
+      var plan_target = d3_updateable(wrapper,".plan-details","div",row_data)
+        .classed("plan-details",true)
+        .style("line-height","20px")
+        .style("min-height","160px")
+
+
+
+      var head = d3_updateable(plan_target, "h3.details","h3")
+        .classed("details",true)
+        .style("margin-bottom","15px")
+        .style("margin-top","20px")
+        .text("Plan Details")
+
+
+
+
+      d3_updateable(plan_target,".what","div")
+        .classed("what",true)
+        .html(function(x) {
+          return "<div style='padding-left:10px;font-weight:bold;width:140px;text-transform:uppercase;display:inline-block'>Category:</div>" + x.key
+        })
+
+      d3_updateable(plan_target,".saving","div")
+        .classed("saving",true)
+        .html(function(x) {
+          console.log(d.count)
+          var percent = d3.sum(x.count,function(z,i) { return x.mask[i] ? 0 : z})/d3.sum(x.count,function(z,i) { return z })
+          return "<div style='padding-left:10px;font-weight:bold;width:140px;text-transform:uppercase;display:inline-block'>Strategy savings:</div>" + d3.format("%")(percent)
+        })
+
+      var when = d3_updateable(plan_target,".when","div",false,function(){ return 1 })
+        .classed("when",true)
+        .style("text-transform","uppercase")
+        .style("font-weight","bold")
+        .style("display","inline-block")
+        .style("width","280px")
+        .html("<div style='padding-left:10px;width:140px;display:inline-block;vertical-align:top'>When to serve:</div>")
+        .datum(function(x) {
+          var bool = false
+          var pos = -1
+          var start_ends = x.mask.reduce(function(p,c) { 
+              pos += 1
+              if (bool != c) {
+                bool = c
+                p.push(pos)
+              }
+              return p
+            },[])
+          var s = ""
+          start_ends.map(function(x,i) {
+            if ((i != 0) && ((i%2) == 0)) s += ", "
+            if (i%2) s += " - "
+
+            if (x == 0) s += "12am"
+            else {
+              var num = (x+1)%12
+              num = num == 0 ? 12 : num
+              s += num + ((x > 11) ? "pm" : "am")
+            }
+           
+          })
+          if ((start_ends.length) % 2) s += " - 12am"
+
+          return s.split(", ")
+        })
+
+       var items = d3_updateable(when,".items","div")
+         .classed("items",true)
+         .style("width","140px")
+         .style("display","inline-block")
+
+       d3_splat(items,".item","div")
+         .classed("item",true)
+         .style("width","140px")
+         .style("display","inline-block")
+         .style("text-transform","none")
+         .style("font-weight","normal")
+         .text(String)
+
+
+
+      var head = d3_updateable(wrapper, "h3.example-sites","h3")
+        .classed("example-sites",true)
+        .style("margin-bottom","15px")
+        .style("margin-top","20px")
+        .text("Example Sites")
+
+
+       var rows = d3_splat(wrapper,".row","div",d.slice(0,15),function(x) { return x.key })
          .classed("row",true)
-         .text(JSON.stringify)
+         .style("line-height","18px")
+         .style("padding-left","20px")
+
+         .text(function(x) {
+           return x.key
+         })
 
        rows.exit().remove()
 
 
     }
-  , render_left: function() {
-      var scaled = transformData(this.data())
+  , render_left: function(scaled) {
+
 
       var wrapper = d3_updateable(this._target,".lhs","div",scaled)
         .classed("lhs col-md-7",true)
 
       var head = d3_updateable(wrapper, "h3","h3")
         .style("margin-bottom","15px")
-        .style("margin-top","10px")
-        .text("Media Plan")
+        .style("margin-top","20px")
+        .text("Media Plan (Category and Time Optimization)")
 
       
       var self = this;
+
+      var head = d3_updateable(wrapper,".head","div")
+        .classed("head",true)
+        .style("height","21px")
+
+      var name = d3_updateable(head,".name","div")
+        .classed("name",true)
+        .style("width","170px")
+        .style("padding-left","5px")
+        .style("display","inline-block")
+        .style("line-height","20px")
+        .style("vertical-align","top")
+
+       d3_splat(head,".hour","div",d3.range(1,25))
+        .classed("sq",true)
+        .style("display","inline-block")
+        .style("width","20px")
+        .style("height","20px")
+        .style("font-size",".85em")
+        .style("text-align","center")
+        .html(function(x) {
+          if (x == 1) return "<b>1a</b>"
+          if (x == 24) return "<b>12a</b>"
+          if (x == 12) return "<b>12p</b>"
+          return x > 11 ? x%12 : x
+        })
+
 
       var row = d3_splat(wrapper,".row","div",false,function(x) { return x.key })
         .classed("row",true)
@@ -229,46 +426,49 @@ MediaPlan.prototype = {
         .attr("class", function(x) { return x.key + " row" })
         .on("mouseover",function(x) {
 
-
           var _d = self.data()
           _d.display_categories = _d.display_categories || {"values":[]}
           var dd = buildDomains(_d)
 
           var d = dd.filter(function(z) { return z.parent_category_name == x.key})
           
-          //var d = d3.nest()
-          //  .key(function(x) { return x.domain})
-          //  .rollup(function(v) { return v.reduce(function(p,c) { p.count += c.count; p.sessions += c.uniques; return p },{count:0,sessions:0}) })
-          //  .entries(data)
-          //  .map(function(x) { x.count = x.values.count; return x})
-          //  .sort(function(p,c) { return c.count - p.count})
-          //  .slice(0,30)
 
-          self.render_right(d)
+          self.render_right(d,x)
         })
 
-      var MAGIC = 15
+      var MAGIC = 25 
 
       var name = d3_updateable(row,".name","div")
         .classed("name",true)
         .style("width","170px")
+        .style("padding-left","5px")
         .style("display","inline-block")
         .style("line-height","20px")
         .style("vertical-align","top")
         .text(function(x) { return x.key })
 
+      var colors = ["#a1d99b", "#74c476", "#41ab5d", "#238b45", "#006d2c", "#00441b"]
+      var colors = ["#238b45"]
+
+      var o = d3.scale.ordinal()
+        .domain([-25,100])
+        .range(colors);
 
       var square = d3_splat(row,".sq","div",function(x) { return x.values }, function(x,i) { return i }) 
         .classed("sq",true)
         .style("display","inline-block")
         .style("width","20px")
         .style("height","20px")
-        .style("background-color",function(x,i) { 
+        .style("background",function(x,i) { 
           var pd = this.parentNode.__data__; 
-          return ((x > MAGIC) && ( (pd.values[i-1] > MAGIC || false) || (pd.values[i+1] > MAGIC|| false) )) ? "black" : undefined 
-        })
-        .style("opacity",function(x) { return x/100 + .5 })
+          pd.mask = pd.mask || []
+          pd.mask[i] = ((x > MAGIC) && ( (pd.values[i-1] > MAGIC || false) || (pd.values[i+1] > MAGIC|| false) ))
+          //return pd.mask[i] ? o(pd.values[i])  : "repeating-linear-gradient( 45deg, #fee0d2, #fee0d2 2px, #fcbba1 5px, #fcbba1 2px) "
+          return pd.mask[i] ? 
+            "repeating-linear-gradient( 135deg, #238b45, #238b45 2px, #006d2c 5px, #006d2c 2px) " : 
+            "repeating-linear-gradient( 45deg, #fee0d2, #fee0d2 2px, #fcbba1 5px, #fcbba1 2px) "
 
+        })
 
 
     }
