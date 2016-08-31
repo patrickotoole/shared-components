99e5b77782b5c4dbe8c06a22d73b7ba39da092ba
rarcher2011 <richardjamesarcher@gmail.com>
2016-08-31 15:52:30 +0000

    update queue to store to further generalize and remove uneeded helpers

--- metrics/lib/topic_queue/queue_to_store.py
+++ metrics/lib/topic_queue/queue_to_store.py
@@ -5,31 +5,50 @@ from link import lnk
 import ujson
 import queue_helpers as helpers
 
-QUERY = """INSERT INTO %(table_name)s (%(columns)s) values ( %(values)s )"""
+QUERY = """INSERT INTO %(table_name)s (%(columns)s) %(subquery)s"""
+SUB_QUERY = "values ( %s )"
 
 class QueueInsert():
     def __init__(self, table_name, col_list, crushercache):
         self.table_name = table_name
         self.columns_list = col_list
         self.crushercache = crushercache
+        self.sub_query = self.build_subquery(col_list, SUB_QUERY)
 
-    def get_values(self, data):
-        columns = list(self.columns_list)
-        str_list, values = helpers.value_builder(data, columns)
-        return str_list, values
+    def build_subquery(self,col_list, query):
+        columns = list(col_list)
+        sub_string = "%({})s"
+        sub_query_cols = []
+        for col in columns:
+            sub_query_cols.append(sub_string.format(col))
+        sub_into_subquery = "\""+ "\",\"".join(sub_query_cols) + "\""
+        sub_query = query % (sub_into_subquery)
+        return sub_query
 
-    def generic_insert(self, values_to_replace, values_dict):
-        cr = self.crushercache.create_connection()
+    def column_list_to_string(self, db_connection):
+        cols = list(self.columns_list)
+        cols_str = ",".join(cols)
+        cols_str = db_connection.escape_string(query_params['columns'])
+        return cols_str
+
+    def build_subquery_w_values(self,values, db_connection):
+        for key,val in values.items():
+            values[key] = db_connection.escape_string(val)
+        built_subquery = self.sub_query % values
+        return built_subquery
+
+    def create_db_dict(self, values_dict):
+        db_connection = self.crushercache.create_connection()
         query_params = {}
         query_params['table_name'] = self.table_name
-        query_params['columns'] = helpers.column_builder(self.columns_list)
-        query_params['columns'] = cr.escape_string(query_params['columns'])
-        query_params['values'] = values_to_replace
-        for key,val in values_dict.items():
-            values_dict[key] = cr.escape_string(val)
-        query_params['values'] = query_params['values'] % values_dict
-        self.crushercache.execute(QUERY % query_params)
+        query_params['columns'] = self.columns_list_to_string(cr)
+        query_params['subquery'] = self.build_subquery_w_values(values_dict, db_connection)
+        return query_params
 
+    def process_message(self, data):
+        assert(all(col in data.keys() for col in columns_as_list))
+        query_params = self.create_db_dict(values)
+        self.crushercache.execute(QUERY % query_params)
 
 if __name__ == '__main__':
 
@@ -40,13 +59,13 @@ if __name__ == '__main__':
     from lib.report.utils.options import parse_command_line
 
     define("table_name",  default="")
-    define("columns", default={"columns":[]})
+    define("columns", default="")
 
     basicConfig(options={})
 
     parse_command_line()
-    assert('columns' in ujson.loads(options.columns))
-    columns_as_list = ujson.loads(options.columns)['columns']
+    assert(len(options.columns) >1)
+    columns_as_list = options.columns.split(",")
     assert(type(columns_as_list)==list)
 
     crushercache = lnk.dbs.crushercache
@@ -59,9 +78,7 @@ if __name__ == '__main__':
         if message is not None:
             data = ujson.loads(message.value)
             try:
-                assert(all(col in data.keys() for col in columns_as_list))
-                str_values, values = QI.get_values(data)
-                QI.generic_insert(str_values, values)
+                QI.process_message(data)
                 import time
                 time.sleep(2)
             except Exception as e:
