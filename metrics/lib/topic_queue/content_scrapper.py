c743e7f80a380e9d019dcb259e3ab6858e395338
rarcher2011 <richardjamesarcher@gmail.com>
2016-08-31 20:22:20 +0000

    update content scrapper to use classes and generally better code that can be unit tested

--- metrics/lib/topic_queue/content_scrapper.py
+++ metrics/lib/topic_queue/content_scrapper.py
@@ -9,7 +9,6 @@ from collections import Counter
 
 URL="http://scrapper.crusher.getrockerbox.com?url=%s"
 
-
 class GraphiteSender():
     import datetime
 
@@ -23,79 +22,75 @@ class GraphiteSender():
             if datetime.datetime.now() > self.sent_time:
                 self.sent_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
                 with self.GW:
-                    self.GW.send("scrapper.content_loop.metrics.pulled_from_kafka", self.cs.get_num_pulled())
-                    self.GW.send("scrapper.content_loop.metrics.num_deferred_created", self.cs.get_num_defer())
-                    self.GW.send("scrapper.content_loop.metrics.responses_from_scrapper", self.cs.get_num_responses())
-                    self.GW.send("scrapper.content_loop.metrics.num_content_added_to_kafka", self.cs.get_num_added())
+                    self.GW.send("scrapper.content_loop.metrics.pulled_from_kafka", self.cs.get("kafka_remove"))
+                    self.GW.send("scrapper.content_loop.metrics.num_deferred_created", self.cs.get("defer_create"))
+                    self.GW.send("scrapper.content_loop.metrics.responses_from_scrapper", self.cs.get("response"))
+                    self.GW.send("scrapper.content_loop.metrics.num_content_added_to_kafka", self.cs.get("kafka_send"))
 
 class GraphiteCounter():
     def __init__(self):
-        self.removed_from_kafka = 0
-        self.defered_created = 0
-        self.response_from_scrapper = 0
-        self.sent_to_kafka = 0
+        self.counter_dict = {"kafka_remove":0, "kafka_send":0, "defer_create":0, "response":0}
 
-    def bump_pulled(self):
-        self.removed_from_kafka+=1
-    def bump_defer(self):
-        self.defered_created+=1
-    def deck_defer(self):
-        self.defered_created-=1
-    def bump_responses(self):
-        self.response_from_scrapper+=1
-    def bump_added(self):
-        self.sent_to_kafka+=1
-    def get_num_pulled(self):
-        return self.removed_from_kafka
-    def get_num_defer(self):
-        return self.defered_created
-    def get_num_responses(self):
-        return self.response_from_scrapper
-    def get_num_added(self):
-        return self.sent_to_kafka
+    def bump(self, what_to_bump):
+        self.counter_dict[what_to_bump]+=1
+    def deck(self, what_to_dek):
+        self.counter_dict[what_to_dek]-=1
+    def get(self, what_to_get):
+        return self.counter_dict[what_to_get]
 
-def run(consumer, count, CS):
-    import datetime
-    #in_5 = datetime.datetime.now() + datetime.timedelta(minutes = 5)
-    def get(url, CS):
+class ScrapperMessage():
+
+    def __init__(self, counter_obj, graphite_counter, producer):
+        self.count = counter_obj
+        self.CS = graphite_counter
+        self.producer = producer
+
+    def get(self,url):
         try:
             _resp = requests.get((URL % url), auth=('rockerbox', 'rockerbox'))
-            CS.bump_responses()
+            CS.bump("response")
             data = _resp.json()
             title = data['result']['title']
         except Exception as e:
-            print str(e)
-            print "could not get title"
+            logging.info(str(e))
+            logging.info("could not get title")
             title = None
-        CS.deck_defer()
+        CS.deck("defer_create")
         return (title, url)
 
-    def send(result,  producer, CS):
+    def send(self,result):
         title, url = result
         if title is not None:
             msg = {"url":url.encode('ascii',  errors='ignore'), "title": title.encode('ascii', errors='ignore')}
-            print str(msg)
-            producer.send_message(json.dumps(msg))
-            CS.bump_added()
-            print "Sent"
+            logging.info(str(msg))
+            self.producer.send_message(json.dumps(msg))
+            self.CS.bump("kafka_send")
+            logging.info("Sent")
 
+    def proccess_message(self, message):
+        CS.bump("kafka_remove")
+        message_object = json.loads(message.value)
+        try:
+            url = message_object['bid_request']['bid_info']['url']
+            limit = count.get(url)
+            count.update({url})
+            if limit ==25:
+                logging.info(url)
+                defr = threads.deferToThread(self.get, url)
+                CS.bump("defer_create")
+                defr.addCallback(self.send)
+        except Exception as e:
+            logging.info(str(e))
+
+def run(consumer, count, CS, producer):
+    SM = ScrapperMessage(count, CS, producer)
     for message in consumer:
         if message is not None:
-            CS.bump_pulled()
-            message_object = json.loads(message.value)
-            try:
-                url = message_object['bid_request']['bid_info']['url']
-                limit = count.get(url)
-                count.update({url})
-                if limit ==25:
-                    print url
-                    defr = threads.deferToThread(get, url, CS)
-                    CS.bump_defer()
-                    defr.addCallback(send, producer,CS)
-            except Exception as e:
-                print str(e)
+            SM.proccess_message(message)
 
 if __name__ == '__main__':
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
     from pykafka import KafkaClient
     from lib.kafka_stream import kafka_stream
@@ -137,6 +132,6 @@ if __name__ == '__main__':
     CS = GraphiteCounter()
     GS = GraphiteSender(CS)
     from twisted.internet import reactor
-    reactor.callInThread(run, consumer, count, CS)
+    reactor.callInThread(run, consumer, count, CS, producer)
     reactor.callInThread(GS.start)
     reactor.run()
