66b5e055d2bea7913082c1219ff97a2103c668e6
rarcher2011 <richardjamesarcher@gmail.com>
2016-10-17 20:25:08 +0000

    update RPC to handle arbitrary python code in db and add schedule scripts script

--- metrics/handlers/admin/RPCQueue.py
+++ metrics/handlers/admin/RPCQueue.py
@@ -7,6 +7,18 @@ import hashlib
 from lib.functionselector import FunctionSelector
 
 SELECT_UDFS = "select name from rpc_function_details where is_recurring=1"
+SELECT_SCRIPTS = "select script from recurring_scripts where name = '%s' and active = 1 and deleted=0"
+
+def custom_script(**kwargs):
+    result = False
+    code_string = kwargs.get('code', Exception("no code arguement passed to custom script"))
+    code = compile(code_string, '<string>','exec')
+    try:
+        exec code in {}
+        result = True
+    except:
+        result = False
+    return result
 
 class RPCQueue():
 
@@ -85,6 +97,7 @@ class RPCQueue():
                 parameters[key] = val
         
         volume = "v{}".format(datetime.datetime.now().strftime('%m%y'))
+
         if udf in ('recurring','backfill'):
             days_ago = 1
             if 'days_ago' in parameters.keys():
@@ -104,11 +117,25 @@ class RPCQueue():
             #need better documentation and clarity on script and udf input for RPC
         FS = FunctionSelector()
         fn = FS.select_function(udf)
+
         work = pickle.dumps((
             fn,
             kwargs
             ))
         priority = int(priority)
+        
+        #Overwrite for custom scripting
+        script_df = self.crushercache.select_dataframe(SELECT_SCRIPTS % udf)
+        if len(script_df) > 0:
+            code_string = script_df.ix[0]['script']
+            fn =  custom_script
+            kwargs={}
+            kwargs['advertiser']='rockerbox'
+            kwargs['code'] = code_string
+            work = pickle.dumps((fn, kwargs))
+            priority = 1
+            debug_bool=True
+
         entry_id = CustomQueue.CustomQueue(self.zookeeper,"python_queue","log",volume).put(work,priority,debug=debug_bool)
         logging.info("added to Cassandra work queue %s for %s" %(segment,advertiser))
         job_id = hashlib.md5(work).hexdigest()
---
+++ metrics/lib/recurring/scheduled_scripts.py
@@ -0,0 +1,23 @@
+if __name__ == "__main__":
+    from link import lnk
+    import time
+    import os
+    import ujson
+    import requests
+    os.environ['TZ'] = 'US/Eastern'
+    
+    _time = time.localtime()
+    _time.tm_hour
+
+    DOW = time.strftime("%a",_time)
+    df = lnk.dbs.crushercache.select_dataframe("SELECT * FROM recurring_scripts where days like '%%%s%%'" % DOW)
+    xx = df.time.map( lambda x: int(x.split(" pm")[0]) +12 if "pm" in x else int(x.split(" am")[0]) ) == _time.tm_hour
+
+    current = df[xx]
+
+    for item in current.iterrows():
+        name = item[1]['name']
+        post_obj = {'udf':name}
+        _resp = requests.post('http://workqueue.crusher.getrockerbox.com/jobs', data=ujson.dumps(post_obj), auth=('rockerbox','rockerbox'))
+
+    print _resp.status_code
