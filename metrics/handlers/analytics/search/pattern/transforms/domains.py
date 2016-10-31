2a5914db8c003d798d16e043f1ba1253d6595b61
rarcher2011 <richardjamesarcher@gmail.com>
2016-10-31 20:02:09 +0000

    change default domains to match udf in database

--- metrics/handlers/analytics/search/pattern/transforms/domains.py
+++ metrics/handlers/analytics/search/pattern/transforms/domains.py
@@ -5,33 +5,49 @@ from lib.helpers import decorators
 from twisted.internet import defer, threads
 
 
-def process_domains(**kwargs):
+def process_domains(response=None,**kwargs):
 
-    response = kwargs['response']
-    df = kwargs['domains']
-    idf = kwargs['idf']
-    df2 = df.groupby(['domain']).count()
-    df3 = df2['uid'].reset_index()
-    df3.columns = ['domain', 'count']
 
-    dfA = df.groupby(['domain','uid'])
-    dfB = dfA.count().reset_index().groupby(['domain']).count()
-    dfC = dfB['uid'].reset_index()
-    dfC.columns = ['domain','uniques']
+    RANK = 'Importance'
+    COLS = ['rank','domain','idf','num_users', 'count', 'uniques','Importance','category_name','parent_category_name']
 
-    df4 = df3.merge(dfC, on='domain')
+    BLACKLIST = ['collective-exchange.com','anonymous.pubmatic.com']
+    domains = kwargs['domains']
+    domains = domains[domains['domain']!="NA"]
+    domains['domain'] = domains['domain'].apply(lambda x: x.lower())
+    domains = domains[domains['domain'].apply(lambda x: x not in BLACKLIST)]
 
-    df5 = df4.merge(idf, on='domain', how='left')
-    df4 = df4.sort(['uniques', 'count'], ascending=False)
-    df4 = df4.fillna('NA')
-    df5 = df4.reset_index()
-    df5 = df5.filter(['domain', 'count','uniques'])
-    df6 = df5.reset_index()
-    df6.columns = ['rank', 'domain','count','uniques']
-    df7 = df6.merge(idf, on='domain', how='left')
-    df7 = df7.fillna('NA')
-    data = df7.to_dict('records')
-    response['response'] = data
+    # Getting count and uniques columns
+    uniques = domains.groupby('domain').apply(lambda x: len(x['uid'].unique())).reset_index()
+    uniques.columns = ['domain','uniques']
+    counts = domains.groupby('domain').apply(lambda x: len(x)).reset_index()
+    counts.columns = ['domain','count']
+    data = uniques.merge(counts, on = 'domain', how = 'outer')
 
-    return response
+    def tail_diff(data, num_users, kwargs, idf = True):
+
+        if not idf:
+            idfs = pd.read_pickle("/root/datascience-steve/qbr/YoshiBot/pop_data/pop_domains.p")
+            idfs = idfs[idfs['domain']!= "NA"]
+        else:
+            idfs = kwargs['idf']
+
+        merged = data.merge(idfs, on = 'domain', how = 'left')
+        merged[['idf']] = merged[['idf']].fillna(idfs['idf'].mean())
+        merged['pct_users'] = merged['uniques'] / float(num_users)
+        # merged['tail_diff'] = merged['pct_users'] / (1./merged['idf'])
+        merged['tfidf'] = merged['uniques'] * merged['idf']
+        return merged[['domain','tfidf','idf','pct_users','category_name','parent_category_name','num_users']]
+
+    num_users = len(domains['uid'].unique())
+    tail_diff_data = tail_diff(data, num_users, kwargs)
+
+    data = data.merge(tail_diff_data, on = 'domain', how = 'left')
+
+    data['Importance'] = data['tfidf']
+    data['rank'] = data[RANK]
+
+    data = data.fillna(0)
+
+    response['response'] = data[COLS].to_dict('records')
 
