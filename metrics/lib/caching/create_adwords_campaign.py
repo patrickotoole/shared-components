67e871242d1384b2955152a5fa5d733e5fa8f55b
rarcher2011 <richardjamesarcher@gmail.com>
2016-11-16 21:01:06 +0000

    improve script to create adwords campaign from hindsight data

--- metrics/lib/caching/create_adwords_campaign.py
+++ metrics/lib/caching/create_adwords_campaign.py
@@ -8,31 +8,38 @@ class AdwordsCreator():
         self.hindsight = hindsight
         self.adwords_wrapper = adw
 
-    def create_budget(self,amount):
-        data = {'name':'adwords_build', 'amount':amount}
-        self.adwords_wrapper.post('/budget', data=ujson.dumps(data))
+    def create_budget(self,name, amount):
+        post_data = {'name':name, 'amount':amount}
+        resp = self.adwords_wrapper.post('/budget', data=ujson.dumps(post_data))
+        budget_id = resp['budget_id']
         return budget_id
 
-    def create_campaign(self, name):
-        post_data = {}
-        return None
+    def create_campaign(self, name, budget_id):
+        post_data = {'name':name, 'budget_id':budget_id}
+        resp = self.adwords_wrapper.post('/budget', data=ujson.dumps(post_data))
+        camp_id = resp['campaign_id']
+        return camp_id
 
-    def make_hourly_campaign(self):
-        return None
+    def make_category_campaign(self, categories_budget):
+        for category in categories_budget:
+            self.create_campaign(category['category_name'],category['budget_id']) 
 
     def get_hindsight_mediaplan(self):
         resp = self.hindsight.get('/crusher/v1/visitor/mediaplan?format=json&url_pattern=/')
         data = resp.json['mediaplan']
         return data
 
-    def create_adgroup(self):
+    def create_adgroup(self, name, camp_id, bid_amount):
+        post_data = {'name': name, 'campaign_id': camp_id, 'bid_amount': bid_amount}
+        resp = self.adwords_wrapper.post('/adgroup', data = ujson.dumps(post_data))
+        adgroup_id = resp.json['adgroup_id']
+        return adgroup_id
+
+    def create_placement(self, url, adgroup_id):
+        post_data = {"placement_ul":url, "adgroup_id":adgroup_id}
+        resp = self.adwords_wrapper.post('/placement', data = ujson.dumps(post_data))
         return None
 
-    def create_placement(self, adgroup)
-        return None
-
-    def sample_get(self):
-        self.adw.get()
 
 if __name__ =="__main__":
 
