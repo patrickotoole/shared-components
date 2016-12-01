93b6e6836835d8664e0c96b339c981c3baa64b97
rarcher2011 <richardjamesarcher@gmail.com>
2016-12-01 21:21:53 +0000

    update recurring scripts to use run everytime and update password

--- metrics/lib/recurring/scheduled_scripts.py
+++ metrics/lib/recurring/scheduled_scripts.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     _time.tm_hour
 
     DOW = time.strftime("%a",_time)
-    df = lnk.dbs.crushercache.select_dataframe("SELECT * FROM recurring_scripts where days like '%%%s%%'" % DOW)
+    df = lnk.dbs.crushercache.select_dataframe("SELECT * FROM recurring_scripts where days like '%%%s%%' and run_everytime=0" % DOW)
     xx = df.time.map( lambda x: int(x.split(" pm")[0]) +12 if "pm" in x else int(x.split(" am")[0]) ) == _time.tm_hour
 
     current = df[xx]
@@ -18,5 +18,12 @@ if __name__ == "__main__":
     for item in current.iterrows():
         name = item[1]['name']
         post_obj = {'udf':name}
-        _resp = requests.post('http://workqueue.crusher.getrockerbox.com/jobs', data=ujson.dumps(post_obj), auth=('rockerbox','rockerbox'))
+        _resp = requests.post('http://workqueue.crusher.getrockerbox.com/jobs', data=ujson.dumps(post_obj), auth=('rockerbox','RBOXX2017'))
+        print _resp.status_code
+
+    df2 = lnk.dbs.crushercache.select_dataframe("SELECT * FROM recurring_scripts where run_everytime=1")
+    for item in df2.iterrows():
+        name = item[1]['name']
+        post_obj = {'udf':name}
+        _resp = requests.post('http://workqueue.crusher.getrockerbox.com/jobs', data=ujson.dumps(post_obj), auth=('rockerbox','RBOXX2017'))
         print _resp.status_code
