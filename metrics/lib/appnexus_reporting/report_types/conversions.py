fb8c18d432b8b5d796313b0547eb442b9e77062e
stphnma <stphnma@gmail.com>
2016-12-16 16:05:47 -0500

    Fixing is valid (#967)
    * Adding function for evaluting agains advertiser_pixel
    
    * removing comments
    
    * testing commit
    
    * testing commit
    
    * Adding function for evaluting agains advertiser_pixel
    
    * removing comments
    
    * testing commit
    
    * testing commit
    
    * seperating getting pixel data
    
    * resolving conflicts
    
    * removing extra function

--- metrics/lib/appnexus_reporting/report_types/conversions.py
+++ metrics/lib/appnexus_reporting/report_types/conversions.py
@@ -2,6 +2,7 @@ from appnexus import AppnexusReport
 from timeutils import *
 from load import DataLoader
 from log import *
+from lib.report.utils.utils import parse_datetime
 
 import pandas
 import logging
@@ -99,18 +100,54 @@ def insert_report(db, table, df, report_params):
 
     return df
 
+
+ADVERTISER_PIXEL = '''
+SELECT * FROM rockerbox.advertiser_pixel
+WHERE external_advertiser_id = %s
+'''
+
+def _is_valid(row, pixel_data):
+    conversion_time = parse_datetime(row['conversion_time'])
+    imp_time = parse_datetime(row['imp_time'])
+
+    time_diff = conversion_time - imp_time
+    time_diff_hours = time_diff.days*24 + (time_diff.seconds/3600.)
+
+    window_hours = pixel_data.ix[row['pixel_id']]['pc_window_hours'] if row['pc'] == 1 else pixel_data.ix[row['pixel_id']]['pv_window_hours'] 
+    is_valid = time_diff_hours <= window_hours
+
+    return is_valid
+
+def get_pixel_data(db, advertiser_id):
+    pixel_data = db.select_dataframe(ADVERTISER_PIXEL%advertiser_id)
+    return pixel_data
+
+    
+def check_is_valid(db, df, advertiser_id):
+    assert len(pixel_data) > 0
+    assert 'pixel_id' in pixel_data.columns
+    assert 'pc_window_hours' in pixel_data.columns
+    assert 'pv_window_hours' in pixel_data.columns
+
+    for pid in df['pixel_id'].unique(): assert pid in pixel_data['pixel_id'].unique()
+
+    pixel_data = pixel_data.set_index('pixel_id')
+    df['is_valid'] = df.apply(lambda row:  _is_valid(row, pixel_data), axis = 1)
+    return df
+
+
 def transform(df,report_params):
 
     df = df.rename(columns={"datetime":"conversion_time","advertiser_id":"external_advertiser_id","post_click_or_post_view_conv":"pc"})
     df['pc'] = df['pc'] == "Post Click"
     df['last_activity'] = now()
     df['source_report_id'] = report_params['report_id']
-
     return df
 
 def run(api, db, table, advertiser_id, start_date, end_date):
 
     df, report_params = get_report(db,api,advertiser_id,start_date,end_date)
+
     if len(df) == 0: 
         report_params['processed_at'] = now()
         log_processed(db,report_params)
@@ -119,5 +156,6 @@ def run(api, db, table, advertiser_id, start_date, end_date):
 
     grouped_df = transform(df, report_params)
 
+    grouped_df = check_is_valid(db, grouped_df, advertiser_id)
     insert_report(db, table, grouped_df.reset_index(), report_params)
 
