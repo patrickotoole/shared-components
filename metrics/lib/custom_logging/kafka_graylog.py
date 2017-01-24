e8529372769404499f45eafc44fcc2b3a5c15712
rarcher2011 <richardjamesarcher@gmail.com>
2017-01-24 21:41:00 +0000

    add lastweek caching and improve graylog kafka

--- metrics/lib/custom_logging/kafka_graylog.py
+++ metrics/lib/custom_logging/kafka_graylog.py
@@ -14,12 +14,13 @@ if __name__ == '__main__':
     from lib.report.utils.options import parse_command_line
 
     define("ip",  default="")
+    define("port", default="")
 
     basicConfig(options={})
 
     parse_command_line()
     
-    s.connect((str(options.ip), 12203))
+    s.connect((str(options.ip), int(options.port)))
     client = KafkaClient(hosts="10.128.248.211:2181/v0_8_1")
     topic = client.topics['application_log']
     consumer = topic.get_simple_consumer(reset_offset_on_start=True, auto_offset_reset=OffsetType.LATEST)
