cc5bd12cccdb12e8cc2b60f2053daa3a96721c63
rarcher2011 <richardjamesarcher@gmail.com>
2017-02-23 16:09:06 +0000

    catch errors relating to blank lines that break pipes

--- metrics/lib/custom_logging/file_to_graylog.py
+++ metrics/lib/custom_logging/file_to_graylog.py
@@ -17,13 +17,13 @@ if __name__ == "__main__":
 
     input_stream = takewhile(bool, (stdin.readline() for _ in count()))
     for i in input_stream:
-        logging.info(i.replace('\n',""))
-        mesos_msg = ujson.loads(i.replace('\n',""))
-        mesos_source = (item for item in mesos_msg['marathon_data'] if "MESOS_TASK_ID" in item.keys()).next()
-        mesos_app = (item for item in mesos_msg['marathon_data'] if "MARATHON_APP_ID" in item.keys()).next()
-        mesos_location= (item for item in mesos_msg['marathon_data']  if "HOST" in item.keys()).next()
-        msg = {"source":mesos_location['HOST'] or "", "app_name":mesos_app['MARATHON_APP_ID'] or "", "application" : mesos_source['MESOS_TASK_ID'], "host":mesos_location['HOST'] or "", "short_message":mesos_msg['short_message'] or "", "version":"1.1"}
         try:
+            logging.info(i.replace('\n',""))
+            mesos_msg = ujson.loads(i.replace('\n',""))
+            mesos_source = (item for item in mesos_msg['marathon_data'] if "MESOS_TASK_ID" in item.keys()).next()
+            mesos_app = (item for item in mesos_msg['marathon_data'] if "MARATHON_APP_ID" in item.keys()).next()
+            mesos_location= (item for item in mesos_msg['marathon_data']  if "HOST" in item.keys()).next()
+            msg = {"source":mesos_location['HOST'] or "", "app_name":mesos_app['MARATHON_APP_ID'] or "", "application" : mesos_source['MESOS_TASK_ID'], "host":mesos_location['HOST'] or "", "short_message":mesos_msg['short_message'] or "", "version":"1.1"}
             producer.send_messages('application_log',ujson.dumps(msg))
         except:
             logging.info("Didnt send")
