7ff9051fa9e55c4202859564542d6340ec511e70
rarcher2011 <richardjamesarcher@gmail.com>
2017-03-09 19:28:36 +0000

    remove comments from beginning of functions in wqloghandler

--- workqueue/wqloghandler.py
+++ workqueue/wqloghandler.py
@@ -7,11 +7,6 @@ from logging import StreamHandler
 class CustomLogHandler(StreamHandler):
 
     def __init__(self, stream=None):
-        """
-        Initialize the handler.
-
-        If stream is not specified, sys.stderr is used.
-        """
         Handler.__init__(self)
         if stream is None:
             stream = sys.stderr
@@ -19,19 +14,11 @@ class CustomLogHandler(StreamHandler):
         self.job_id = ""   
 
     def customformat(self,record):
-        """
-        @rarcher2011 use custom formating fucntion instead of default format function
-        """
         template = '%(asctime)s  %(name)s.%(funcName)s:%(lineno)d  %(job_id)s> %(message)s'
         msg = template % {"asctime":datetime.datetime.fromtimestamp(record.created ).strftime('%Y-%m-%d %H:%M:%S'), "name":record.name, "funcName":record.filename, "lineno":record.lineno, "job_id":self.job_id, "message":record.msg}
         return msg
 
     def emit(self, record):
-        """
-        Emit a record.
-
-        @rarcher2011 Overwrite here to force to specific format
-        """
         try:
             msg = self.customformat(record)
             stream = self.stream
