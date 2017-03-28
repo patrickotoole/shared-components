9bdb4497cd877bd9f32663e9990e6ade3f5bda02
rickotoole <stphnma@gmail.com>
2017-03-28 19:28:16 +0000

    yoshi_setup unit tests

--- yoshi/handlers/yoshi_setup/tests/test_database.py
+++ yoshi/handlers/yoshi_setup/tests/test_database.py
@@ -11,20 +11,37 @@ CREATE_SETUP_TABLE = '''
 CREATE TABLE `yoshi_setup` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `external_advertiser_id` int(10) NOT NULL,
-  `source` varchar(400) NOT NULL,
-  `subsource` varchar(400) NOT NULL,
+  `mediaplan` varchar(400) NOT NULL DEFAULT '',
   `num_domains` int(10) NOT NULL,
   `active` int(1) NOT NULL DEFAULT '1',
   `last_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  `line_item_name` varchar(400) NOT NULL,
+  `line_item_name` varchar(400) DEFAULT 'Yoshi Testing',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `external_advertiser_id` (`external_advertiser_id`,`source`,`subsource`,`line_item_name`)
+  UNIQUE KEY `external_advertiser_id` (`external_advertiser_id`,`mediaplan`,`line_item_name`)
 )
 '''
+
+
 SETUP_VALUES = [
-    {'external_advertiser_id': 430556 , "source": "source1", "subsource":"source1a", "num_domains":5, "line_item_name":"Yoshi Testing" }
+    {'external_advertiser_id': 430556 , "mediaplan": "mediaplan", "num_domains":5, "line_item_name":"Yoshi Testing" }
 ]
 
+CREATE_ADWORD_ENDPOINTS = '''
+CREATE TABLE `adwords_campaign_endpoint` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `advertiser_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  `deleted` tinyint(4) DEFAULT '0',
+  `last_activity` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `endpoint` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+'''
+
+ADWORDS_VALUES = [
+    {'advertiser_id':430556, 'name': "mediaplan", "endpoint": "\/test"}
+]
 
 
 class DataBaseTest(unittest.TestCase):
@@ -38,30 +55,45 @@ class DataBaseTest(unittest.TestCase):
             self.db.execute("DROP TABLE yoshi_setup") 
         self.db.execute(CREATE_SETUP_TABLE)
         data = pd.DataFrame(SETUP_VALUES)
-        dl.insert_df(data, "yoshi_setup",[], SETUP_VALUES[0].keys() )
+        dl.insert_df(data, "yoshi_setup",[], SETUP_VALUES[0].keys())
+
+        len_check = self.db.execute("show tables like 'adwords_campaign_endpoint'")
+        if len(len_check.as_dataframe())>0:
+            self.db.execute("DROP TABLE adwords_campaign_endpoint")
+        self.db.execute(CREATE_ADWORD_ENDPOINTS)
+        data = pd.DataFrame(ADWORDS_VALUES)
+        dl.insert_df(data, "adwords_campaign_endpoint",[], ADWORDS_VALUES[0].keys())
 
         self.Database = yoshi_setup.SetupDatabase()
         self.Database.db = self.db
+        self.Database.crushercache = self.db
 
     def teardown(self):
         self.db.execute("DROP TABLE yoshi_setup") 
+        self.db.execute("DROP TABLE adwords_campaign_endpoint")
 
+    def test_get_yoshi_setup(self):
+        cols = ["mediaplan", "num_domains", "line_item_name"]
+        true = pd.DataFrame(SETUP_VALUES).sort(['mediaplan']).reset_index(drop = True)[cols]
+        actual = self.Database.get_yoshi_setup(430556).sort(['mediaplan']).reset_index(drop = True)[cols]
+        assert true.equals(actual)
 
-    def test_get_setup(self):
-        cols = SETUP_VALUES[0].keys()
-        true = pd.DataFrame(SETUP_VALUES).sort(['external_advertiser_id','source','subsource']).reset_index(drop = True)
-        actual = self.Database.get_setup(430556).sort(['external_advertiser_id','source','subsource']).reset_index(drop = True)  
-        assert true[cols].equals(actual[cols])
+    def test_get_media_plans(self):
+
+        cols = ['name','endpoint']
+        true = pd.DataFrame(ADWORDS_VALUES).sort('name').reset_index(drop = True)[cols]
+        actual = self.Database.get_media_plans(430556).sort(['name']).reset_index(drop = True)[cols]
+        assert true.equals(actual)
 
     def test_insert(self):
 
         to_write = pd.DataFrame([
-            {'external_advertiser_id': 430556 , "source": "source2", "subsource":"source2a", "num_domains":5, "line_item_name":"Yoshi Testing" }
+            {'external_advertiser_id': 430556 , "mediaplan": "source2", "num_domains":5, "line_item_name":"Yoshi Testing" }
         ])
         self.Database.insert(to_write)
 
         query = '''
-        SELECT * FROM yoshi_setup WHERE external_advertiser_id = 430556 AND source = "source2" AND subsource = "source2a" AND num_domains = 5
+        SELECT * FROM yoshi_setup WHERE external_advertiser_id = 430556 AND mediaplan = "source2" AND num_domains = 5
         AND line_item_name = "Yoshi Testing" AND active = 1
         '''
 
