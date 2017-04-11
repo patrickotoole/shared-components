f06b6e74841e27c28855c54ef09aee1ec233daa1
rarcher2011 <richardjamesarcher@gmail.com>
2017-04-11 20:45:41 +0000

    update wq ui to show dates and whether and if cached for advertiser segments

--- workqueue/handlers/checkshandler.py
+++ workqueue/handlers/checkshandler.py
@@ -1,10 +1,15 @@
 import tornado
 import ujson
 import tornado.web
+import pandas
+import numpy
+import datetime
 
 ADVERTISER_QUERY ="select a.pixel_source_name, a.action_name, a.action_id from  action a join advertiser b on a.pixel_source_name = b.pixel_source_name where b.crusher=1"
-#CACHEQUERY = "select currenttime from generic_function_cache where advertiser = '%s' and udf='domains_full_time_minute' and action_id='%s'"
-CACHEQUERY = "select count(*) from generic_function_cache where advertiser = '%s' and udf='domains_full_time_minute' and action_id='%s'"
+CACHEQUERY = "select date from generic_function_cache where advertiser = '%s' and udf='domains_full_time_minute' and action_id='%s'"
+
+QUERY = "select distinct advertiser, action_id, date from generic_function_cache where udf='domains_full_time_minute' and date > '%s'"
+QUERY2 = "select distinct action_name, filter_id as action_id from advertiser_caching_segment"
 
 class ChecksHandler(tornado.web.RequestHandler):
     
@@ -14,8 +19,19 @@ class ChecksHandler(tornado.web.RequestHandler):
 
     def get(self):
         advertisers_segments = self.db.select_dataframe(ADVERTISER_QUERY)
-        data = {"values":[]}
-        for x in advertisers_segments.iterrows():
-            data2 = self.crushercache.select_dataframe(CACHEQUERY % (x[1]['pixel_source_name'], x[1]['action_id']))
-            data['values'].append({"Advertiser":x[1]['pixel_source_name'], "Segment Name": x[1]["action_name"], "Count of Dashboard Agg":data2['count(*)'][0]}) 
+        df_adv = self.db.select_dataframe(QUERY2)
+        df = self.crushercache.select_dataframe(QUERY % (datetime.datetime.now() - datetime.timedelta(days=30)).strftime("%Y-%m-%d"))
+        #df['date'] = df.apply(lambda x : str(x))
+        df['filled'] = 1
+        df = df.merge(df_adv,on="action_id")
+        ndict = df.set_index(["advertiser","action_name","date"])['filled'].unstack(2).fillna(0).reset_index(['advertiser','action_name'])
+        ndict.columns = [str(x) for x in ndict.columns]
+        colheaders = ndict.columns.tolist()
+        ndict = ndict.to_dict('records')
+        colheaders.sort(reverse=True)
+        heads = [{"key":x,"value":x} for x in colheaders]
+        
+        data ={'values':ndict, 'headers':heads}
         self.render("checks.html", data=data, paths="")
+
+
