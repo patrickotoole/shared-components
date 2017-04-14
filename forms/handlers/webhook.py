2fd75c77a292130ac7ccfaa1cca3ae32483e227d
rarcher2011 <richardjamesarcher@gmail.com>
2017-04-14 19:00:46 +0000

    update webhook handlers to operate with deferreds and inlinecallbacks

--- forms/handlers/webhook.py
+++ forms/handlers/webhook.py
@@ -2,6 +2,8 @@ import tornado.web
 import logging
 import json
 from webhook_helpers import *
+from lib.helpers import decorators
+import lib.custom_defer as custom_defer
 
 class WebhookHandler(tornado.web.RequestHandler, WebhookDatabase, WebhookWorkqueue):
 
@@ -9,9 +11,14 @@ class WebhookHandler(tornado.web.RequestHandler, WebhookDatabase, WebhookWorkque
         self.db = kwargs.get("db",False)
         self.crushercache = kwargs.get("crushercache",False) 
 
+    @custom_defer.inlineCallbacksErrors
+    def get_and_add(self,uri,data):
+        scriptname = yield self.get_script(uri)
+        success = yield self.add_webhook_to_wq(scriptname, data)
+        logging.info("Success, recieved message from webhook")
+
+    @decorators.error_handling
     def post(self,uri):
         data = json.loads(self.request.body)
-        scriptname = self.get_script(uri)
-        self.add_webhook_to_wq(scriptname, data)
-        logging.info("Success, recieved message from webhook")
+        self.get_and_add(uri, data)
 
--- forms/handlers/webhook_helpers.py
+++ forms/handlers/webhook_helpers.py
@@ -1,12 +1,14 @@
 import logging
 import ujson
 import requests
+from lib.helpers import decorators
 
 class WebhookDatabase():
 
     def __init__(self,**kwargs):
         self.crushercache = kwargs.get("crushercache",False)
 
+    @decorators.deferred
     def get_script(self,uri):
         scriptname = self.crushercache.select_dataframe("select scriptname from forms_webhooks where path = '%s'" % uri)
         return scriptname['scriptname'][0]
@@ -16,8 +18,10 @@ class WebhookWorkqueue():
     def __init__(self,**kwargs):
         self.crushercache = kwargs.get("crushercache",False)
 
+    @decorators.deferred
     def add_webhook_to_wq(self,scriptname, data):
         logging.info("posting")
         post_data = {"udf": scriptname, "parameters": data}
         resp = requests.post("http://workqueue.crusher.getrockerbox.com/jobs", auth=("rockerbox","RBOXX2017"), data = ujson.dumps(post_data))
         logging.info(resp.text)
+        return True
