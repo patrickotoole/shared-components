496d578b87aa8c563359f920c72f47e5bc958a61
rickotoole <stphnma@gmail.com>
2017-04-16 04:50:09 +0000

    changing select query to grab most recent mediaplan

--- hindsight/handlers/adwords_mediaplan.py
+++ hindsight/handlers/adwords_mediaplan.py
@@ -9,7 +9,25 @@ from base import BaseHandler
 from twisted.internet import defer
 from lib.helpers import *
 
-SELECT = "select * from adwords_campaign_endpoint where active = 1 and deleted = 0 and advertiser_id = '%s'"
+SELECT = '''
+SELECT a.* FROM
+(
+    SELECT name, endpoint, last_activity
+    FROM adwords_campaign_endpoint
+    WHERE advertiser_id = %(advertiser_id)s
+    AND active = 1 AND deleted = 0
+) a
+JOIN
+(
+    SELECT name, max(last_activity) as last_activity
+    FROM adwords_campaign_endpoint
+    WHERE advertiser_id = %(advertiser_id)s
+    AND active = 1 AND deleted = 0
+    GROUP BY 1
+) b
+ON(a.name = b.name AND a.last_activity = b.last_activity)
+'''
+
 INSERT = "insert into adwords_campaign_endpoint (advertiser_id, name, endpoint) values (%s, %s, %s)"
 INSERT2 = "insert into yoshi_setup (external_advertiser_id, mediaplan, line_item_name, num_domains) values (%s, %s, %s, 20)"
 
@@ -22,7 +40,7 @@ class AdwordsHandler(BaseHandler):
 
     def get(self):
         advertiser_id = self.current_advertiser
-        data = self.crushercache.select_dataframe(SELECT % advertiser_id)
+        data = self.crushercache.select_dataframe(SELECT % {'advertiser_id':advertiser_id})
         self.write( ujson.dumps({"response":data.to_dict('records')}) )
 
     def post(self):
