a1e1a9de031dd6a8a717580c117b29a4100f00f7
root <stphnma@gmail.com>
2017-05-23 16:12:59 +0000

    radial tree

--- campaign_analysis/handlers/database.py
+++ campaign_analysis/handlers/database.py
@@ -25,7 +25,7 @@ LEFT JOIN
     GROUP BY 1, 2
 ) v2
 ON (v4.date = v2.date AND v4.campaign_id = v2.campaign_id)
-LEFT JOIN
+RIGHT JOIN
 (
     SELECT a.campaign_id, a.campaign_name, a.line_item_id, b.line_item_name
     FROM rockerbox.advertiser_campaign a
@@ -34,12 +34,13 @@ LEFT JOIN
     WHERE a.external_advertiser_id = %(advertiser_id)s
     AND b.external_advertiser_id = %(advertiser_id)s
     AND a.campaign_id IN (%(campaign_list)s)
+    AND a.active = 1 AND a.deleted = 0
 ) c
 ON (v4.campaign_id = c.campaign_id)
 '''
 
 
-CAMPAIGNS = '''
+CAMPAIGNS_TREE = '''
 SELECT a.*
 FROM rockerbox.advertiser_campaign a
 JOIN (
@@ -47,23 +48,52 @@ JOIN (
     WHERE campaign_id =  %(campaign_id)s
     AND external_advertiser_id = %(advertiser_id)s
 ) b 
-ON a.campaign_name LIKE CONCAT('%%', b.campaign_name, '%%')'''
+ON a.campaign_name LIKE CONCAT('%%', b.campaign_name, '%%')
+'''
+
+LINE_ITEM_CAMPAIGNS = '''
+SELECT a.campaign_id, a.campaign_name, a.line_item_id, b.line_item_name
+FROM rockerbox.advertiser_campaign a
+JOIN rockerbox.advertiser_line_item b
+ON (a.line_item_id = b.line_item_id)
+WHERE a.external_advertiser_id = %(advertiser_id)s
+AND b.external_advertiser_id = %(advertiser_id)s
+AND a.line_item_id = %(line_item_id)s
+AND a.deleted = 0
+'''
+
 
 class DataBase(object):
 
-    def get_data(self, advertiser_id, campaign_id, start_date, end_date):
-
+    def get_data_from_campaign_list(self, advertiser_id, start_date, end_date, campaign_list):
         logging.info("retrieving data from db")
-        campaigns = self.db.select_dataframe(CAMPAIGNS%{'campaign_id':campaign_id, 'advertiser_id':advertiser_id})
-        campaign_list = ",".join(campaigns['campaign_id'].astype(str).tolist())
-        # print DATA%{'start_date':start_date, 'end_date': end_date, 'advertiser_id':advertiser_id, 'campaign_list':campaign_list}
         data = self.db.select_dataframe(DATA%{'start_date':start_date, 'end_date': end_date, 'advertiser_id':advertiser_id, 'campaign_list':campaign_list})
-        logging.info("retrieved data")
-
         data['date'] = pd.to_datetime(data['date'])
         data['campaign_id'] = data['campaign_id'].astype(str)
+        data['campaign_name'] = data['campaign_name'].astype(str)
         data = data.fillna(0)
+        logging.info("retrieved data")
         
+        return data
+
+    def campaigns_from_line_items(self, advertiser_id, line_item_id):
+        logging.info("getting campaigns")
+        df = self.db.select_dataframe(LINE_ITEM_CAMPAIGNS%{'advertiser_id':advertiser_id,'line_item_id':line_item_id})
+        logging.info("got campaigns")
+        return df
+
+    def get_campaign_tree(self, advertiser_id, campaign_id):
+        logging.info("getting campaign tree for %s" %campaign_id)
+        campaigns = self.db.select_dataframe(CAMPAIGNS_TREE%{'campaign_id':campaign_id, 'advertiser_id':advertiser_id})
+        return campaigns['campaign_id'].astype(str).tolist()
+
+
+
+    def get_data(self, advertiser_id, campaign_id, start_date, end_date):
+        logging.info("retrieving data from db")
+        campaigns = self.db.select_dataframe(CAMPAIGNS_TREE%{'campaign_id':campaign_id, 'advertiser_id':advertiser_id})
+        campaign_list = ",".join(campaigns['campaign_id'].astype(str).tolist())
+        data = self.get_data_from_campaign_list(advertiser_id, start_date, end_date, campaign_list)
         return data
 
 
--- campaign_analysis/handlers/handler.py
+++ campaign_analysis/handlers/handler.py
@@ -6,7 +6,10 @@ import logging
 from database import *
 import numpy as np
 from database import *
+from datetime import datetime, timedelta
 
+def clean_name(name):
+    return name.replace("platform_placement_targets: ","").replace("domain: ","").replace("size :","").replace("dma :","")
 
 def build_tree(df, levels, max_level):
 
@@ -16,24 +19,52 @@ def build_tree(df, levels, max_level):
 
     if level == max_level or len(levels) == 0:
         for name, dd in df.groupby(level):
-
-            tree.append({
-                'name': name,
-                'level': level,
-                'metrics': dd.sum(numeric_only = True).to_dict(),
-                'campaigns': dd['campaign_id'].tolist()
-            })
+            if name is not None:
+                tree.append({
+                    'name': clean_name(name),
+                    'level': level,
+                    'metrics': dd.sum(numeric_only = True).to_dict(),
+                    # 'campaigns': dd['campaign_id'].tolist()
+                })
     else:
         for name, dd in df.groupby(level):
+            if name is not None:
 
-            tree.append({
-                'name': name,
-                'level': level,
-                'metrics': dd[pd.isnull(dd[levels[0]])].sum(numeric_only = True).to_dict(),
-                'children': build_tree(dd, levels, max_level),
-                'campaigns': dd['campaign_id'].tolist()
-            })
+                tree.append({
+                    'name': clean_name(name),
+                    'level': level,
+                    #'metrics': dd.sum(numeric_only = True).to_dict(),
+                    'metrics': dd[pd.isnull(dd[levels[0]])].sum(numeric_only = True).to_dict(),
+                    'children': build_tree(dd, levels, max_level),
+                    # 'campaigns': dd['campaign_id'].tolist()
+                })
     return tree
+
+def build_metrics(df, groups, end_date = datetime.today().strftime("%Y%m%d")):
+
+    seven_days_ago = (datetime.strptime(end_date, "%Y%m%d")-timedelta(days=7)).strftime("%Y%m%d")
+    thirty_days_ago = (datetime.strptime(end_date, "%Y%m%d")-timedelta(days=30)).strftime("%Y%m%d")
+    yest = (datetime.strptime(end_date, "%Y%m%d")-timedelta(days=1)).strftime("%Y%m%d")
+
+    thirty_days_data = df[df['date']>=thirty_days_ago].groupby(groups).sum()
+    seved_days_data = df[df['date']>=seven_days_ago].groupby(groups).sum()
+    yest_data = df[(df['date']>= yest) & (df['date']< end_date) ].groupby(groups).sum()
+    today_data = df[df['date']>=end_date].groupby(groups).sum()
+
+    g1 = pd.merge(thirty_days_data, seved_days_data, left_index = True, right_index = True, how = 'outer', suffixes = ["_30d","_7d"])
+    g2 = pd.merge(yest_data, today_data, left_index = True, right_index = True, how = 'outer', suffixes = ["_yest","_today"])
+
+    metrics = pd.merge(g1, g2, left_index = True, right_index = True, how = 'outer').fillna(0).reset_index()
+    metrics['cpa_30d'] = metrics['media_cost_30d']/ metrics['conv_30d']
+    metrics['cpa_7d'] = metrics['media_cost_7d']/ metrics['conv_7d']
+    metrics['cpa_yest'] = metrics['media_cost_yest']/ metrics['conv_yest']
+
+
+    metrics = metrics.fillna(0)
+    metrics = metrics.replace([np.nan, np.inf, -np.inf],0)
+
+    return metrics
+
 
 
 class CampaignHandler(tornado.web.RequestHandler, DataBase):
@@ -42,29 +73,48 @@ class CampaignHandler(tornado.web.RequestHandler, DataBase):
         self.db = kwargs.get("reporting",False) 
 
     def get(self):
-        from datetime import datetime
 
         advertiser_id = self.get_query_argument("advertiser")
         start_date = str(self.get_query_argument("start_date", "20170101"))
         end_date = str(self.get_query_argument("end_date", datetime.today().strftime("%Y%m%d")))
-        campaign_id = self.get_query_argument("campaign_id")
+        campaign_id = self.get_query_argument("campaign_id", False)
+        line_item_id = self.get_query_argument("line_item_id", False)
 
-        data = self.get_data(advertiser_id, campaign_id, start_date, end_date)
+        if campaign_id:
 
-        campaign_metrics = data.groupby(['line_item_name','campaign_name','campaign_id']).sum().reset_index()
-        campaign_metrics['cpa'] = campaign_metrics['media_cost'] / campaign_metrics['conv']
-        campaign_metrics = campaign_metrics.fillna(0)
-        campaign_metrics = campaign_metrics.replace([np.nan, np.inf, -np.inf],0)
+            data = self.get_data(advertiser_id, campaign_id, start_date, end_date)
+            campaign_metrics = build_metrics(data, ['line_item_name','campaign_name'], end_date)
+            max_levels = campaign_metrics['campaign_name'].apply(lambda x: len(x.split(" - include "))).max()
 
+            for k in range(max_levels):
+                campaign_metrics['level%s'%str(k)] = campaign_metrics['campaign_name'].apply(lambda x:  x.split(" - include ")[k] if len(x.split(" - include "))>k else None   )
 
-        max_levels = campaign_metrics['campaign_name'].apply(lambda x: len(x.split(" - include "))).max()
+            levels = ["level%s"%str(i) for i in range(max_levels) ] 
+            tree = build_tree(df =campaign_metrics, levels = levels, max_level = levels)
+            self.render("campaignOptTreeRadial.html", tree = json.dumps(tree[0]) )
 
-        for k in range(max_levels):
-            campaign_metrics['level%s'%str(k)] = campaign_metrics['campaign_name'].apply(lambda x:  x.split(" - include ")[k] if len(x.split(" - include "))>k else None   )
+        elif line_item_id:
 
-        levels = ["level%s"%str(i) for i in range(max_levels) ] 
+            campaigns = self.campaigns_from_line_items(advertiser_id,line_item_id)
 
-        tree = build_tree(df =campaign_metrics, levels = levels, max_level = levels[-1])
+            campaign_list = []
+            for campaign_id in campaigns['campaign_id'].tolist():
+                campaign_opt_tree = self.get_campaign_tree(advertiser_id, campaign_id)
+                campaign_list = campaign_list + campaign_opt_tree
 
-        self.render("campaignOptTree.html", tree = json.dumps(tree[0]) )
+            data = self.get_data_from_campaign_list(advertiser_id, start_date, end_date, ",".join(campaign_list))
+            campaign_metrics = build_metrics(data, ['line_item_name','campaign_name'], end_date)
+
+            campaign_metrics['opt_level'] = campaigns['line_item_name'].iloc[0] + " - include " + campaign_metrics['campaign_name']
+
+            max_levels = campaign_metrics['opt_level'].apply(lambda x: len(x.split(" - include "))).max()
+
+            for k in range(max_levels):
+                campaign_metrics['level%s'%str(k)] = campaign_metrics['opt_level'].apply(lambda x:  x.split(" - include ")[k] if len(x.split(" - include "))>k else None   )
+
+            levels = ["level%s"%str(i) for i in range(max_levels) ] 
+
+            tree = build_tree(df =campaign_metrics, levels = levels, max_level = levels[-1])
+
+            self.render("campaignOptTreeRadial.html", tree = json.dumps(tree[0]) )
 
