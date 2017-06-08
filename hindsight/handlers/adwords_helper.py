5a02c5fcdf037ef7b0dd5ecdef7b103ae3be50be
rarcher2011 <richardjamesarcher@gmail.com>
2017-06-08 14:37:07 +0000

    update adwords helper to use helper function with apply instead of long replace

--- hindsight/handlers/adwords_helper.py
+++ hindsight/handlers/adwords_helper.py
@@ -4,10 +4,16 @@ SELECT = "select endpoint, name, advertiser_id, id from adwords_campaign_endpoin
 
 class AdwordsHelper:
 
+    def helper_replace(self,url):
+        url = url.replace('/crusher/dashboard','/crusher/v1/visitor/yoshi_mediaplan')
+        url = url.replace('selected_action','filter_id') 
+        url = url + '&prevent_sample=true&num_days=2'
+        return url
+
     @decorators.deferred
     def build_response(self,advertiser_id, plan):
         data = self.crushercache.select_dataframe(SELECT % advertiser_id)
         if plan == "media":
-            data['endpoint'] = data['endpoint'].apply(lambda x : x.replace('/crusher/dashboard','/crusher/v1/visitor/yoshi_mediaplan').replace('selected_action','filter_id') + '&prevent_sample=true&num_days=2')
+            data['endpoint'] = data['endpoint'].apply(lambda x : self.helper_replace(x))
         resp = {"response":data.to_dict('records')}
         return resp
