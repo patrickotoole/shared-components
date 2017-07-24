243eac033242164378114922ce5d5c114c7b5dc2
patrickotoole <patrick.n.otoole@gmail.com>
2017-07-24 17:44:02 +0000

    modifications to dashboard event to include API call

--- js/dashboard/src/events/action_events.js
+++ js/dashboard/src/events/action_events.js
@@ -1,10 +1,13 @@
 import {s as state} from 'state';
+import * as api from 'api'
+
 
 
 export default function init() {
   const s = state;
 
   state
+    .registerEvent("actions.get", function() { s.publishStatic("actions",api.action.getAll) })
     .registerEvent("action.change", function(action) { s.publish("selected_action",action) })
     .registerEvent("action_date.change", function(date) { s.publish("action_date",date) })
     .registerEvent("comparison_date.change", function(date) { s.publish("comparison_date",date) })
